188cff362ecdf05d8b24bcdb97159236cc171989
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-04-17 16:01:29 +0900

    Day 2

---
+++ CodingStudy/BAEKJOON 10951.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	while (1) {
+		cin >> a >> b;
+		if (cin.eof() == true) //eof get ^Z (ctrl+z)
+			break;
+		cout << a + b << "\n";
+	}
+	return 0;
+}
