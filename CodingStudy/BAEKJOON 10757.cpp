6ed069d2e9cf57692eda9a948b57c5f631ed2ced
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-04-19 16:42:12 +0900

    Day 4
    BAEKJOON 10757
    how to deal big numbers with c++

---
+++ CodingStudy/BAEKJOON 10757.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string.h>
+#include <cstring>
+using namespace std;
+
+int main() {
+	string a, b;
+	cin >> a >> b;
+	char x[10000], y[10000];
+	int result[10000];
+	strcpy(x, a.c_str());
+	strcpy(y, b.c_str());
+	int p = strlen(x), q = strlen(y);
+	int len;
+
+	/* 
+	
+	123456789    9
+	
+	012345678
+
+	000000123    0
+	
+	
+	2->9
+	*/
+	if (p >= q) {
+		len = p;
+		for (int i = p - 1, j = q - 1; i > p - q - 1 ; i--, j--) 
+			y[i] = y[j];
+		for (int i = p - q - 1; i > -1; i--) 
+			y[i] = '0';
+	}
+	else {
+		len = q;
+		for (int i = q - 1, j = p - 1; i > q - p - 1; i--, j--)
+			x[i] = x[j];
+		for (int i = q - p - 1; i > -1; i--)
+			x[i] = '0';
+	}
+
+
+	int digi = 0, sum = 0;
+	for (int i = len-1; i > -1; i--) {
+		sum = (x[i] - '0') + (y[i] - '0') + digi;
+
+		if (sum > 9) {
+			result[len - i - 1] = sum % 10;
+			digi = 1;
+			if (i == 0) {
+				result[len] = 1;
+				len += 1;
+				break;
+			}
+		}
+		else {
+			result[len - i - 1] = sum;
+			digi = 0;
+		}
+	}
+
+
+
+	for (int i = len - 1  ; i > - 1 ; i--) 
+		cout << result[i];
+	
+
+	return 0;
+}
--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,31 +1,55 @@
 #include <iostream>
+#include <string.h>
+#include <cstring>
 using namespace std;
 
-class Test {
-public:
-	int H, W, N;
-};
 int main() {
-	int T;
-	cin >> T;
+	string a, b;
+	cin >> a >> b;
+	char x[10000], y[10000];
+	int result[10000];
+	strcpy(x, a.c_str());
+	strcpy(y, b.c_str());
+	int p = strlen(x), q = strlen(y);
+	int len;
 
-	Test* arr = new Test[T];
-
-	for (int i = 0; i < T ; i++) 
-		cin >> arr[i].H >> arr[i].W >> arr[i].N;
-
-	float a, b;
-	for (int i = 0; i < T; i++) {
-		b = float (arr[i].N) / float(arr[i].H);
-		if (int(b) - b != 0)// 소수점이면
-			b = int(b) + 1;
-		a = arr[i].N - arr[i].H * (b - 1);
-
-		cout << a;
-		cout.width(2); // 폭을 2로 지정
-		cout.fill('0'); // 채움 문자는 '0'
-		cout << b << endl;
+	if (p >= q) {
+		len = p;
+		for (int i = p - 1, j = q - 1; i > p - q - 1 ; i--, j--) 
+			y[i] = y[j];
+		for (int i = p - q - 1; i > -1; i--) 
+			y[i] = '0';
 	}
+	else {
+		len = q;
+		for (int i = q - 1, j = p - 1; i > q - p - 1; i--, j--)
+			x[i] = x[j];
+		for (int i = q - p - 1; i > -1; i--)
+			x[i] = '0';
+	}
+
+
+	int digi = 0, sum = 0;
+	for (int i = len-1; i > -1; i--) {
+		sum = (x[i] - '0') + (y[i] - '0') + digi;
+
+		if (sum > 9) {
+			result[len - i - 1] = sum % 10;
+			digi = 1;
+			if (i == 0) {
+				result[len] = 1;
+				len += 1;
+				break;
+			}
+		}
+		else {
+			result[len - i - 1] = sum;
+			digi = 0;
+		}
+	}
+
+	for (int i = len - 1  ; i > - 1 ; i--) 
+		cout << result[i];
 
 	return 0;
 }
