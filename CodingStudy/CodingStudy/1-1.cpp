f241606179768b18a46a464634f438e9b1aac5f1
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-05-29 22:41:48 +0900

    Day 1-2
    - Dynamic Programming
    
    BAEKJOON 1003
    
    *reference
    https://velog.io/@polynomeer/%EB%8F%99%EC%A0%81-%EA%B3%84%ED%9A%8D%EB%B2%95Dynamic-Programming

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,31 +1,67 @@
 #include <iostream>
-#include <cstring>
-#include <string>
-
 using namespace std;
 
-int main() {
-	string word;
-	cin >> word;
+int memo[40][2]; //memorize subproblem's information
+int memo2[40];
 
-	//단어 입력 string -> char array, 길이도!
-	char* arr = new char[word.length() + 1];
-	memmove(arr, word.c_str(), word.length());
+// n을 n-1, n-2로 나눈다 
+// n-1이 구한적이 없으면 피보나치 아니면 패스
 
+int fibonacci(int n) {
+	if (n == 0) {
+		memo2[0] = 0;
+		memo[0][0] = 1;
+		memo[0][1] = 0;
+		return 0;
+	}
+	else if (n == 1) {
+		memo2[1] = 1;
+		memo[1][0] = 0;
+		memo[1][1] = 1;
+		return 1;
+	}
 
-	//2중 for문
-	//1번째 a~z 아스키코드 활용
-	//2번째 입력받은 단어에서 찾으면 break,else printout -1
-	
-	for (int i = 0; i < 26 ; i++) {
-		for (int j = 0; j < word.length()+1; j++) {
-			if (char(i + 97) == arr[j]) {
-				cout << j << " ";
-				break;
-			}
-			if (j == word.length())
-				cout << "-1 ";
+	else {
+		if (memo2[n - 1] < 0) {
+			memo2[n - 1] = fibonacci(n - 1);
+			
 		}
+		if (memo2[n - 2] < 0)
+			memo2[n - 2] = fibonacci(n - 2);
+	
+		memo[n][0] = memo[n - 1][0] + memo[n - 2][0];
+		memo[n][1] = memo[n - 1][1] + memo[n - 2][1];
+		return memo2[n - 1] + memo2[n - 2];
+	}
+}
+
+int main() {
+	// Dynamic Programming (동적 계획법)
+	// solving problem by breaking big problem into smaller problems
+
+	// to solve problem by Dynamic Programming, two properties must be satisfied
+	// 1. Overlapping Subproblem    2. Optimal Substructure
+
+	// in Dynamic Programming, you should solve each Subproblems only onc
+	// because of Optimal Substructure same Subproblems have same answer
+	// Therefore, once you have found the answer, you memorize it in the cache
+	// and this is called 'Memoization'
+
+	int test_case;
+	cin >> test_case;
+	int *num = new int[test_case];
+	for (int i = 0; i < test_case; i++) {
+		cin >> num[i];
+	}
+
+	for (int i = 0; i < test_case; i++) {
+		for (int i = 0; i < 40; i++) {
+			memo[i][0] = 0;
+			memo[i][1] = 0;
+			memo2[i] = -1;
+		}
+		fibonacci(num[i]);
+		cout << memo[num[i]][0]<< " " <<memo[num[i]][1]<< endl;
 	}
 
 	return 0;
