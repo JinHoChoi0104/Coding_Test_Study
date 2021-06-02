9dfe173761415372447afca632cf65f2087cdbb9
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-06-02 16:29:25 +0900

    Day 5
    BAEKJOON 1904
    
    - Clear!!
    
    sol )
    a[1] = 1, a[2] = 2
    a[n] = a[n-1] + a[n-2]   (n >= 3)
    
    proof)
    a[n] can be divided with 2cases
    - case1: end with '00'
    'n-2' numbers are arranged in the front of '00'
    and arranged with 'n-2' numbers there are a[n-2] cases
    
    - case2: end with '1'
    'n-1' numbers are arranged in the front of '1'
    and arranged with 'n-1' numbers there are a[n-1] cases
    
    result)
    a[n] = a[n-1] + a[n-2]

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,43 +1,10 @@
 #include <iostream>
 using namespace std;
 
-long int memo[1500001]; //memorize subproblem's information
-// 1 <= N <= 1,000,000
-// 1 <= x <= 500,000
-// 1 <= y <= 1,000,000
-// 1 <= x + y <= 1,500,000
-
-long int factorial(int n) {
-	if (n == 0 || n == 1)
-		n = 1;
-	else 
-		for (int i = n - 1; i > 0; i--) {
-			n %= 15746;
-			n *= i;
-//			n %= 15746;
-		}
-	return n;
-}
-
-int tile(int x, int y) {
-	if (memo[x + y] < 0) {
-		memo[x + y] = factorial(x + y);
-		cout << x << " + "<< y << " : " << memo[x + y] << endl;
-	}
-		//memo[(x + y) % 15746] = factorial((x + y) % 15746);
-	if (memo[x] < 0) {
-		memo[x] = factorial(x);
-		cout << x  << " : " << memo[x] << endl;
-	}
-	if (memo[y] < 0) {
-		memo[y] = factorial(y);
-	//	cout << memo[y] << endl;
-		cout << y << " : " << memo[y] << endl;
-	}
-	//cout << x << " " << y << " " << x + y << endl;
-	//cout << memo[x] << " " << memo[y] << " " << memo [x + y] << endl;
-	return memo[x + y] / memo[x] / memo[y];
-}
+int memo[1000001];
+// a[n] = a[n-1] + a[n-2]
+// a[1] = 1
+// a[2] = 2
 
 int main() {
 	// Dynamic Programming (µ¿Àû °èÈ¹¹ý)
@@ -51,24 +18,16 @@ int main() {
 	// Therefore, once you have found the answer, you memorize it in the cache
 	// and this is called 'Memoization'
 
-	
 	int N;
-	//cout << factorial(21);
 	cin >> N;
 
-	for (int i = 0; i < 1500001; i++) 
-		memo[i] = -1;
+	memo[1] = 1;
+	memo[2] = 2;
 
-	int sum = 0;
-	// 1. get combination with repetition of give number N by '00' and '1'
-	for (int i = 0; i < N / 2 + 1; i++) { // if) N = 4, 5 then N/2 = 2
-		//sum %= 15746;
-		sum += tile(i, N - 2 * i); // 2. get permutation of N's combination of '00' and '1', and then sum them all
-		cout << i << " " << N - 2 * i << " => " << sum << endl;
-	//	cout << sum << endl;
-	
-	}
-	cout << sum % 15746;
+	for (int i = 3; i < N+1; i++) 
+		memo[i] = (memo[i - 1] + memo[i - 2])%15746;
+
+	cout << memo[N];
 	
 	return 0;
 }
