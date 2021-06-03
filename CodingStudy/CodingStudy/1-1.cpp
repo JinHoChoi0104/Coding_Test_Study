075098759a8b9c665f0bfdc0462289b5d422525f
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-06-03 20:55:17 +0900

    Day 6
    BAEKJOON 1932

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int memo[1000001];
-// a[n] = a[n-1] + a[n-2]
-// a[1] = 1
-// a[2] = 2
+unsigned long memo[101];
+// P[n] = P[n-1] + P[n-5] (n>=6)
+// P[1~5] = {1, 1, 1, 2 ,2}
 
 int main() {
 	// Dynamic Programming (동적 계획법)
@@ -18,16 +17,39 @@ int main() {
 	// Therefore, once you have found the answer, you memorize it in the cache
 	// and this is called 'Memoization'
 
+	// two ways of solving
+	// 1. Top-down : breaking into smaller problems, using recursion calling
+	// 2. Bottom-up : strating from smaller problems to bigger problems, mostly using loop
+
 	int N;
 	cin >> N;
 
-	memo[1] = 1;
-	memo[2] = 2;
+	int** arr = (int**)malloc(sizeof(int*) * N);
+	for (int i = 0; i < N; i++) {
+		arr[i] = (int*)malloc(sizeof(int) * N);
+		for (int j = 0; j < i+1; j++) {
+			cin >> arr[i][j];
+		}
+	}
 
-	for (int i = 3; i < N+1; i++) 
-		memo[i] = (memo[i - 1] + memo[i - 2])%15746;
+	for (int i = 1; i < N; i++) { //세로 층 수
+		for (int j = 1; j < i; j++) { //가로
+			if (arr[i - 1][j - 1] > arr[i - 1][j])
+				arr[i][j] += arr[i - 1][j-1];
+			else
+				arr[i][j] += arr[i - 1][j];
+		}
+		arr[i][0] += arr[i - 1][0];
+		arr[i][i] += arr[i - 1][i - 1];
+		
+	}
 
-	cout << memo[N];
+	int max = arr[N - 1][0];
+	for (int i = 1; i < N; i++) {
+		if (max < arr[N - 1][i])
+			max = arr[N - 1][i];
+	}
+	cout << max;
 	
 	return 0;
 }
