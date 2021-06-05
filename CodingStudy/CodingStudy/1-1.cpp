825e3fa5ac34b4cfebfa24c22dbbe837621563bc
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-06-05 23:38:28 +0900

    Day 8
    BAEKJOON 1463

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
+#define MAXVALUE 2
+
 using namespace std;
 
-unsigned long memo[3];
-// P[n] = P[n-1] + P[n-5] (n>=6)
-// P[1~5] = {1, 1, 1, 2 ,2}
+int mini = 100000;
+unsigned long memo[100000];
 
+// BAEKJOON 1436
+// solving in a Top-down way
+void make_one(int n, int level) {
+	if (n == 1) {
+		if (mini > level)
+			mini = level;
+	}
+	else if (n % 3 == 0 && n % 2 == 0) {
+		if (mini > level)
+			make_one(n / 3, level + 1);
+		if (mini > level)
+			make_one(n / 2, level + 1);
+	}
+	else if (n % 3 == 0) {
+		if (mini > level)
+			make_one(n / 3, level + 1);
+		if (mini > level)
+			make_one(n - 1, level + 1);
+	}
+	else if (n % 2 == 0) {
+		if (mini > level)
+			make_one(n / 2, level + 1);
+		if (mini > level)
+			make_one(n - 1, level + 1);
+	}
+	else {
+		if (mini > level)
+			make_one(n - 1, level + 1);
+	}
+
+}
 int main() {
 	// Dynamic Programming (µ¿Àû °èÈ¹¹ý)
 	// solving problem by breaking big problem into smaller problems
@@ -12,7 +44,7 @@ int main() {
 	// to solve problem by Dynamic Programming, two properties must be satisfied
 	// 1. Overlapping Subproblem    2. Optimal Substructure
 
-	// in Dynamic Programming, you should solve each Subproblems only onc
+	// in Dynamic Programming, you should solve each Subproblems only once
 	// because of Optimal Substructure same Subproblems have same answer
 	// Therefore, once you have found the answer, you memorize it in the cache
 	// and this is called 'Memoization'
@@ -24,55 +56,9 @@ int main() {
 	int N;
 	cin >> N;
 
+	// divide in to 4 cases depending on whether it is divided by 2 and 3
+	make_one(N, 0);
 	
-	unsigned long* arr = new unsigned long[N];
-
-	for (int i = 0; i < N; i++) {
-		cin >> arr[i];
-	}
-
-	if (N >= 4) {
-		memo[0] = arr[N - 1] + arr[N - 3] + arr[N - 4]; // oxoo
-		memo[1] = arr[N - 1] + arr[N - 3]; //oxox
-		memo[2] = arr[N - 1] + arr[N - 2] + arr[N - 4]; //ooxo
-	}
-	else if (N == 3) {
-		memo[0] = 0;
-		memo[1] = arr[N - 1] + arr[N - 2];
-		memo[2] = arr[N - 1] + arr[N - 3];
-	}
-	else if (N == 2) {
-		memo[0] = arr[N - 1] + arr[N - 2];
-		memo[1] = arr[N - 1];
-		memo[2] = 0;
-	}
-	else {
-		memo[0] = arr[N - 1];
-		memo[1] = arr[N - 1];
-		memo[2] = arr[N - 1];
-	}
-
-	unsigned long a, b, c;
-	for (int i = N - 5; i > -1; i--) {
-		a = memo[0];
-		b = memo[1];
-		c = memo[2];
-
-		memo[2] = b + arr[i];
-		memo[0] = c + arr[i];
-		if (a > c)
-			memo[1] = a;
-		else
-			memo[1] = c;
-	}
-	
-	unsigned long max = memo[0];
-
-	for (int i = 1; i < 3; i++) 
-		if (max < memo[i])
-			max = memo[i];
-
-	cout << max;
-	
+	cout << mini;
 	return 0;
 }
