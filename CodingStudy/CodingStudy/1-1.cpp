24e9d7b372fe08d6e0f7af178bb4adfc0811a988
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-06-17 23:27:01 +0900

    Day 21
    solving BAEKJOON 11000
    time over
    need to reduce time

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -14,6 +14,8 @@ public:
 	}
 	void SetInfo(int _S, int _T);
 	void GetInfo(); 
+	int GetS() { return S; };
+	int GetT() { return T; };
 };
 
 void lecture::SetInfo(int _S, int _T) {
@@ -40,6 +42,36 @@ int main()
 	}
 
 	for (int i = 0; i < arr.size(); i++) {
-		arr[i].GetInfo();
+	//	arr[i].GetInfo();
 	}
+	int result = 0;
+	int min = 1000000001; // arr.front().GetS();;//arr.front().GetS();
+	int check = 0; // when the previous class is over
+	while (1) {
+		//min = arr.front().GetS();
+		int index = 0;
+		int cnt = 0;
+		for (int i = 0; i < arr.size(); i++) {
+			if (min > arr[i].GetS() && check < arr[i].GetS()) {
+				min = arr[i].GetS();
+				index = i;
+				check = arr[i].GetT();
+			}
+			else
+				cnt++;
+		}
+		if (cnt != arr.size())
+			arr.erase(arr.begin() + index);
+		else {
+			min = 1000000001; // arr.front().GetS();
+			check = 0;
+			result++;
+		}
+
+		
+	//	cout << min << endl;
+		if (arr.size() == 0)
+			break;
+	}
+	cout << result;
 }
