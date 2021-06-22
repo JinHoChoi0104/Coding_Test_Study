8f125cf70381623e8515fcecce3fa60fec61bd50
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-06-22 18:24:34 +0900

    Day 26
    solved BAEKJOON 1697
    instead of using DFS, used BFS
    
    BFS is more powerful in unweighted shortest path search

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,46 +1,61 @@
 #include <iostream>
-#include <cmath>
+#include <queue>
+#include <set>
+using namespace std;
 
-// BAEKJOOON 4948 *Prime Number Algorithm
-// get Prime Number between given number N and 2N
-// 1 <= N <= 123456
+//BAEKJOON 1697
 
-/* There are 2ways of geting Prime Number
- 
- 1. To find out whether Given numerb N is prime number
- divide N by 1 ~ sqrt(N) and if it is not divided then N is prime number 
- 
- 2. let's say we get prime numer in range of 1 ~ n
- multiple of 2 is not prime number, so exclude it
- multiple of 3 is not prime number, so exclude it
- multiple of 5 is not prime number, so exclude it
- multiple of 7 is not prime number, so exclude it ...
- do this until sqrt(N)
- numbers except excluded by the above process are prime number
-*/
-const int range = 123456;
+int HideAndSick(int pos, int K) { // pos: position where SooBin is placed now
+	set<int> visited; //don't visit places you've alrady been
+	visited.insert(pos);
+
+	int depth = 0;
+	queue<pair<int, int>>arr;
+	arr.push({ pos ,depth });
+	
+
+	while (K != pos) {
+		pos = arr.front().first;
+		depth = arr.front().second;
+		arr.pop();
+		if (pos >= K) {
+			if (visited.find(pos - 1) == visited.end()) { //set.find return index (iterator), if there is no element then return set.end()	
+				arr.push({ pos - 1,depth + 1 });
+				visited.insert(pos - 1);
+			}
+		}
+		else {
+			if (K == pos * 2) {
+				if (visited.find(pos * 2) == visited.end()) {
+					arr.push({ pos * 2,depth + 1 });
+					visited.insert(pos * 2);
+				}
+			}
+			else {
+				if (visited.find(pos * 2) == visited.end()) { 	
+					arr.push({ pos * 2,depth + 1 });
+					visited.insert(pos * 2);
+				}
+				if (visited.find(pos - 1) == visited.end()) {
+					arr.push({ pos - 1,depth + 1 });
+					visited.insert(pos - 1);
+				}
+				if (visited.find(pos + 1) == visited.end()) {
+					arr.push({ pos + 1,depth + 1 });
+					visited.insert(pos + 1);
+				}
+			}
+		}
+	}
+
+	return depth;
+}
+
 
 int main()
 {
-	int N = 1;
-	int cnt = 0;
-	int arr[range * 2 + 1] = {0}; // 1: not prime number, 0: prime number
-	arr[0] = 1;
-	arr[1] = 1;
+	int N, K;
+	cin >> N >> K;
 
-	for (int i = 2; i <= sqrt(2*range); i++) {
-		if (arr[i] != 1) 
-			for (int j = 2; j * i <=  range * 2; j++) 
-				arr[i * j] = 1;
-	}
-
-	scanf("%d", &N);
-	while (N != 0) {
-		cnt = 0;
-		for (int i = N + 1; i <= 2 * N; i++) 
-			arr[i] == 0 ? cnt ++ : NULL;
-		
-		printf("%d\n", cnt);
-		scanf("%d", &N);
-	}
+	cout << HideAndSick(N, K);
 }
