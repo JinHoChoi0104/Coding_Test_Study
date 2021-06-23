3a96d3e9bcd89d6871b82506f4ecedb0ddf188f3
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-06-23 11:03:01 +0900

    Day 27
    BAEKJOON 1260
    - DFS and BFS basic

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,35 +1,65 @@
-// BAEKJOON 1157
+// BAEKJOON 1260
 
 #include <iostream>
-#include <cstring>
-
 using namespace std;
 
-int main() {
+#include <queue>
 
-	string word;
-	cin >> word;
-	char* arr = new char[word.length() + 1]; // string to char array
-	memmove(arr, word.c_str(), word.length());
-
-	int alpha[26] = {0};
-	for (int i = 0; i < word.length(); i++) {
-		if ((int)arr[i] > 96) // if small , a = 97
-			alpha[(int)arr[i] - 97]++;
-		else // if capital a = 65
-			alpha[(int)arr[i] - 65]++;
+void dfs(int from, int *arr[], bool *visited, int N) { //used recursion to implement dfs
+	visited[from] = true;
+	cout << from << " ";
+	for (int i = 1; i < N + 1; i++) {
+		if (arr[from][i] != 0 && visited[i] == false)
+			dfs(i, arr, visited, N);
 	}
+}
 
-	int max = 0, max_cnt = 0;
-	for (int i = 0; i < 26; i++) {
-		if (max_cnt < alpha[i]) {
-			max_cnt = alpha[i];
-			max = i;
+queue <int> map;
+void bfs(int from, int* arr[], bool* visited, int N) {
+	cout << from << " ";
+	visited[from] = true;
+	
+	for (int i = 1; i < N + 1; i++) {
+		if (arr[from][i] != 0 && visited[i] == false) {
+			map.push(i);
+			visited[i] = true;
 		}
-		else if (max_cnt == alpha[i]) 
-			max = -2;
-		
+	}
+	
+	if (!map.empty()) { //check if there is no places to visit
+		int next = map.front();
+		map.pop();
+		bfs(next, arr, visited, N);
+	}
+}
+
+int main() {
+	int N, M, V; //nod, line, starting nod
+	cin >> N >> M >> V;
+
+	bool *visited = new bool[N+1];
+	int** arr = (int**)malloc(sizeof(int*) * (N + 1));
+
+
+	for (int i = 0; i < N + 1; i++) {
+		arr[i] = (int*)malloc(sizeof(int) * (N + 1));
+		visited[i+1] = false;
+		for (int j = 0; j < N + 1; j++)
+			arr[i][j] = 0;
 	}
 
-	cout << char(max + 65);
+	int a, b;
+	for (int i = 0; i < M; i++) {
+		cin >> a >> b;
+		arr[a][b] = 1;
+		arr[b][a] = 1;
+	}
+
+	dfs(V, arr, visited, N);
+	cout << endl;
+
+	for (int i = 0; i < N + 1; i++) 
+		visited[i + 1] = false;
+	
+	bfs(V, arr, visited, N);
 }
