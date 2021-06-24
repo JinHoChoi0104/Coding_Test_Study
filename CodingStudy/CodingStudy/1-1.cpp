9ad576af4488689a370aa57ae6d97d4034f2bfd0
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-06-24 17:25:52 +0900

    Day 28
    BAEKJOON 2206
    since it's aim is to find fastest path, I used BFS

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,61 +1,137 @@
-// BAEKJOON 2667
+// BAEKJOON 2206
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-#include <vector>
+#include <queue>
 
-bool isVisitable(int toX, int toY, bool* visited[], int N) {
-	if(toX > -1 && toX < N)
+class jurney {
+public:
+	int y;
+	int x;
+	bool crushed;
+	int cnt;
+	jurney(int _y, int _x, bool _crushed, int _cnt) {
+		y = _y;
+		x = _x;
+		crushed = _crushed;
+		cnt = _cnt;
+	}
+};
+
+bool isVisitable(int toY, int toX, bool* obstacle[], bool* uncrushed_visited[], bool* crushed_visited[], int N, int M, bool crushed) {
+	if (toX > -1 && toX < M) // check if it is in range
 		if (toY > -1 && toY < N)
-			if(!visited[toX][toY]) //if not visited
-				return true;
+			if (crushed) { //if it has crushed obstacle already => can't crush obstacle anymore
+				if (!obstacle[toY][toX] && !crushed_visited[toY][toX])
+					return true;
+				else
+					return false;
+			}
+			else {// if is hasn't crushed obstacle yet
+				if (obstacle[toY][toX]) {
+					if (!crushed_visited[toY][toX])  //it will crush obstacle
+						return true;
+					else
+						return false;
+				}
+				else {
+					if (!uncrushed_visited[toY][toX]) //if not visited
+						return true;	
+					else
+						return false;
+				}
+			}
+		
 	return false;
 }
 
-void dfs(int fromY, int fromX, bool *visited[], int N, int *cnt) { //used recursion to implement dfs
-	visited[fromY][fromX] = true;
-	(*cnt)++;
+void bfs(int fromY, int fromX, bool* obstacle[], bool* uncrushed_visited[], bool* crushed_visited[], int N, int M){
+	queue <jurney> navi;
+	navi.push(jurney(fromY, fromX, false, 1));
+	uncrushed_visited[fromY][fromX] = true;
+
+	int X, Y, cnt;
+	bool crush;
+	while (!navi.empty()) {
+		X = navi.front().x;
+		Y = navi.front().y;
+		crush = navi.front().crushed;
+		cnt = navi.front().cnt;
+		navi.pop();
 	
-	//RIGHT DOWN LEFT UP
-	if (isVisitable(fromY, fromX+1, visited, N))
-		dfs(fromY, fromX+1, visited, N, cnt);
-	if (isVisitable(fromY+1, fromX, visited, N))
-		dfs(fromY+1, fromX, visited, N, cnt);
-	if (isVisitable(fromY, fromX-1, visited, N))
-		dfs(fromY, fromX-1, visited, N, cnt);
-	if (isVisitable(fromY-1, fromX, visited, N))
-		dfs(fromY-1, fromX, visited, N, cnt);
+		if (Y == N - 1 && X == M - 1) {
+			cout << cnt;
+			break;
+		}
+		if (isVisitable(Y, X + 1, obstacle, uncrushed_visited, crushed_visited, N, M, crush)) {
+			if (obstacle[Y][X + 1])
+				navi.push(jurney(Y, X + 1, true, cnt + 1));
+			else
+				navi.push(jurney(Y, X + 1, crush, cnt + 1));	
+			if (!crush && !obstacle[Y][X + 1]) 
+				uncrushed_visited[Y][X + 1] = true;
+			else 
+				crushed_visited[Y][X + 1] = true;
+
+		}
+		if (isVisitable(Y + 1, X, obstacle, uncrushed_visited, crushed_visited, N, M, crush)) {
+			if (obstacle[Y + 1][X])
+				navi.push(jurney(Y + 1, X, true, cnt + 1));
+			else
+				navi.push(jurney(Y + 1, X , crush, cnt + 1));
+			if (!crush && !obstacle[Y + 1][X]) 
+				uncrushed_visited[Y + 1][X] = true;
+			else 
+				crushed_visited[Y + 1][X] = true;
+	
+		}
+		if (isVisitable(Y, X - 1, obstacle, uncrushed_visited, crushed_visited, N, M, crush)) {
+			if (obstacle[Y][X - 1])
+				navi.push(jurney(Y, X - 1, true, cnt + 1));
+			else
+				navi.push(jurney(Y, X - 1, crush, cnt + 1));
+			if (!crush && !obstacle[Y][X - 1])
+				uncrushed_visited[Y][X - 1] = true;
+			else
+				crushed_visited[Y][X - 1] = true;
+		}
+		if (isVisitable(Y - 1, X, obstacle, uncrushed_visited, crushed_visited, N, M, crush)) {
+			if (obstacle[Y - 1][X])
+				navi.push(jurney(Y - 1, X, true, cnt + 1));
+			else
+				navi.push(jurney(Y - 1, X, crush, cnt + 1));
+			if (!crush && !obstacle[Y - 1][X])
+				uncrushed_visited[Y - 1][X] = true;
+			else
+				crushed_visited[Y - 1][X] = true;
+		}
+	}
+	if (Y == N - 1 && X == M - 1);
+	else
+		cout << "-1";
 }
 
 int main() {
-	int N; //map's size
-	cin >> N;
+	int N, M; //map's size , N = y, M = x
+	cin >> N >> M;
 		
 	int input = 0;
-	bool **visited = (bool**)malloc(sizeof(bool*) * (N));
+	
+	bool** obstacle = (bool**)malloc(sizeof(bool*) * (N));
+	bool** uncurshed_visited = (bool**)malloc(sizeof(bool*) * (N));
+	bool** curshed_visited = (bool**)malloc(sizeof(bool*) * (N));
 	
 	for (int i = 0; i < N; i++) {
-		visited[i] = (bool*)malloc(sizeof(bool) * (N));
-		for (int j = 0; j < N ; j++){
+		obstacle[i] = (bool*)malloc(sizeof(bool) * (M));
+		uncurshed_visited[i] = (bool*)malloc(sizeof(bool) * (M));
+		curshed_visited[i] = (bool*)malloc(sizeof(bool) * (M));
+		for (int j = 0; j < M ; j++){
 			scanf("%1d", &input); // get each number once, not using input such as \n
-			visited[i][j] = input == 1 ? false : true;
+			obstacle[i][j] = input == 1 ? true : false;
+			uncurshed_visited[i][j] = false;
+			curshed_visited[i][j] = false;
 		}
 	}
 
-	vector<int> house_set;
-	int cnt;
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++) {
-			cnt = 0;
-			if (visited[i][j] == false) {
-				dfs(i, j, visited, N, &cnt);
-				house_set.push_back(cnt);
-			}
-		}
-
-	cout << house_set.size() << endl;
-	sort(house_set.begin(), house_set.end());
-	for (int i = 0; i < house_set.size(); i++)
-		cout << house_set[i] << endl;
+	bfs(0, 0, obstacle, uncurshed_visited, curshed_visited, N, M);
 }
