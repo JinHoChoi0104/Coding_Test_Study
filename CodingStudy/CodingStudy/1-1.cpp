47c0616f14b86d92486749839e595f3c9e124d9e
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-07-03 21:33:10 +0900

    Day 37
    BAEKJOON 18870
    - sorting using <algorithm> library
    - delete duplicated element using 'erase' and 'unique'

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -2,29 +2,53 @@
 #include<stdio.h>
 #include<string>
 #include<vector>
-
+#include<algorithm>
 using namespace std;
 
+class location {
+public:
+	int rank;
+	int X;
+	int transX;
+	location(int _rank, int _X) {
+		rank = _rank;
+		X = _X;
+	}
+};
+bool compareByX(location a, location b) {
+	return a.X < b.X;
+}
+bool compareByRank(location a, location b) {
+	return a.rank < b.rank;
+}
 int main(void) {
 	int N;
 	cin >> N;
 
-	vector < vector <string> > byAge(201);
-	int age;
-	string name;
+	vector <location> arr;
+	vector <int> xValue;
+	int X;
 
 	for (int i = 0; i < N; i++) {
-		cin >> age >> name;
-		byAge[age].push_back(name);
+		scanf("%d", &X);
+		arr.push_back(location(i, X));
+		xValue.push_back(X);
 	}
+	sort(xValue.begin(), xValue.end());
+	xValue.erase(unique(xValue.begin(), xValue.end()), xValue.end());
 
-	for (int i = 1; i < 201; i++) {
-		if (byAge[i].size() > 0) {
-			for (int j = 0; j < byAge[i].size(); j++) {
-				cout << i << " " << byAge[i][j];
-				printf("\n");
-			}
-		}
+	sort(arr.begin(), arr.end(), compareByX);
+	int index = 0;
+	for (int i = 0; i < N; i++) {
+		if (arr[i].X == xValue[index]) 
+			arr[i].transX = index;
+		else
+			arr[i].transX = ++index;	
 	}
+	sort(arr.begin(), arr.end(), compareByRank);
+
+	for (int i = 0; i < N; i++)
+		printf("%d ", arr[i].transX);
+	
 	return 0;
 }
