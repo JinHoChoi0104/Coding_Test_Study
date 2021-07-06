dc3e94b7865dc952e93694870aaf23b6449e56ef
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-07-06 16:33:08 +0900

    Day 40
    solving BAEKJOON 2580
    sudoku problem
    
    occured run time error
    it's becaue my code can't solve problems which have multiple answer, for example input like
    0 0 0 0 0 0 0 0 0
    0 0 0 0 0 0 0 0 0
    0 0 0 0 0 0 0 0 0
    0 0 0 0 0 0 0 0 0
    0 0 0 0 0 0 0 0 0
    0 0 0 0 0 0 0 0 0
    0 0 0 0 0 0 0 0 0
    0 0 0 0 0 0 0 0 0
    0 0 0 0 0 0 0 0 0
    
    my code can only solve promblems which have only one answer
    
    And these problems with multi answers make time runout error
    
    So I will change code lot, should add guess input maybe using bfs using queue

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,79 +1,102 @@
-// BAEKJOON 9663
+// BAEKJOON 2580
+// Sudoku
 #include<iostream>
+#include<queue>
 using namespace std;
 
-void locateQueen(int y, int x, int N, bool isAble[][14]) {
-	for (int i = 0; i < N; i++) 
-		isAble[y][i] = false;
-	for (int i = 0; i < N; i++)
-		isAble[i][x] = false;
+queue<pair<int, int>>zero_arr;
 
-	int y2 = y, x2 = x;
-	while (y2 >= 0 && x2 >= 0) 
-		isAble[y2--][x2--] = false;
-
-	y2 = y, x2 = x;
-	while (y2 >= 0 && x2 < N) 
-		isAble[y2--][x2++] = false;
-
-	y2 = y, x2 = x;
-	while (y2 < N && x2 >= 0) 
-		isAble[y2++][x2--] = false;
-
-	y2 = y, x2 = x;
-	while (y2 < N && x2 < N) 
-		isAble[y2++][x2++] = false;
-}
-
-void chessTree(int y, int x, int N, bool isAble[][14], int *cnt) { // (0~N-1, 0)
-	bool isAble2[14][14];
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-			isAble2[i][j] = isAble[i][j];
-	locateQueen(y, x, N, isAble2);
-
-	for (int i = 0; i < N; i++) {
-		if (isAble2[i][x + 1]) {
-			if (x + 1 == N - 1)
-				(*cnt)++;
-			else
-				chessTree(i, x + 1, N, isAble2, cnt);
-		}
-	}
-}
-
-void showMeTheBoard(int N, bool board[][14]) {
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			if (board[i][j])
-				printf("0");
-			else
-				printf("1");
+void show_board(int board[][9]) {
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			printf("%d ", board[i][j]);
 		}
 		printf("\n");
 	}
 }
 
-int main(void) {
-	int N;
-	cin >> N;
- 
-	bool isAble[14][14];
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-			isAble[i][j] = true;
+void findZero(int board[][9]) {
+	for (int i = 0; i < 9; i++)
+		for (int j = 0; j < 9; j++)
+			if (board[i][j] == 0)
+				zero_arr.push(make_pair(i, j));
+}
 
-	int cnt = 0, cnt2 = 0;
-	if (N == 1)
-		cnt = 1;
-	else {
-		for (int i = 0; i < N; i++) {
-			cnt2 = 0;
-			chessTree(i, 0, N, isAble, &cnt2);
-			cnt += cnt2;
+void showZero() {
+	while (!zero_arr.empty()) {
+		cout << zero_arr.front().first << ", " << zero_arr.front().second << endl;
+		zero_arr.pop();
+	}
+}
+
+void make_false(bool* check) {
+	for (int i = 0; i < 10; i++)
+		check[i] = false;
+}
+
+bool makeZeroToNum(int board[][9], int i, int j, bool* check) {
+	int cnt = 0;
+
+	for (int k = 0; k < 9; k++) {
+		if (!check[board[i][k]]) {
+			check[board[i][k]] = true;
+			cnt++;
+		}
+		if (!check[board[k][j]]) {
+			check[board[k][j]] = true;
+			cnt++;
+		}
+		if (cnt == 9)
+			break;
+	}
+
+	//box
+	if (cnt != 9) {
+		for (int a = 0; a < 3; a++) {
+			for (int b = 0; b < 3; b++) {
+				if (!check[board[(i / 3) * 3 + a][(j / 3) * 3 + b]]) {
+					check[board[(i / 3) * 3 + a][(j / 3) * 3 + b]] = true;
+					cnt++;
+				}
+				if (cnt == 9)
+					break;
+			}
+			if (cnt == 9)
+				break;
 		}
 	}
-	cout << cnt << endl;
 
+	if (cnt == 9) {
+		for (int k = 1; k < 10; k++)
+			if (!check[k]) {
+				board[i][j] = k;
+				return true;
+			}
+	}
+
+	return false;
+}
+
+int main(void) {
+	int board[9][9];
+	for (int i = 0; i < 9; i++)
+		for (int j = 0; j < 9; j++)
+			scanf("%d", &board[i][j]);
+
+	findZero(board);
+
+	int y = 0, x = 0;
+	bool check[10] = { false };
+	while (!zero_arr.empty()) {
+		y = zero_arr.front().first;
+		x = zero_arr.front().second;
+		zero_arr.pop();
+
+		if(!makeZeroToNum(board, y, x, check))
+			zero_arr.push(make_pair(y, x));
+		make_false(check);
+	}
+
+	show_board(board);
 	return 0;
 }
