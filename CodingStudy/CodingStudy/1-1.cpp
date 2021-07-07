32c19c442cd046f37b7efc8f4634b03d92eccd13
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-07-07 15:59:27 +0900

    Day 41
    still solving BAEKJOON 2580
    to solve problems
    I have changed strategy three times. First was to scan board by vertical, horizotal, and square in order, and evey time you find zero you make up number for it.
    Second was to look up for evey zero and save it's location in queue and make up number for front element in that queue
    Third was using BFS, however it needed too much time when input was 81 of zeros
    So at last I had changed strategy to DFS. To do DFS I used recursion function, and stack. However neighter one was possible,,,,
    For conclusin I think DFS using recursion function is the best tool for now, so I will keep on trying it

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -2,17 +2,29 @@
 // Sudoku
 #include<iostream>
 #include<queue>
+#include<vector>
+#include<stack>
 using namespace std;
 
+stack<vector<int>> arr;
+queue<vector<int>> board_arr;
 queue<pair<int, int>>zero_arr;
 
-void show_board(int board[][9]) {
+void showBoard(vector<int>& board) {
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 9; j++) {
-			printf("%d ", board[i][j]);
+			printf("%d ", board[i * 9 + j]);
 		}
 		printf("\n");
 	}
+}
+
+bool isPerfect(vector<int>& board) {
+	for (int i = 0; i < 9; i++)
+		for (int j = 0; j < 9; j++)
+			if (board[i * 9 + j] == 0)
+				return false;
+	return true;
 }
 
 void findZero(int board[][9]) {
@@ -77,14 +89,159 @@ bool makeZeroToNum(int board[][9], int i, int j, bool* check) {
 	return false;
 }
 
+void BFS(vector<int>& board, bool* check) {
+
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			if (board[i * 9 + j] == 0) {
+				for (int k = 0; k < 9; k++) {
+					if (!check[board[i * 9 + k]]) {
+						check[board[i * 9 + k]] = true;
+					}
+					
+					if (!check[board[k * 9 + j]]) {
+						check[board[k * 9 + j]] = true;
+					}
+				}
+
+				for (int a = 0; a < 3; a++) {
+					for (int b = 0; b < 3; b++) {
+						if (!check[board[ ((i / 3) * 3 + a) * 9 + ((j / 3) * 3 + b)]]) {
+							check[board[((i / 3) * 3 + a) * 9 + ((j / 3) * 3 + b)]] = true;
+						}
+					}			
+				}
+
+				for (int k = 1; k < 10; k++)
+					if (!check[k]) {
+						board[i * 9 + j] = k;
+						board_arr.push(board);
+					}
+
+
+			}
+		
+		}
+		
+	}
+}
+void makeGuess() {
+	vector<int> board;
+	bool check[10] = { false };
+	while (!board_arr.empty()) {
+		board = board_arr.front();
+		board_arr.pop();
+		//showBoard(board);
+	//	cout << endl;
+		make_false(check);
+		if (isPerfect(board)) {
+			break;
+		}
+		else
+			BFS(board, check);
+
+	}
+	showBoard(board);
+
+}
+
+bool finished = false;
+vector<int> answer_board;
+int cnt = 0;
+int cnt2 = 1;
+int cnt3 = 1;
+void DFS(vector<int> board) {
+	if (isPerfect(board)) {
+		answer_board = board;
+	}
+	else {
+		bool check[10] = { false };
+		//cout << cnt3 << "======" << endl;
+		//showBoard(board);
+		//finished = true;
+
+		//vector<int> board;
+		//board = arr.top();
+		//arr.pop();
+		cnt = 0;
+		make_false(check);
+
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (board[i * 9 + j] == 0) {
+					for (int k = 0; k < 9; k++) {
+						if (!check[board[i * 9 + k]]) {
+							check[board[i * 9 + k]] = true;
+							cnt++;
+						}
+
+						if (!check[board[k * 9 + j]]) {
+							check[board[k * 9 + j]] = true;
+							cnt++;
+						}
+						if (cnt == 10)
+							break;
+					}
+
+					for (int a = 0; a < 3; a++) {
+						
+						for (int b = 0; b < 3; b++) {
+							if (!check[board[((i / 3) * 3 + a) * 9 + ((j / 3) * 3 + b)]]) {
+								check[board[((i / 3) * 3 + a) * 9 + ((j / 3) * 3 + b)]] = true;
+							}
+
+						}
+					}
+					/*
+					for (int k = 1; k < 10; k++)
+						if (!check[k]) {
+							board[i * 9 + j] = k;
+							arr.push(board);
+						}
+
+						*/
+
+					for (int k = 1; k < 10; k++) {
+						if (!check[k]) {
+							board[i * 9 + j] = k;
+							if (isPerfect(board)) {
+								answer_board = board;
+								//cout << cnt2<<"======" << endl;
+								//showBoard(board);
+								finished = true;
+						
+								//cnt2++;
+							}
+							if (!finished)
+								DFS(board);
+						}
+					}
+					break;
+				}
+			}
+		}
+	}
+}
 int main(void) {
-	int board[9][9];
-	for (int i = 0; i < 9; i++)
-		for (int j = 0; j < 9; j++)
-			scanf("%d", &board[i][j]);
+	vector<int> board;
+	int num = 0;
+	for (int i = 0; i < 81; i++) {
+		scanf("%d", &num);
+		board.push_back(num);
+	}
 
-	findZero(board);
+	//board_arr.push(board);
+	//arr.push(board);
+	//bool check[10] = { false };
+	
+	//while (!isPerfect(arr.top()))
+		
+	DFS(board);
+	showBoard(answer_board);
+	//showBoard(arr.top());
+//	makeGuess();
 
+	/*
 	int y = 0, x = 0;
 	bool check[10] = { false };
 	while (!zero_arr.empty()) {
@@ -97,6 +254,6 @@ int main(void) {
 		make_false(check);
 	}
 
-	show_board(board);
+	show_board(board);*/
 	return 0;
 }
