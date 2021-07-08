322ccc0283ec20d21f2c16905011425659effb5b
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-07-08 15:19:46 +0900

    Day 42
    finall solved BAEKJOON 2580
    I used DFS to do guessing about unknown number (which input is 0)
    When there was a time out problem, it found 0 every time DFS was called.
    To solve problems I stored 0's location on queue, and gave it to DFS, every time when it is called.

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -3,12 +3,9 @@
 #include<iostream>
 #include<queue>
 #include<vector>
-#include<stack>
+
 using namespace std;
 
-stack<vector<int>> arr;
-queue<vector<int>> board_arr;
-queue<pair<int, int>>zero_arr;
 
 void showBoard(vector<int>& board) {
 	for (int i = 0; i < 9; i++) {
@@ -19,26 +16,11 @@ void showBoard(vector<int>& board) {
 	}
 }
 
-bool isPerfect(vector<int>& board) {
+void findZero(vector<int> board, queue<pair<int, int>>& arr) {
 	for (int i = 0; i < 9; i++)
 		for (int j = 0; j < 9; j++)
 			if (board[i * 9 + j] == 0)
-				return false;
-	return true;
-}
-
-void findZero(int board[][9]) {
-	for (int i = 0; i < 9; i++)
-		for (int j = 0; j < 9; j++)
-			if (board[i][j] == 0)
-				zero_arr.push(make_pair(i, j));
-}
-
-void showZero() {
-	while (!zero_arr.empty()) {
-		cout << zero_arr.front().first << ", " << zero_arr.front().second << endl;
-		zero_arr.pop();
-	}
+				arr.push(make_pair(i, j));
 }
 
 void make_false(bool* check) {
@@ -46,178 +28,55 @@ void make_false(bool* check) {
 		check[i] = false;
 }
 
-bool makeZeroToNum(int board[][9], int i, int j, bool* check) {
-	int cnt = 0;
-
-	for (int k = 0; k < 9; k++) {
-		if (!check[board[i][k]]) {
-			check[board[i][k]] = true;
-			cnt++;
-		}
-		if (!check[board[k][j]]) {
-			check[board[k][j]] = true;
-			cnt++;
-		}
-		if (cnt == 9)
-			break;
-	}
-
-	//box
-	if (cnt != 9) {
-		for (int a = 0; a < 3; a++) {
-			for (int b = 0; b < 3; b++) {
-				if (!check[board[(i / 3) * 3 + a][(j / 3) * 3 + b]]) {
-					check[board[(i / 3) * 3 + a][(j / 3) * 3 + b]] = true;
-					cnt++;
-				}
-				if (cnt == 9)
-					break;
-			}
-			if (cnt == 9)
-				break;
-		}
-	}
-
-	if (cnt == 9) {
-		for (int k = 1; k < 10; k++)
-			if (!check[k]) {
-				board[i][j] = k;
-				return true;
-			}
-	}
-
-	return false;
-}
-
-void BFS(vector<int>& board, bool* check) {
-
-	for (int i = 0; i < 9; i++) {
-		for (int j = 0; j < 9; j++) {
-			if (board[i * 9 + j] == 0) {
-				for (int k = 0; k < 9; k++) {
-					if (!check[board[i * 9 + k]]) {
-						check[board[i * 9 + k]] = true;
-					}
-					
-					if (!check[board[k * 9 + j]]) {
-						check[board[k * 9 + j]] = true;
-					}
-				}
-
-				for (int a = 0; a < 3; a++) {
-					for (int b = 0; b < 3; b++) {
-						if (!check[board[ ((i / 3) * 3 + a) * 9 + ((j / 3) * 3 + b)]]) {
-							check[board[((i / 3) * 3 + a) * 9 + ((j / 3) * 3 + b)]] = true;
-						}
-					}			
-				}
-
-				for (int k = 1; k < 10; k++)
-					if (!check[k]) {
-						board[i * 9 + j] = k;
-						board_arr.push(board);
-					}
-
-
-			}
-		
-		}
-		
-	}
-}
-void makeGuess() {
-	vector<int> board;
-	bool check[10] = { false };
-	while (!board_arr.empty()) {
-		board = board_arr.front();
-		board_arr.pop();
-		//showBoard(board);
-	//	cout << endl;
-		make_false(check);
-		if (isPerfect(board)) {
-			break;
-		}
-		else
-			BFS(board, check);
-
-	}
-	showBoard(board);
-
-}
-
 bool finished = false;
 vector<int> answer_board;
-int cnt = 0;
-int cnt2 = 1;
-int cnt3 = 1;
-void DFS(vector<int> board) {
-	if (isPerfect(board)) {
+
+void DFS(vector<int> board, queue<pair<int, int>> zero_arr) {
+	if (zero_arr.empty()) {
 		answer_board = board;
+		finished = true;
 	}
 	else {
 		bool check[10] = { false };
-		//cout << cnt3 << "======" << endl;
-		//showBoard(board);
-		//finished = true;
-
-		//vector<int> board;
-		//board = arr.top();
-		//arr.pop();
-		cnt = 0;
+		int cnt = 0;
 		make_false(check);
 
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				if (board[i * 9 + j] == 0) {
-					for (int k = 0; k < 9; k++) {
-						if (!check[board[i * 9 + k]]) {
-							check[board[i * 9 + k]] = true;
-							cnt++;
-						}
+		int i = zero_arr.front().first;
+		int j = zero_arr.front().second;
+		zero_arr.pop();
 
-						if (!check[board[k * 9 + j]]) {
-							check[board[k * 9 + j]] = true;
-							cnt++;
-						}
-						if (cnt == 10)
-							break;
-					}
 
-					for (int a = 0; a < 3; a++) {
-						
-						for (int b = 0; b < 3; b++) {
-							if (!check[board[((i / 3) * 3 + a) * 9 + ((j / 3) * 3 + b)]]) {
-								check[board[((i / 3) * 3 + a) * 9 + ((j / 3) * 3 + b)]] = true;
-							}
+		for (int k = 0; k < 9; k++) {
+			if (!check[board[i * 9 + k]]) {
+				check[board[i * 9 + k]] = true;
+				cnt++;
+			}
 
-						}
-					}
-					/*
-					for (int k = 1; k < 10; k++)
-						if (!check[k]) {
-							board[i * 9 + j] = k;
-							arr.push(board);
-						}
+			if (!check[board[k * 9 + j]]) {
+				check[board[k * 9 + j]] = true;
+				cnt++;
+			}
+			if (cnt == 10)
+				break;
+		}
 
-						*/
-
-					for (int k = 1; k < 10; k++) {
-						if (!check[k]) {
-							board[i * 9 + j] = k;
-							if (isPerfect(board)) {
-								answer_board = board;
-								//cout << cnt2<<"======" << endl;
-								//showBoard(board);
-								finished = true;
-						
-								//cnt2++;
-							}
-							if (!finished)
-								DFS(board);
-						}
-					}
-					break;
+		for (int a = 0; a < 3; a++) {
+			if (cnt == 10)
+				break;
+			for (int b = 0; b < 3; b++) {
+				if (!check[board[((i / 3) * 3 + a) * 9 + ((j / 3) * 3 + b)]]) {
+					check[board[((i / 3) * 3 + a) * 9 + ((j / 3) * 3 + b)]] = true;
 				}
+			}
+		}
+
+		for (int k = 1; k < 10; k++) {
+			if (!check[k]) {
+				board[i * 9 + j] = k;
+				if (!finished)
+					DFS(board, zero_arr);
+				else
+					break;
 			}
 		}
 	}
@@ -229,31 +88,12 @@ int main(void) {
 		scanf("%d", &num);
 		board.push_back(num);
 	}
-
-	//board_arr.push(board);
-	//arr.push(board);
-	//bool check[10] = { false };
 	
-	//while (!isPerfect(arr.top()))
-		
-	DFS(board);
+	queue<pair<int, int>> zero_arr;
+	findZero(board, zero_arr);
+
+	DFS(board, zero_arr);
+
 	showBoard(answer_board);
-	//showBoard(arr.top());
-//	makeGuess();
-
-	/*
-	int y = 0, x = 0;
-	bool check[10] = { false };
-	while (!zero_arr.empty()) {
-		y = zero_arr.front().first;
-		x = zero_arr.front().second;
-		zero_arr.pop();
-
-		if(!makeZeroToNum(board, y, x, check))
-			zero_arr.push(make_pair(y, x));
-		make_false(check);
-	}
-
-	show_board(board);*/
 	return 0;
 }
