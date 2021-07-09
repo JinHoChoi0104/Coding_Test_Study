1009ad0847b0d9c6b02c96808a6e45f841715b87
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-07-09 22:28:35 +0900

    Day 43
    BAEKJOON 2156

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,75 +1,39 @@
-// BAEKJOON 14889
-
+// BAEKJOON 2156
 #include<iostream>
-#include<math.h>
-#include<vector>
 
 using namespace std;
 
-int score_arr[20][20];
-int min_difference = 100000;
-
-bool check[20] = { false };
-void makeFalse(bool* check) {
-	for (int i = 0; i < 20; i++)
-		check[i] = false;
-}
-void calculateScore(vector<int> member, int total_mem) {
-	makeFalse(check);
-	//Team with member0
-	int sum1 = 0;
-	for (int i = 0; i < member.size() - 1; i++) {
-		for (int j = i + 1; j < member.size(); j++) {
-			sum1 += score_arr[member[i]][member[j]];
-			sum1 += score_arr[member[j]][member[i]];
-		}
-	}
-	for (int i = 0; i < member.size(); i++) {
-		check[member[i]] = true;
-	}
-
-	//Team without member0
-	vector<int>member2;
-	for (int i = 0; i < total_mem; i++) {
-		if (!check[i])
-			member2.push_back(i);
-	}
-
-	int sum2 = 0;
-	for (int i = 0; i < member2.size()-1; i++) {
-		for (int j = i+1; j < member2.size(); j++) {
-			sum2 += score_arr[member2[i]][member2[j]];
-			sum2 += score_arr[member2[j]][member2[i]];
-		}
-	}
-
-	if (min_difference > abs(sum1 - sum2))
-		min_difference = abs(sum1 - sum2);
-}
-
-void findMember(vector<int> member, int last_mem , int total_mem, int need) {
-	member.push_back(last_mem);
-	if (need == 0) {
-		calculateScore(member, total_mem);
-	}
-	else {
-		for (int i = last_mem + 1; i < total_mem; i++) 
-			findMember(member, i, total_mem, need - 1);
-	}
-}
-
 int main(void) {
 	int N; 
-	cin >> N;
-	int n = N / 2 - 1;//Number of people required per team
+	scanf("%d", &N);
 
+	int arr[10000];
 	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-			cin >> score_arr[i][j];
+		scanf("%d", &arr[i]);
 
-	vector<int> member;
-	findMember(member, 0, N, n);
+	int drinkingState[4] = { 0 }; // OX, XO, OO, XX , 4 states of sipping
+	int OX = 0, XO = 0, OO = 0, XX = 0;
 
-	cout << min_difference;
+	for (int i = 0; i < N; i++) {
+		OX = drinkingState[0];
+		XO = drinkingState[1];
+		OO = drinkingState[2];
+		XX = drinkingState[3];
+		
+		//XO , OO -> OX
+		drinkingState[0] = XO > OO ? XO : OO;
+		//OX , XX -> XO
+		drinkingState[1] = OX > XX ? OX + arr[i] : XX + arr[i];
+		//XO -> OO
+		drinkingState[2] = XO + arr[i];
+		//OX -> XX
+		drinkingState[3] = OX;
+	}
+
+	int max_sip = 0;
+	for (int i = 0; i < 4; i++) 
+		max_sip < drinkingState[i] ? max_sip = drinkingState[i] : NULL;
+	
+	cout << max_sip;
 	return 0;
 }
