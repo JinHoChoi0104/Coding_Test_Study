d82ee3c6edcb11e52d1f89ee28dd037e1ed1d504
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-07-10 22:58:47 +0900

    Day 44-1
    solved BAEKJOON 1941

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,75 +1,77 @@
 // BAEKJOON 1941
 #include<iostream>
 #include<vector>
-#include<queue>
-#include<algorithm>
+
 using namespace std;
 
-int arr[5][5];
-void showRoutine(vector<pair<int, int>>routine) {
-	cout << "========" << endl;
+int arr[25];
+bool board[5][5];
+int sum = 0;
+
+// 1. choose 7 people 25C7
+// 2. eliminate under 3 S
+// while doing 1, use cutting by using logic 2
+// 3. check whether they are linked
+
+void makeFalse(){
 	for (int i = 0; i < 5; i++)
 		for (int j = 0; j < 5; j++)
-			arr[i][j] = 0;
-	int y = 0, x = 0;
-	while (!routine.empty()) {
-		y = routine.back().first;
-		x = routine.back().second;
-		arr[y][x] = 1;
-		routine.pop_back();
-	}
-	for (int i = 0; i < 5; i++) {
-		for (int j = 0; j < 5; j++) {
-			cout << arr[i][j];
-		}
-		cout << endl;
-	}
-}
-void DFS(int y, int x, vector<pair<int, int>> routine, int n) {
-	if (y > -1 && y < 5 && x > -1 && x < 5) {
-		//cout << "a";
-		routine.push_back(pair<int, int>(y, x));
-		n++;
-		if (n == 4)
-			showRoutine(routine);
-		else {
-			DFS(y + 1, x, routine, n);
-			DFS(y, x + 1, routine, n);
-			DFS(y - 1, x, routine, n);
-			DFS(y, x - 1, routine, n);
-		}
-	}
-	
+			board[i][j] = false;
 }
 
+void DFS(int y, int x, int &cnt) {
+	if (y > -1 && y < 5 && x > -1 && x < 5)	{
+		if (board[y][x]) {
+			cnt++;
+			board[y][x] = false;
 
+			DFS(y + 1, x, cnt);
+			DFS(y, x + 1, cnt);
+			DFS(y - 1, x, cnt);
+			DFS(y, x - 1, cnt);
+		}
+	}
+}
+
+void isLinked(vector<int> member) {
+	makeFalse();
+	for (int i = 0; i < member.size(); i++) {
+		board[member[i]/5][member[i]%5] = true;
+	}
+
+	int cnt = 0;
+	DFS(member[0] / 5, member[0] % 5, cnt);
+	if (cnt == 7)
+		sum++;
+}
+
+void findMember(vector<int> member, int last_mem, int need, int isS) {
+	member.push_back(last_mem);
+	if (need == 0) {
+		if(isS >= 4)
+			isLinked(member);
+	}
+	else if(isS >= 4 - need){
+		for (int i = last_mem + 1; i < 25; i++) {
+			findMember(member, i, need - 1, isS + arr[i]);
+		}
+	}
+}
 
 int main(void) {
-	vector<pair<int, int>> routine;
-	//DFS(2, 2, routine, 0);
-	routine.push_back(pair<int, int>(1, 1));
-	routine.push_back(pair<int, int>(1, 2));
-
-
-	vector<pair<int, int>> routine2;
-	//DFS(2, 2, routine, 0);
-	routine2.push_back(pair<int, int>(1, 1));
-	routine2.push_back(pair<int, int>(1, 2));
-
-	vector<vector<pair<int, int>>>arr;
-	arr.push_back(routine);
-	arr.push_back(routine2);
-
-	arr.erase(unique(arr.begin(), arr.end()), arr.end());
-	vector<pair<int, int>> a;
-	while (!arr.empty()) {
-		a = arr.back();
-		arr.pop_back();
-		cout << "=====" << endl;
-
-		while (!a.empty()) {
-			cout << a.back().first << ", " << a.back().second << endl;
-			a.pop_back();
-		}
+	char a;
+	int index = 0;
+	for (int i = 0; i < 29; i++) {
+		scanf("%1c", &a);
+		if (a == 'S')
+			arr[index++] = 1;
+		else if (a == 'Y')
+			arr[index++] = 0;
 	}
+
+	vector<int> b;
+	for (int i = 0; i < 25; i++) 
+		findMember(b, i, 6, arr[i] );
+
+	cout << sum;
 }
