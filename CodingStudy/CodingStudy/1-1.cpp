359cd24ead143a7e16fb73c3bb3c5d693d037add
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-07-12 18:00:55 +0900

    Day 46
    finally solved BAEKJOON 11000
    
    to solve the timeout problem I have changed my logic
    In worst case (for example 200000 same inpust with value 1, 2000)
    my previous logic searched all existing class whether new lecture can fit in. This leads to O(n!)
    So my aim was to search only once per one lecture input. To do this, you should compare whith fastest ending class. Because, if it doesn't fit to fastest ending class, it would not fit in to any classes.
    So instead of storing classes to vector, I stored them to multiset. Since multisets are automatically sorted when data is saved. New input data(lecture info) only needed to compare with multiset's front data
    
    *multiset vs set
    -They are sorted automatically when data is saved
    -To use this both need same header
    #include <set>
    -Difference of them is multiset allows multiple(duplicate) instances (or keys)

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -2,42 +2,40 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <set>
 using namespace std;
 
 
 bool compare(const pair<int, int>& a, const pair <int, int>& b) {
-	return a.first < b.first;
+	//if(a.first!=b.first)
+		return a.first < b.first;
+	//return a.second < b.second;
 }
 
 int main() {
 	int N; // 1 <= N <= 200,000
-	scanf ("%d", &N);
+	scanf("%d", &N);
 
 	vector <pair <int, int>> arr;
 	int S, T;
 	for (int i = 0; i < N; i++) {
 		scanf("%d %d", &S, &T);
-		arr.push_back(pair<int , int>(S, T));
+		arr.push_back(pair<int, int>(S, T));
 	}
 	sort(arr.begin(), arr.end(), compare);
 	// before assigning lecture to class room, sort list of lecture by it's 'S' (start time)
 	// using Sort
-
-	int result = 0; // how many class?
-	int check = 1; // when the previous class is over
 	
-	while (!arr.empty()) {
-		for (int i = 0; i < arr.size(); i++) {	
-			if (arr[i].first >= check) {
-				
-				check = arr[i].second;
-				arr.erase(arr.begin() + i);
-				i--;
-			}		
+	multiset<int> class_end_time; //to check when lecture was over per classes
+	class_end_time.insert(arr[0].second);
+	for (int i = 1; i < arr.size(); i++) {
+		if (arr[i].first >= *class_end_time.begin()) { //if fastest starting lecture starts after fastest ending class end
+			class_end_time.erase(class_end_time.begin());
+			class_end_time.insert(arr[i].second);
 		}
-		result++;
-		check = 1;
+		else
+			class_end_time.insert(arr[i].second);
 	}
-
-	printf("%d", result);
+	
+	printf("%d", class_end_time.size());
 }
