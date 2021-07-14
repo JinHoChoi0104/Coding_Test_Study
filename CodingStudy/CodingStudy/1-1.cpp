6f40d9ba81c1b5d901ef8789e7cf65c4bfb00f9d
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-07-14 20:47:27 +0900

    Day 48
    BAEKJOON 18404
    solving this problem learned a lot
    
    1. structure vs class
    Both are used for gathering related data together. Difference is accessibility to data. Structure's default is public and class is private.
    So mostly in coding test, using structre might be better
    
    2. new ways of moving
    There are a lot of moving problem in coding test. by using array you can  make your cord shorter
    
    3. C is very fast
    So, you might be able to get some answer before other input comes

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,367 +1,69 @@
-// BAEKJOON 13460
-#include <iostream>
-#include <string>
-#include <queue>
+// BAEKJOON 18404
+#include<iostream>
+#include<vector>
+#include<queue>
 using namespace std;
 
-class ball_state {
-public:
-	int R[2]; //[y][x]
-	int B[2];
+struct knight {int x, y, n;};
+struct enemy {int x, y, n;};
+int N, M, cnt = 0;
+int board[501][501]; //no 0
+// -1 have been before, 1 never have been before , 2 enemy
+//vector <pair<int, int>> enemy; //[x][y], x is vertical
+vector <enemy> enemy_arr;
+queue <knight> knight_arr;
 
-	int n;//how many moves have done?
-
-	//from where?
-	bool right = false;
-	bool left = false;
-	bool up = false;
-	bool down = false;
-
-	ball_state(int RY, int RX, int BY, int BX, bool _right, bool _left, bool _up, bool _down, int _n) {
-		R[0] = RY;
-		R[1] = RX;
-		B[0] = BY;
-		B[1] = BX;
-
-		n = _n;
-
-		right = _right;
-		left = _left;
-		up = _up;
-		down = _down;
-	}
-};
-
-bool board[10][10];
-int M = 0, N = 0; // board size
-int OY = 0, OX = 0;
-queue <ball_state> arr;
-
-bool goRight(int RY, int RX, int BY, int BX, int n ) {
-	int pre_RY = RY; //previous position
-	int pre_RX = RX;
-	int pre_BY = BY;
-	int pre_BX = BX;
-
-	//check if R and B are in same horizontal state
-	if (RY == BY ) {
-		//if so move rightest ball first and then move another ball
-		if (RX > BX) {
-			while (board[RY][RX + 1]) {
-				RX++;
-				if (RY == OY && RX == OX)
-					break;
-			}
-			if(RY != OY || RX != OX)
-				board[RY][RX] = false;
-			while (board[BY][BX + 1]) {
-				BX++;
-				if (BY == OY && BX == OX)
-					break;
-			}
-			board[RY][RX] = true;
-		}
-		else {
-			while (board[BY][BX + 1]) {
-				BX++;
-				if (BY == OY && BX == OX)
-					return false;
-			}
-			board[BY][BX] = false;
-			while (board[RY][RX + 1]) {
-				RX++;
-				if (RY == OY && RX == OX)
-					return true;
-			}
-			board[BY][BX] = true;
+void killEnemy(int x, int y, int n) {
+	for (int i = 0; i < M; i++) {
+		if (enemy_arr[i].x == x && enemy_arr[i].y == y) {
+			cnt++;
+			enemy_arr[i].n = n;
+			break;
 		}
 	}
-	else { //else just move
-		while (board[RY][RX + 1]) {
-			RX++;
-			if (RY == OY && RX == OX)
-				return true;
-		}
-		while (board[BY][BX + 1]) {
-			BX++;
-			if (BY == OY && BX == OX)
-				return false;
-		}
-	}
-	
-
-	//case 1: no movement -> no push
-	//case 2: 1 movement 
-	//case 2-a: R goal in ->alert
-	//case 2-b: B goal in -> no push
-	//case 3: 2 movement
-	//case 3-a : R goal in ->alert 
-	//case 3-b : B goal in -> no push
-	//case 3-c: R and B boath goal in -> no push
-	//save ball state to queue
-
-	if (BY == OY && BX == OX);//no push
-	else if (RY == OY && RX == OX)
-		return true;
-	else if(n < 9)
-		if (pre_RY != RY || pre_RX != RX || pre_BY != BY || pre_BX != BX) // if no move has done, no push
-			arr.push(ball_state(RY, RX, BY, BX, true, false, false, false, n + 1));
-	return false;
 }
+int K1[] = { -2,-2,-1,-1,1,1,2,2 }, K2[] = { -1,1,-2,2,-2,2,-1,1 };
+void moveKnight() { //doing bfs
+	int x = 0, y = 0, n = 0;
+	while (!knight_arr.empty()) {
+		x = knight_arr.front().x;
+		y = knight_arr.front().y;	
+		n = knight_arr.front().n;
+		knight_arr.pop();
 
-bool goLeft(int RY, int RX, int BY, int BX, int n) {
-	int pre_RY = RY;
-	int pre_RX = RX;
-	int pre_BY = BY;
-	int pre_BX = BX;
-	
-	if (RY == BY) {
-		if (RX < BX) {
-			while (board[RY][RX - 1]) {
-				RX--;
-				if (RY == OY && RX == OX)
-					break;
-			}
-			if (RY != OY || RX != OX) {
-				board[RY][RX] = false;
-			}
-			while (board[BY][BX - 1]) {
-				BX--;
-				if (BY == OY && BX == OX)
-					return false;
-			}
-			board[RY][RX] = true;
-		}
-		else {
-			while (board[BY][BX - 1]) {
-				BX--;
-				if (BY == OY && BX == OX)
-					return false;
-			}
-			board[BY][BX] = false;
-			while (board[RY][RX - 1]) {
-				RX--;
-				if (RY == OY && RX == OX)
-					return true;
-			}
-			board[BY][BX] = true;
-		}
-	}
-	else {
-		while (board[RY][RX - 1]) {
-			RX--;
-			if (RY == OY && RX == OX)
-				break;
-		}
-		while (board[BY][BX - 1]) {
-			BX--;
-			if (BY == OY && BX == OX)
-				break;
-		}
-	}
+		if (x > 0 && x < N + 1 && y > 0 && y < N + 1) { // is in range?
+			if (board[x][y] > 0) { //is never have been before
+				if (board[x][y] == 2) 
+					killEnemy(x, y, n);
 
-	if (BY == OY && BX == OX);//no push
-	else if (RY == OY && RX == OX) 
-		return true;
-	else if (n < 9)
-		if (pre_RY != RY || pre_RX != RX || pre_BY != BY || pre_BX != BX)
-			arr.push(ball_state(RY, RX, BY, BX, false, true, false, false, n + 1));
-	return false;
-}
-
-
-bool goUp(int RY, int RX, int BY, int BX, int n) {
-	int pre_RY = RY;
-	int pre_RX = RX;
-	int pre_BY = BY;
-	int pre_BX = BX;
-
-	if (RX == BX) {
-		if (RY < BY) {
-			while (board[RY - 1][RX]) {
-				RY--;
-				if (RY == OY && RX == OX)
-					break;
-			}
-			if (RY != OY || RX != OX)
-				board[RY][RX] = false;
-			while (board[BY - 1][BX]) {
-				BY--;
-				if (BY == OY && BX == OX)
-					return false;
-			}
-			board[RY][RX] = true;
-		}
-		else {
-			while (board[BY - 1][BX]) {
-				BY--;
-				if (BY == OY && BX == OX)
-					return false;
-			}
-			board[BY][BX] = false;
-			while (board[RY - 1][RX]) {
-				RY--;
-				if (RY == OY && RX == OX)
-					break;
-			}
-			board[BY][BX] = true;
-		}
-	}
-	else {
-		while (board[RY - 1][RX]) {
-			RY--;
-			if (RY == OY && RX == OX)
-				break;
-		}
-		while (board[BY - 1][BX]) {
-			BY--;
-			if (BY == OY && BX == OX)
-				break;
-		}
-	}
-
-	if (BY == OY && BX == OX); 
-	else if (RY == OY && RX == OX) 
-		return true;
-	else if (n < 9) 
-		if (pre_RY != RY || pre_RX != RX || pre_BY != BY || pre_BX != BX) 
-			arr.push(ball_state(RY, RX, BY, BX, false, false, true, false, n + 1));
-	
-	return false;
-}
-
-bool goDown(int RY, int RX, int BY, int BX, int n) {
-	int pre_RY = RY;
-	int pre_RX = RX;
-	int pre_BY = BY;
-	int pre_BX = BX;
-
-	if (RX == BX) {
-		if (RY > BY) {
-			while (board[RY + 1][RX]) {
-				RY++;
-				if (RY == OY && RX == OX)
-					break;
-			}
-			if (RY != OY || RX != OX)
-				board[RY][RX] = false;
-			while (board[BY + 1][BX]) {
-				BY++;
-				if (BY == OY && BX == OX)
-					return false;
-			}
-			board[RY][RX] = true;
-		}
-		else {
-			while (board[BY + 1][BX]) {
-				BY++;
-				if (BY == OY && BX == OX)
-					return false;
-			}
-			board[BY][BX] = false;
-			while (board[RY + 1][RX]) {
-				RY++;
-				if (RY == OY && RX == OX)
-					break;
-			}
-			board[BY][BX] = true;
-		}
-	}
-	else {
-		while (board[RY + 1][RX]) {
-			RY++;
-			if (RY == OY && RX == OX)
-				break;
-		}
-		while (board[BY + 1][BX]) {
-			BY++;
-			if (BY == OY && BX == OX)
-				break;
-		}
-	}
-
-	if (BY == OY && BX == OX); //no push
-	else if (RY == OY && RX == OX) 
-		return true;
-	else if (n < 9) 
-		if (pre_RY != RY || pre_RX != RX || pre_BY != BY || pre_BX != BX) 
-			arr.push(ball_state(RY, RX, BY, BX, false, false, false, true, n + 1));
-	
-	return false;
-}
-
-void BFS(){
-	ball_state ball = arr.front();
-	int num = -1;
-	while (!arr.empty()) {
-		ball = arr.front();
-		arr.pop();
-		if (!ball.left) { //if ball went from left you don't have to go to right
-			if (goRight(ball.R[0], ball.R[1], ball.B[0], ball.B[1], ball.n)) {
-				num = ball.n + 1;
-				break;
+				board[x][y] = -1;
+				for(int i = 0; i < 8; i++)
+					knight_arr.push(knight{ x + K1[i], y + K2[i], n + 1});
 			}
 		}
-		if (!ball.right) {
-			if (goLeft(ball.R[0], ball.R[1], ball.B[0], ball.B[1], ball.n)) {
-				num = ball.n + 1;
-				break;
-			}
-		}
-		if (!ball.up) {
-			if (goDown(ball.R[0], ball.R[1], ball.B[0], ball.B[1], ball.n)) {
-				num = ball.n + 1;
-				break;
-			}
-		}
-		if (!ball.down) {
-			if (goUp(ball.R[0], ball.R[1], ball.B[0], ball.B[1], ball.n)) {
-				num = ball.n + 1;
-				break;
-			}
-		}
-		if (ball.n == 10)
+		if (cnt == M) 
 			break;
 	}
-	cout << num;
 }
 
 int main(void) {
-	scanf("%d %d", &N, &M); //[N][M]
-	char c;
-	
-	int B[2];
-	int R[2];
-	char in_board[10][10];
-	for (int i = 0; i < N; i++) 
-		for (int j = 0; j < M; j++) 
-			cin >> in_board[i][j];
-		
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			if (in_board[i][j] == '#')
-				board[i][j] = false;
-			else {
-				board[i][j] = true;
-				if (in_board[i][j] == 'B') {
-					B[0] = i;
-					B[1] = j;
-				}
-				else if (in_board[i][j] == 'R') {
-					R[0] = i;
-					R[1] = j;
-				}
-				else if (in_board[i][j] == 'O') {
-					OY = i;
-					OX = j;
-				}
-			}
-		}
+	int x = 0, y = 0;
+	cin >> N >> M >> x >> y;
+	knight_arr.push(knight{ x, y, 0 });
+
+	for (int i = 1; i <= N; i++) 
+		for (int j = 1; j <= N; j++)
+			board[i][j] = 1;
+
+	for (int i = 0; i < M; i++) {
+		scanf("%d %d", &x, &y);
+		board[x][y] = 2;
+		enemy_arr.push_back({ x,y,0 });
 	}
 
-	arr.push(ball_state(R[0], R[1], B[0], B[1], false, false, false, false,0));
-	
-	BFS();
+	moveKnight();
 
+	for (int i = 0; i < M; i++)
+		printf("%d ",enemy_arr[i].n);
 	return 0;
 }
