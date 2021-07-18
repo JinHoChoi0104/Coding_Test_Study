3f14b2425a556c4a3df8fcdb34a44fe7ced71a97
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-07-18 14:24:38 +0900

    Day 52
    BAEKJOON 11053
    LIS problem
    reference book: '프로그래밍 대회에서 배우는 알고리즘 문제 해결 전략'
    This code has n^2 complexity but there is way to make n*logn complexity. I should look up for that

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -2,24 +2,34 @@
 #include<algorithm>
 using namespace std;
 // Sutdying LIS (Longest Increasing Subsequence)
-
+// Studying with book '프로그래밍 대회에서 배우는 알고리즘 문제 해결 전략'
 int n;
-int cache[100], S[100];
+int cache[1000], S[1000];
 
-// S[start]에서 시작하는 증가 부분 수열 중 최대 길이를 반환한다.
-int list(int start) {
+// return LIS starts from S[start] (and this includes S[start])
+int lis2(int start) {
 	int& ret = cache[start];
-	if (ret != -1) return ret;
-	//항상 S[start]는 있기 때문에 길이는 최하 1
+	if (ret != -1) return ret; //if LIS for start has been answered immediately return
+
+	// since S[start] is always at there, LIS should be at least 1
 	ret = 1;
-	for (int next = start + 1; next < n; ++next) {
+	for (int next = start + 1; next < n; ++next) 
 		if (S[start] < S[next])
 			ret = max(ret, lis2(next) + 1);
-		return ret;
-	}
+	
+	return ret;
 }
 
 int main(void) {
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &S[i]);
+		cache[i] = -1;
+	}
+	int max_lis = 0;
+	for (int i = 0; i < n; i++) 
+		max_lis = max(max_lis, lis2(i));
 
+	cout << max_lis;
 	return 0;
 }
