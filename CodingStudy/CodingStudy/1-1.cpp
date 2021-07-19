d8605466b7f5709d594391c85ae7f0a93af4998f
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-07-19 17:11:15 +0900

    Day 53
    BAEKJOON 21968
    *pow
    STL has funciton pow, and it's return value is dobule. Since this problem had to deal with big number. I made new funciton longPow to return pow in long value

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,75 +1,48 @@
 #include<iostream>
-#include<algorithm>
 using namespace std;
 
-int n, max_locate = 0;//board size
-int p, q;
-bool canLocate(bool board[][10]) {
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			if (board[i][j])
-				return true;
-	return false;
+long long longPow(int n, int k) {
+	long long num = 1;
+	for (int i = 0; i < k; i++)
+		num *= n;
+	return num;
 }
 
-int m0[4] = { 1, 1, -1, -1 }, m1[4] = { 1,-1 ,-1,1 };
-void locateBishop(int x, int y, bool arr[][10], int cnt) {
-	bool board[10][10];
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			board[i][j] = arr[i][j];
+// 3^0 + 3^1 + 3^2 + ... + 3^(n-1) < 3^n
 
-	board[x][y] = false;
-	for (int i = 0; i < 4; i++) { //lock diagonal
-		p = x + m0[i], q = y + m1[i];
-		while (p > -1 && p < n && q > -1 && q < n) {
-			board[p][q] = false;
-			p += m0[i], q += m1[i];
+// numbers of SeanRin under 3^n are 2^(n-1) - 1 (because 0 is not SeanRin)
+// it means 3^n are (2^n)th SeanRin
+
+// if ( 2^(n-1) < N <= 2^n )
+// then N include 3^n
+
+long long GetSeanRin(long long n) {
+	long long sum = 0;//SeanRin
+	int k = 0;
+	while (n > 0) {
+		if (n == longPow(2, k)) {
+			sum += longPow(3, k);
+			n -= longPow(2, k);
+		}
+		else if (n > longPow(2, k))
+			k++;
+		else {
+			sum += longPow(3, k - 1);
+			n -= longPow(2, k - 1);
+			k = 0;
 		}
 	}
-
-	if (!canLocate(board)) // if cant locate any more
-		max_locate = max(max_locate, cnt + 1);
-	else { // can locate more bishop
-		for (int i = 0; i < n; i++)
-			for (int j = 0; j < n; j++)
-				if (board[i][j]) {
-					p = i, q = j;
-					while (p < n && q > -1) {
-						if (board[p][q])
-							locateBishop(p, q, board, cnt+1);
-						p++, q--;
-					}
-					i = n, j = n;
-				}
-	}
+	return sum;
 }
 
 int main(void) {
-	bool board[10][10]; //true: can go , false: can't go
-	cin >> n;
-	int x;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++) {
-			cin >> x;
-			if (x)
-				board[i][j] = true;
-			else
-				board[i][j] = false;
-		}
+	int T;
+	cin >> T;
 
-
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			if (board[i][j]) {
-				p = i, q = j;
-				while (p < n &&q > -1) {
-					if (board[p][q]) 
-						locateBishop(p, q, board, 0);
-					p++, q--;
-				}
-				i = n, j = n;
-			}
-
-	cout << max_locate;
+	long long n;
+	for (int i = 0; i < T; i++) {
+		cin >> n;
+		printf("%lld\n", GetSeanRin(n));
+	}
+	return 0;
 }
