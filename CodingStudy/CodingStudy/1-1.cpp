2ee30b7f5ed27f73e4252398e70fc06cb2b1a6c3
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-07-20 19:54:07 +0900

    Day 54
    BAEKJOON 22232
    This problem took a long time. But, learned a lot!!
    
    1. never use 'printf' and 'scanf' with
    'ios_base::sync_with_stdio(false)'
    
    to get string I had to use 'cin', and added'ios_base::sync_with_stdio(false)' to reduce time in cin and cout.
    Also instead of using 'endl' (because it takes very long time) I had to used '\n'. So I also used 'printf'.
    And this had occured problem.
    
    2. 'map' vs 'vector'
    I am not sure but find() run faster when data is stored on map.
    
    I don't know why but when data was stored on vector, It has occured timeout problem. But just by changing vector to map, it had been solved.
    ( Maybe because map is sorted automatically when new data is saved to it. But still even I had done sort() on vector and then used find(). It still occured timeout)
    
    3. new code I used
    - string function....
    int index = str.find('.'); //reture index of '.'
    
    - map
    string str;
    cin >> str;
    m[str]++;
    map <string, int>
    if (m.find(arr[i].e) != m.end()) ; //if e exitst in m

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,14 +1,49 @@
 #include<iostream>
 #include<string>
+#include<vector>
+#include<map>
+#include<algorithm>
 using namespace std;
 
+struct jo { 
+	string f, e; 
+	int OS; // If OS doesn't recognize extension, In other words if e is not included in m 
+};
+
+vector <jo > arr; //filename
+map <string, int>m; //extension
+
+bool compare(const jo& a, const jo& b) {
+	if (a.f != b.f)	//if file names are different
+		return a.f < b.f;
+	else if (a.OS != b.OS) 
+		return a.OS > b.OS;
+	else 
+		return a.e < b.e;
+}
 int main(void) {
-	int N;
-	cin >> N;
+	int N, M, index;
+	string str;
+	scanf("%d%d", &N, &M);
+	for (int i = 0; i < N; i++) {
+		cin >> str;
+		index = str.find('.');
+		arr.push_back({ str.substr(0, index), str.substr(index + 1), 0 });
+	}
+	for (int i = 0; i < M; i++) {
+		cin >> str;
+		m[str]++;
+	}
 
-	string arr;
-	cin >> arr;
-
-	cout << arr.substr(N - 5);
+	for (int i = 0; i < N; i++)	
+		if (m.find(arr[i].e) != m.end()) //if e exitst in m
+			arr[i].OS = 1;
+	
+	sort(arr.begin(), arr.end(), compare);
+	
+	for (int i = 0; i < N; i++) {
+		cout << arr[i].f << "." << arr[i].e;
+		printf("\n");
+	}
 	return 0;
 }
