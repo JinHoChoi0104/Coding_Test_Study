6196a5b46ed5f20050c1324c981539cfbb4c2eb5
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-07-22 11:00:05 +0900

    Day 56 - 1
    BAEKJOON 7562
    While running BFS, check whether heading position is right before push in to queue. It's much faster than check it when it's pop

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,49 +1,59 @@
 #include<iostream>
 #include<queue>
-#include<string>
+
 using namespace std;
 
-int main(void) {
-	std::ios_base::sync_with_stdio(false);
-	std::cin.tie(NULL);
-	std::cout.tie(NULL);
+struct night {	int x, y, cnt, from;};
+bool board[300][300]; //true: never been before so can go
+int L; //board size
+int m[2][8] = { { -1,-2,-2,-1,1,2,2,1 } ,{ -2,-1,1,2,2,1,-1,-2 } };
+void clearBoard() {
+	for (int i = 0; i < L; i++)
+		for (int j = 0; j < L; j++)
+			board[i][j] = true;
+}
 
-	int N, num = 0;
-	cin >> N;
+int BFS(int fromX, int fromY, int toX, int toY) {
+	queue <night> q;
+	q.push({ fromX, fromY, 0, 8 });
+	int x, y, cnt, from;
+	while (1) {
+		x = q.front().x;
+		y = q.front().y;
+		from = q.front().from;
+		from = from >= 4 ? from - 4 : from + 4;
+		for (int i = 0; i < 8; i++) {
+			if (from != 8) {
+				x = q.front().x + m[0][i];
+				y = q.front().y + m[1][i];
 
-	string str;
-	queue <int> q;
+				if (x > -1 && x < L && y > -1 && y < L) { //in range?
+					if (board[x][y]) { //never have been?
+						cnt = q.front().cnt + 1;
+						if (x == toX && y == toY) 
+							return cnt;
 
-	for (int i = 0; i < N; i++) {
-		cin >> str;
-		if (str == "pop") {
-			if (q.empty())
-				cout << "-1\n";
-			else {
-				cout << q.front() << "\n";
-				q.pop();
+						board[x][y] = false;
+						q.push({ x, y, cnt, i });
+					}
+				}
 			}
 		}
-		else if (str == "size")
-			cout << q.size() << "\n";
-		else if (str == "empty")
-			if (q.empty())
-				cout << "1\n";
-			else
-				cout << "0\n";
-		else if (str == "front")
-			if (q.empty())
-				cout << "-1\n";
-			else
-				cout << q.front() << "\n";
-		else if (str == "back")
-			if (q.empty())
-				cout << "-1\n" ;
-			else
-				cout << q.back() << "\n";
-		else if (str == "push") {
-			cin >> num;
-			q.push(num);
+		q.pop();
+	}
+}
+
+int main(void) {
+	int T;
+	scanf("%d", &T);
+	int fromX, fromY, toX, toY;
+	while (T--) {
+		scanf("%d %d %d %d %d", &L, &fromX, &fromY, &toX, &toY);
+		if (fromX == toX && fromY == toY)
+			cout << "0\n";
+		else {
+			clearBoard();
+			cout << BFS(fromX, fromY, toX, toY) << "\n";
 		}
 	}
 	return 0;
