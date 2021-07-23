bf725e6bfe71d44cd1b87e51bce4cdd1df6996e4
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-07-23 14:49:56 +0900

    Day 57
    BAEKJOON 1707
    solved with DFS

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,59 +1,80 @@
 #include<iostream>
+#include<algorithm>
+#include<vector>
 #include<queue>
-
 using namespace std;
 
-struct night {	int x, y, cnt, from;};
-bool board[300][300]; //true: never been before so can go
-int L; //board size
-int m[2][8] = { { -1,-2,-2,-1,1,2,2,1 } ,{ -2,-1,1,2,2,1,-1,-2 } };
-void clearBoard() {
-	for (int i = 0; i < L; i++)
-		for (int j = 0; j < L; j++)
-			board[i][j] = true;
+int V, E;
+int v[20001];
+queue<int>q[20001];
+
+bool DFS(int n, int index) {
+	v[n] = index;
+	E--;
+
+	index *= -1;
+	bool isBiG = true;
+	int to;
+	while (!q[n].empty() && isBiG) {
+		to = q[n].front();
+		q[n].pop();
+		if (v[to] == index * -1) {
+			cout << "NO\n";
+			return false;
+		}
+		isBiG = DFS(to, index);;
+	}
+	return isBiG;
 }
 
-int BFS(int fromX, int fromY, int toX, int toY) {
-	queue <night> q;
-	q.push({ fromX, fromY, 0, 8 });
-	int x, y, cnt, from;
-	while (1) {
-		x = q.front().x;
-		y = q.front().y;
-		from = q.front().from;
-		from = from >= 4 ? from - 4 : from + 4;
-		for (int i = 0; i < 8; i++) {
-			if (from != 8) {
-				x = q.front().x + m[0][i];
-				y = q.front().y + m[1][i];
-
-				if (x > -1 && x < L && y > -1 && y < L) { //in range?
-					if (board[x][y]) { //never have been?
-						cnt = q.front().cnt + 1;
-						if (x == toX && y == toY) 
-							return cnt;
-
-						board[x][y] = false;
-						q.push({ x, y, cnt, i });
-					}
-				}
-			}
-		}
-		q.pop();
+void reset() {
+	for (int i = 1; i <= V; i++) {
+		v[i] = 0;
+		while (!q[i].empty())
+			q[i].pop();
 	}
 }
 
 int main(void) {
-	int T;
-	scanf("%d", &T);
-	int fromX, fromY, toX, toY;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	bool isBiG;
+	int T, a, b, index, to;
+	cin >> T;
 	while (T--) {
-		scanf("%d %d %d %d %d", &L, &fromX, &fromY, &toX, &toY);
-		if (fromX == toX && fromY == toY)
-			cout << "0\n";
-		else {
-			clearBoard();
-			cout << BFS(fromX, fromY, toX, toY) << "\n";
+		cin >> V >> E;
+		reset();
+		isBiG = true;
+		
+		for (int i = 0; i < E; i++) {
+			cin >> a >> b;
+			q[a].push(b);
+			q[b].push(a);
+		}
+		E *= 2;
+		for (int i = 1; i <= V; i++) {
+			while (!q[i].empty() && isBiG) {
+				to = q[i].front();
+				q[i].pop();
+
+				index = v[i] == 1 ? 1 : -1;
+				v[i] = index;
+				index *= -1;
+				if (v[to] == index * -1) {
+					cout << "NO\n";
+					isBiG = false;
+					break;
+				}
+				isBiG = DFS(to, index);
+			}
+			if (!isBiG)
+				break;
+			else if (E == 0) {
+				cout << "YES\n";
+				break;
+			}
 		}
 	}
 	return 0;
