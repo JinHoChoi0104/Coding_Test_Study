931f4a7210dc9e48119356c46165e8e4e825de52
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-07-25 18:18:48 +0900

    Day 59
    BAEKJOON 11866

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,22 +1,25 @@
 #include<iostream>
 #include<queue>
+
 using namespace std;
 
-queue<int>q;
 int main(void) {
-	int N, num;
-	scanf("%d", &N);
-	
-	for (int i = 1; i <= N; i++) 
+	int N, K;
+	scanf("%d%d", &N, &K);
+	queue<int>q;
+	for (int i = 1; i <= N; i++)
 		q.push(i);
-	
-	while (q.size()!=1) {
-		q.pop();
-		num = q.front();
-		q.pop();
-		q.push(num);
-	}
 
-	printf("%d", q.front());
+	printf("<", q.front());
+	while (q.size()!=1) {
+		for (int i = 1; i < K; i++) {
+			q.push(q.front());
+			q.pop();
+		}
+		printf("%d, ", q.front());
+		q.pop();
+	}
+	printf("%d>", q.front());
+
 	return 0;
 }
