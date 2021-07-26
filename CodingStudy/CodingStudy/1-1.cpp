27b80899b9a878b2e27aad938d1a63e2b0ae5aff
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-07-26 19:12:40 +0900

    Day 60 - 1
    BAEKJOON 14502
    by using
    freopen("input.txt", "r", stdin);
    you can skip entering input
    and this will save your time

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,71 +1,75 @@
 #include<iostream>
+#include<algorithm>
 #include<vector>
 #include<queue>
-#include<list>
 using namespace std;
 
-int N; //size of board
-vector<vector<int>> board(101, vector<int>(101, 0)); //-1 bame, 0 nothing, 1: apple
-queue<pair<int, int>> rout; //cnt, direction
-list<pair<int, int>> baem; //save baem's position
+int N, M, max_air, min_virus = 64; //size of map
 int m[2][4] = { {-1, 0 ,1, 0}, {0, 1, 0, -1} }; // up, right, down, left
 
-void checkR(int cnt, int& dir) { //check whether if has to turn
-	if (cnt == rout.front().first) {
-		dir += rout.front().second;
-		if (dir == 4)
-			dir = 0;
-		else if (dir == -1)
-			dir = 3;
-		rout.pop();
+vector<vector<int>> map(8, vector<int>(8)); //0: air, 1: wall, 2: virus
+vector<pair<int, int>> air;
+
+struct virus { int x, y; }; // store original virus's position
+queue <virus> virusq;
+
+vector<vector<int>> tmp_map; //temporary container for BFS
+queue <virus> tmp_q;
+
+void BFS() {
+	tmp_map = map;
+	tmp_q = virusq;
+	int x, y, tox, toy, cnt = 0;
+	while (!tmp_q.empty() && cnt <= min_virus && cnt != max_air) {//if virus visit more than before, or polluted all air then stop!!!
+		x = tmp_q.front().x, y = tmp_q.front().y;
+		tmp_q.pop();
+		
+		for (int i = 0; i < 4; i++) {
+			tox = x + m[0][i], toy = y + m[1][i];
+			if (tox > -1 && tox < N && toy > -1 && toy < M) //check range
+				if (tmp_map[tox][toy] == 0) { //check whether it is air
+					tmp_q.push({ tox,toy });
+					tmp_map[tox][toy] = 2; //visit
+					cnt++;
+				}
+		}
 	}
-}
 
-void dummy(int &cnt, int dir) {
-	if (!rout.empty()) 
-		checkR(cnt, dir);
-
-	cnt++;
-	int x = baem.front().first + m[0][dir];
-	int y = baem.front().second + m[1][dir];
-
-	if (x == 0 || y == 0 || x == N + 1 || y == N + 1 ) // if not in range 
-		return;
-	
-	if (board[x][y] == 0) { // if no apple
-		board[baem.back().first][baem.back().second] = 0;
-		baem.pop_back();
-	}
-
-	if (board[x][y] == -1) // if it's head meet baem
-		return;
-	
-	board[x][y] = -1; // beam has moved!!
-	baem.push_front(make_pair(x, y));
-
-	dummy(cnt, dir);
+	min_virus = min(min_virus, cnt);
 }
 
 int main(void) {
-	int K, L, x, y, cnt, time = 0;
-	scanf("%d%d", &N, &K);
-	for (int i = 0; i < K; i++) {
-		scanf("%d%d", &x, &y);
-		board[x][y] = 1;
+	//freopen("input.txt", "r", stdin);
+	scanf("%d%d", &N, &M);
+	int num;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			scanf("%d", &num);
+			map[i][j] = num;
+			if (num == 0) 
+				air.push_back(make_pair(i, j));
+			else if (num == 2)
+				virusq.push({ i,j });
+		}
 	}
-	board[1][1] = -1; // put baem
-	baem.push_back(make_pair(1, 1));
-	char d;
-	scanf("%d", &L);
-	for (int i = 0; i < L; i++) {
-		scanf("%d %c", &cnt, &d);
-		if (d == 'D')
-			rout.push(make_pair(cnt, 1)); // == push(pair<int,int> (l, 2));
-		else
-			rout.push(make_pair(cnt, -1));
+
+	max_air = air.size();
+
+	for (int i = 0; i < max_air - 2; i++) {
+		map[air[i].first][air[i].second] = 1;
+		for (int j = i + 1; j < max_air - 1; j++) {
+			map[air[j].first][air[j].second] = 1;
+			for (int k = j + 1; k < max_air; k++) {
+				map[air[k].first][air[k].second] = 1;
+				BFS();
+				map[air[k].first][air[k].second] = 0;
+			}
+			map[air[j].first][air[j].second] = 0;
+		}
+		map[air[i].first][air[i].second] = 0;
+		
 	}
-	
-	dummy(time, 1);
-	printf("%d", time);
+
+	printf("%d", max_air - min_virus - 3);
 	return 0;
 }
