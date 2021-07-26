c6ee8cb5cec5061a288d747c2737ff69eadbe0cf
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-07-26 16:59:04 +0900

    Day 60
    BAEKJOON 3190
    1. Initializing 2 Dimensional Array
    int m[2][4] = { {-1, 0 ,1, 0}, {0, 1, 0, -1} };
    (It's not () inside)
    
    2. Link container
    - since it's one of sequence container it has order
    - when searching for data instead of using at(), or []. Uses ++, --
    - push_front(), push_back(), pop_front(), pop_back()
    - can also use insert(), erase()
    
    3. pair
    queue <pair<int, int>> q;
    q.push(make_pair(1, 2));  == q.push(pair<int,int> (1, 2));
    
    4. when using container like vector, queue, list, etc...
    careful wtih case if it is empty

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,45 +1,71 @@
 #include<iostream>
 #include<vector>
-
+#include<queue>
+#include<list>
 using namespace std;
 
-vector < vector<bool> > video(64, vector<bool>(64, false));
+int N; //size of board
+vector<vector<int>> board(101, vector<int>(101, 0)); //-1 bame, 0 nothing, 1: apple
+queue<pair<int, int>> rout; //cnt, direction
+list<pair<int, int>> baem; //save baem's position
+int m[2][4] = { {-1, 0 ,1, 0}, {0, 1, 0, -1} }; // up, right, down, left
 
-void QuadTree(int x, int y, int size) {
-	bool color = video[x][y];
-	if (size != 1) {
-		for (int i = x; i < x + size; i++) {
-			for (int j = y; j < y + size; j++) {
-				if (color != video[i][j]) {
-					printf("(");
-					QuadTree(x, y, size / 2);
-					QuadTree(x, y + size / 2, size / 2);
-					QuadTree(x + size / 2, y, size / 2);
-					QuadTree(x + size / 2, y + size / 2, size / 2);
-					printf(")");
-					return;
-				}
-			}
-		}
+void checkR(int cnt, int& dir) { //check whether if has to turn
+	if (cnt == rout.front().first) {
+		dir += rout.front().second;
+		if (dir == 4)
+			dir = 0;
+		else if (dir == -1)
+			dir = 3;
+		rout.pop();
+	}
+}
+
+void dummy(int &cnt, int dir) {
+	if (!rout.empty()) 
+		checkR(cnt, dir);
+
+	cnt++;
+	int x = baem.front().first + m[0][dir];
+	int y = baem.front().second + m[1][dir];
+
+	if (x == 0 || y == 0 || x == N + 1 || y == N + 1 ) // if not in range 
+		return;
+	
+	if (board[x][y] == 0) { // if no apple
+		board[baem.back().first][baem.back().second] = 0;
+		baem.pop_back();
 	}
 
-	if (color)
-		printf("1");
-	else
-		printf("0");
+	if (board[x][y] == -1) // if it's head meet baem
+		return;
+	
+	board[x][y] = -1; // beam has moved!!
+	baem.push_front(make_pair(x, y));
+
+	dummy(cnt, dir);
 }
 
 int main(void) {
-	int N, num;
-	scanf("%d", &N);
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			scanf("%1d", &num);
-			if (num == 1)
-				video[i][j] = true;
-		}
+	int K, L, x, y, cnt, time = 0;
+	scanf("%d%d", &N, &K);
+	for (int i = 0; i < K; i++) {
+		scanf("%d%d", &x, &y);
+		board[x][y] = 1;
+	}
+	board[1][1] = -1; // put baem
+	baem.push_back(make_pair(1, 1));
+	char d;
+	scanf("%d", &L);
+	for (int i = 0; i < L; i++) {
+		scanf("%d %c", &cnt, &d);
+		if (d == 'D')
+			rout.push(make_pair(cnt, 1)); // == push(pair<int,int> (l, 2));
+		else
+			rout.push(make_pair(cnt, -1));
 	}
 	
-	QuadTree(0, 0, N);
+	dummy(time, 1);
+	printf("%d", time);
 	return 0;
 }
