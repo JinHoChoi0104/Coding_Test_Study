70837ded8ba8950d55eb44b2c034c3fab4b35e59
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-07-29 10:49:46 +0900

    Day 63
    BAEKJOON 9251
    
    LCS (Longest Common Subsequence) problem
    *reference:
    https://twinw.tistory.com/126
    
    str.at[index] return char of string's index
    
    Should more study dealing string

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,36 +1,46 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
-
+#include<string>
 using namespace std;
 
-int N;
-vector < int > a(1000);
-vector < int > l; //save lds
-
-void lds() { // simply change direction of LIS
-	int index = 0;
-	l.push_back(a[N-1]);
-
-	for (int i = N-2; i >=0 ; i--) {
-		if (l[index] < a[i]) {
-			l.push_back(a[i]);
-			index++;
-		}
-		else 
-			l[lower_bound(l.begin(), l.end(), a[i]) - l.begin()] = a[i];
-	}
-	
-	printf("%d", index+1);
-}
+vector < vector<int> > a(1001, vector<int>(1001));
 
 int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 	//freopen("input.txt", "r", stdin);
-	scanf("%d", &N);
-	for (int i = 0; i < N; i++)
-		scanf("%d", &a[i]);
 
-	lds();
+	int len1, len2, N;
+	string str;
+	char c1[1000], c2[1000];
+
+	cin >> str;
+	len1 = str.size(); // vertical
+	c1[0] = '0', a[0][0] = 0;
+	for (int i = 0; i < len1; i++) {
+		c1[i + 1] = str.at(i);
+		a[i][0] = 0;
+	}
+
+	cin >> str;
+	len2 = str.size(); //horizontal
+	c2[0] = '0';
+	for (int i = 0; i < len2; i++){
+		c2[i + 1] = str.at(i);
+		a[0][i] = 0;
+	}
+	
+	for (int i = 1; i <= len1; i++) {
+		for (int j = 1; j <= len2; j++) {
+			if (c1[i] == c2[j])
+				a[i][j] = a[i - 1][j - 1] + 1;
+			else
+				a[i][j] = max(a[i - 1][j], a[i][j - 1]);
+		}
+	}
+
+	cout << a[len1][len2];
 
 	return 0;
 }
