20c791fa1e44c22ceb4f66a153c0092bec18646c
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-08-02 17:01:37 +0900

    Day 67 - 1
    BAEKJOON 12865
    
    knapsack problem
    * reference book: '프로그래밍 대회에서 배우는 알고리즘 문제 해결 전략'
    one of 'DP promblems'

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,54 +1,37 @@
-#include<iostream>
-#include<stack>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-int main(void) {
-	freopen("input.txt", "r", stdin);
-	int n, num, index = 1, k=0;
-	char c[200000];
-	bool isS = true;
-	scanf("%d", &n);
-	stack <int> s;
+int N, K;
+vector <pair<int, int>> arr(100001); //weight, value
+vector <vector <int>> dp(100001, vector<int>(101,-1)); //loade weight, index
+// max value when [loaded weight][index ~ end]
 
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &num);
-		if (!isS)
-			continue;
-		if (!s.empty()) {
-			if (s.top() == num) {
-				s.pop();
-				c[k++] = '-';
-			}
-			else if (num >= index) {
-				while (s.top() != num) {
-					s.push(index++);
-					c[k++] = '+';
-				}
-				s.pop();
-				c[k++] = '-';
-			}
-			else
-				isS = false;
-		}
-		else {
-			if (num >= index) {
-				s.push(index++);
-				c[k++] = '+';
-				while (s.top() != num) {
-					s.push(index++);
-					c[k++] = '+';
-				}
-				s.pop();
-				c[k++] = '-';
-			}
-			else
-				isS = false;
-		}
+//returning max value, pick items from index to end, and when remain weight is 'capacity'
+int bagPacking(int capacity, int index) { 
+	if (index == N) return 0;
+	int& ret = dp[capacity][index];
+	if (ret != -1) //if already have answer
+		return ret;
+	ret = bagPacking(capacity, index + 1);
+	if (capacity >= arr[index].first)
+		ret = max(ret, bagPacking(capacity - arr[index].first, index + 1) + arr[index].second);
+	return ret;
+}
+int main() {
+	freopen("input.txt", "r", stdin);
+	scanf("%d %d", &N, &K);
+	int w, v, max_val = 0;
+	for (int i = 0; i < N; i++) {
+		scanf("%d %d", &w, &v);
+		arr[i].first = w, arr[i].second = v;
 	}
-	if (!isS)
-		printf("NO");
-	else
-		for (int i = 0; i < k; i++)
-			printf("%c\n", c[i]);
-	return 0;
+	
+	for (int i = 0; i <= K; i++) {
+		printf("%d: %d\n", i, bagPacking(i, 0));
+		max_val = max(max_val, bagPacking(i, 0));
+	}
+	printf("%d", max_val);
 }
