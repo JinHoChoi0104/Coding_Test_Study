8edcdf5c28769f802b1933ed706977342dde9eaa
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-08-02 18:20:38 +0900

    Day 67 - 2
    BAEKJOON 12920
    
    knapsack problem(2)
    More harder than before. When solving DP problem, you should avoid duplicated work much as you can.

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -5,12 +5,12 @@
 using namespace std;
 
 int N, K;
-vector <pair<int, int>> arr(100001); //weight, value
-vector <vector <int>> dp(100001, vector<int>(101,-1)); //loade weight, index
+vector <pair<int, int>> arr(1401); //weight, value
+vector <vector <int>> dp(10001, vector<int>(1401, -1)); //load weight, index
 // max value when [loaded weight][index ~ end]
 
 //returning max value, pick items from index to end, and when remain weight is 'capacity'
-int bagPacking(int capacity, int index) { 
+int bagPacking(int capacity, int index) {
 	if (index == N) return 0;
 	int& ret = dp[capacity][index];
 	if (ret != -1) //if already have answer
@@ -21,17 +21,29 @@ int bagPacking(int capacity, int index) {
 	return ret;
 }
 int main() {
-	freopen("input.txt", "r", stdin);
+	//freopen("input.txt", "r", stdin);
 	scanf("%d %d", &N, &K);
-	int w, v, max_val = 0;
+	int v, c, k, j = 0, max_val = 0;
 	for (int i = 0; i < N; i++) {
-		scanf("%d %d", &w, &v);
-		arr[i].first = w, arr[i].second = v;
+		scanf("%d %d %d", &v, &c, &k);
+		
+		while (k > 3) {
+			arr[j].first = v * (int)(k / 2), arr[j++].second = c * (int)(k / 2);
+			if (k % 2 == 1)
+				k = k / 2 + 1;
+			else
+				k = k / 2;
+		}
+		if (k == 3) 
+			arr[j].first = v * 2, arr[j++].second = c * 2;
+		else if (k == 2) 
+			arr[j].first = v, arr[j++].second = c;
+		arr[j].first = v, arr[j++].second = c;
 	}
-	
-	for (int i = 0; i <= K; i++) {
-		printf("%d: %d\n", i, bagPacking(i, 0));
+	N = j;
+
+	for (int i = 0; i <= K; i++)
 		max_val = max(max_val, bagPacking(i, 0));
-	}
+
 	printf("%d", max_val);
 }
