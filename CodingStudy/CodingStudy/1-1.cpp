fd61426a48042bbd0d128af1c471ffa7708eb406
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-08-03 14:59:37 +0900

    Day 68
    BAEKJOON 15903
    Priorty Queue
    - Instead of FIFO like original Queue, data which has higer priority gets out first
    - Mostly stored at Heap data container
    - When new data is inserted. It costs O(log N)
    
    priority_queue <int> p_q;
    //descending order (p_q.top() returns biggest num)
    
    priority_queue <ll, vector<ll>,greater<ll>> p_q;
    //ascending order (p_q.top() returns smallest num)

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,49 +1,32 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 #include <algorithm>
-
 using namespace std;
 
-int N, K;
-vector <pair<int, int>> arr(1401); //weight, value
-vector <vector <int>> dp(10001, vector<int>(1401, -1)); //load weight, index
-// max value when [loaded weight][index ~ end]
+#define ll long long
 
-//returning max value, pick items from index to end, and when remain weight is 'capacity'
-int bagPacking(int capacity, int index) {
-	if (index == N) return 0;
-	int& ret = dp[capacity][index];
-	if (ret != -1) //if already have answer
-		return ret;
-	ret = bagPacking(capacity, index + 1);
-	if (capacity >= arr[index].first)
-		ret = max(ret, bagPacking(capacity - arr[index].first, index + 1) + arr[index].second);
-	return ret;
-}
 int main() {
 	//freopen("input.txt", "r", stdin);
-	scanf("%d %d", &N, &K);
-	int v, c, k, j = 0, max_val = 0;
-	for (int i = 0; i < N; i++) {
-		scanf("%d %d %d", &v, &c, &k);
-		
-		while (k > 3) {
-			arr[j].first = v * (int)(k / 2), arr[j++].second = c * (int)(k / 2);
-			if (k % 2 == 1)
-				k = k / 2 + 1;
-			else
-				k = k / 2;
-		}
-		if (k == 3) 
-			arr[j].first = v * 2, arr[j++].second = c * 2;
-		else if (k == 2) 
-			arr[j].first = v, arr[j++].second = c;
-		arr[j].first = v, arr[j++].second = c;
+	int n, m, num;
+	ll sum = 0;
+	//priority_queue <int> p_q; //descending order (p_q.top() returns biggest num)
+	priority_queue <ll, vector<ll>,greater<ll>> p_q; //ascending order (p_q.top() returns smallest num)
+	scanf("%d %d", &n, &m);
+	while (n--) {
+		scanf("%d", &num);
+		p_q.push(num);
 	}
-	N = j;
-
-	for (int i = 0; i <= K; i++)
-		max_val = max(max_val, bagPacking(i, 0));
-
-	printf("%d", max_val);
+	ll a, b;
+	while (m-- > 0) { // pop two numbers from priority queue
+		a = p_q.top(), p_q.pop(); // smallest num
+		b = p_q.top(), p_q.pop(); // 2nd smallest num
+		a += b, b = a; // add both
+		p_q.push(a), p_q.push(b); // push
+	}
+	while (!p_q.empty()) {
+		sum += p_q.top();
+		p_q.pop();
+	}
+	printf("%lld", sum);
+	return 0;
 }
