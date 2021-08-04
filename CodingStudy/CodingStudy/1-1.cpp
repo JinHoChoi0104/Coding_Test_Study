16d9c411a38c1e46b4e59cd83c3b3897714cf630
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-08-04 11:10:14 +0900

    Day 69 - 1
    BAEKJOON 10973
    how to use <algorithm> library's function to array
    - If you want to find first number's index which is bigger than 'num', among same or bigger numbers
    int index = lower_bound(arr , arr + N, num) - arr;
    (N is Size of arr)
    int index = lower_bound(arr + i, arr + N, num) - arr;
    (find in range of i to end)
    
    - Sort array be descending order
    sort(arr, arr + N, greater<>());

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,112 +1,36 @@
 #include <iostream>
-#include <vector>
-#include <set>
-#include <queue>
+#include <algorithm>
 using namespace std;
 
-vector<vector<int>> arr(100, vector<int>(100)); // -1 exposed air, 0 inner air, 1 chease
-set < pair <int,int>> s;
-int N, M, num, max_air = 0, time = 0,  m[2][4] = { { -1, 1, 0, 0} ,{0, 0, -1, 1} };
-
-void melt() {
-	set < pair <int, int>> exs(s);
-	set < pair <int, int>> ans;
-	s.clear();
-
-	int x, y, tox, toy, cnt;
-	while (!exs.empty()) {
-		auto it = exs.begin();
-		pair<int,int> tmp = *it;
-		x = tmp.first, y = tmp.second;
-		exs.erase(it);
-		if (arr[x][y] == -1)
-			continue;
-		cnt = 0;
-		for (int k = 0; k < 4; k++) {
-			tox = x + m[0][k], toy = y + m[1][k];
-			if (arr[tox][toy] == -1)
-				cnt++;
-		}
-		if (cnt < 2) {
-			s.insert(make_pair(x, y));
-			continue;
-		}
-		for (int k = 0; k < 4; k++) {
-			tox = x + m[0][k], toy = y + m[1][k];
-			if (arr[tox][toy] == 1)
-				s.insert(make_pair(tox, toy));
-		}
-		ans.insert({x, y});
-	}
-	if (!ans.empty())
-		time++;
-	while (!ans.empty()) {
-		auto it = ans.begin();
-		pair<int,int> tmp = *it;
-		x = tmp.first, y = tmp.second;
-		ans.erase(it);
-		
-		arr[x][y] = -1; //no chease now!
-	}
-}
-
-
-void BFS() { // find exposed air
-	queue <pair <int, int>> aq; 
-	vector<vector<bool>> visited(100, vector<bool>(100, false));
-	aq.push({ 0,0 }), arr[0][0] = -1, visited[0][0] = false;
-	int x, y, tox, toy;
-	while (!aq.empty()) {
-		x = aq.front().first, y = aq.front().second;
-		aq.pop();
-		for (int k = 0; k < 4; k++) {
-			tox = x + m[0][k], toy = y + m[1][k];
-			if (tox > -1 && tox < N && toy > -1 && toy < M) {
-				if (arr[tox][toy] == 1 || visited[tox][toy])
-					continue;
-				if (arr[tox][toy] == 0) {
-					max_air--;
-					arr[tox][toy] = -1;
-				}
-				visited[tox][toy] = true;
-				aq.push({ tox, toy });
-			}
-		}
-	}
-	
-}
 int main() {
-//	freopen("input.txt", "r", stdin);
-	
-	scanf("%d %d", &N, &M);
+	//freopen("input.txt", "r", stdin);
+	int N, arr[10000];
+	scanf("%d", &N);
 	for (int i = 0; i < N; i++) 
-		for (int j = 0; j < M; j++) {
-			scanf("%d", &arr[i][j]);
-			if (arr[i][j] == 0)
-				max_air++;
-		}	
+		scanf("%d", &arr[i]);
+			
+	int index = N;
+	for (int i = N - 1; i > 0; i--) { //searching from back
+		if (arr[i - 1] < arr[i])
+			continue;
+		index = i - 1; //find first higer num
+		break;
+	}
+	if (index == N) { // if numbers are in ascending order
+		printf("-1");
+		return 0;
+	}
+	// Among numbers behind index, find highest number but smaller than index number 
+	int target = lower_bound(arr + (index + 1), arr + N, arr[index]) - arr - 1;
+	
+	int ret = arr[index]; // Switch index with target number
+	arr[index] = arr[target], arr[target] = ret;
 
-	int tox, toy, cnt;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			if (arr[i][j] == 1) {
-				cnt = 0;
-				for (int k = 0; k < 4; k++) {
-					tox = i + m[0][k], toy = j + m[1][k];
-					if (arr[tox][toy] == 0)
-						cnt++;
-				}
-				if (cnt >= 2) //find exposed chease
-					s.insert(make_pair(i, j ));
-			}
-		}
-	}
-	while (!s.empty()) {
-		if(max_air > 0)
-			BFS();
-		melt();
-	}
-	printf("%d", time);
+	// With numbers behind the index, sort by descending order
+	sort(arr + (index + 1), arr + N, greater<>());
+
+	for (int i = 0; i < N; i++)
+		printf("%d ", arr[i]);
 
 	return 0;
 }
