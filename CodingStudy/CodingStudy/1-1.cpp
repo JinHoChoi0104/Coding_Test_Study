9585e537a025c62c7193be6f398f85f7a78e70e5
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-08-05 19:07:39 +0900

    Day 70
    BAEKJOON 14500
    solved with Brute Force

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -2,35 +2,58 @@
 #include <algorithm>
 using namespace std;
 
+int board[500][500];
+int N, M, max_sum = 0, m[2][4] = { {-1,1,0,0} , {0,0,-1,1} };
+
+void DFS(int x, int y, int cnt, int dir, int sum) {
+	int tox, toy, add = 0, add2 = 0;
+	sum += board[x][y];
+	
+	if (dir == 1) dir = 0;
+	else if (dir == 0) dir = 1;
+	else if (dir == 2) dir = 3; 
+	else if (dir == 3) dir = 2; //not going back
+
+	for (int k = 0; k < 4; k++) {
+		if (k == dir)
+			continue;
+		tox = x + m[0][k], toy = y + m[1][k];
+		if (tox > -1 && tox <N && toy > -1 && toy < M) {
+			if (cnt != 3) {
+				DFS(tox, toy, cnt + 1, k, sum);
+				if (cnt == 2) {
+					if (board[tox][toy] > add && board[tox][toy] > add2) {
+						add2 = add;
+						add = board[tox][toy];
+					}
+					else if (board[tox][toy] > add2)
+						add2 = board[tox][toy];
+				}
+			}
+			else 
+				add = max(add, board[tox][toy]);
+		}
+	}
+	if (cnt == 2) {
+		sum = sum + add + add2;
+		max_sum = max(max_sum, sum);
+	}
+	else if (cnt == 3) {
+		sum += add;
+		max_sum = max(max_sum, sum);
+	}
+}
 int main() {
 	//freopen("input.txt", "r", stdin);
-	int N, arr[10000];
-	scanf("%d", &N);
-	for (int i = 0; i < N; i++) 
-		scanf("%d", &arr[i]);
-			
-	int index = N;
-	for (int i = N - 1; i > 0; i--) { //searching from back
-		if (arr[i - 1] < arr[i])
-			continue;
-		index = i - 1; //find first higer num
-		break;
-	}
-	if (index == N) { // if numbers are in ascending order
-		printf("-1");
-		return 0;
-	}
-	// Among numbers behind index, find highest number but smaller than index number 
-	int target = lower_bound(arr + (index + 1), arr + N, arr[index]) - arr - 1;
-	
-	int ret = arr[index]; // Switch index with target number
-	arr[index] = arr[target], arr[target] = ret;
-
-	// With numbers behind the index, sort by descending order
-	sort(arr + (index + 1), arr + N, greater<>());
-
+	scanf("%d %d", &N, &M);
 	for (int i = 0; i < N; i++)
-		printf("%d ", arr[i]);
+		for (int j = 0; j < M; j++)
+			scanf("%d", &board[i][j]);
+	int ans = 0, sum = 0;
+	for (int i = 0; i < N; i++) 
+		for (int j = 0; j < M; j++)
+			DFS(i, j, 1, 4, 0);
 
+	printf("%d", max_sum);
 	return 0;
 }
