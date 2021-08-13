df17e7615f9756483eff6ec5b21081a3863ced5b
JinHoChoi <63338253+DovyIsFree@users.noreply.github.com>
2021-08-13 12:11:24 +0900

    Day 78
    BAEKJOON 1541
    Greedy problem

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,48 +1,57 @@
 #include <iostream>
-#include <queue>
+#include <vector>
+
 using namespace std;
 
-int N, M, m[2][8] = { {-1,0,1,-1,1,-1,0,1}, {-1,-1,-1,0,0,1,1,1} };
-bool letter[250][250];
-
-void DFS(int x, int y) {
-	int tox, toy;
-	letter[x][y] = false;
-	for (int k = 0; k < 8; k++) {
-		tox = x + m[0][k], toy = y + m[1][k];
-		if (tox > -1 && tox < N && toy > -1 && toy < M)
-			if (letter[tox][toy])
-				DFS(tox, toy);
-	}
-}
-
 int main() {
-	scanf("%d %d", &N, &M);
-	int num;
-	queue<pair<int, int>>q;
-	bool isl = false;
-	for (int i = 0; i < N; i++) {
-		isl = false;
-		for (int j = 0; j < M; j++) {
-			scanf("%d", &num);
-			if (num == 1) {
-				letter[i][j] = true;
-				if (!isl) //if it's in same line you don't have to start searching at that point
-					q.push(make_pair(i, j)); //save 1's postion to search
-				isl = true;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	string str;
+	cin >> str;
+	int len = str.length(), ret = 0;
+	vector<int> a;
+	bool minus = false;
+	char c;
+	for (int i = 0; i <= len; i++) {
+		if (i != len)
+			c = str.at(i);
+		else
+			c = '+';
+		if (c == '-' || c == '+') {
+			if (ret != 0) {
+				if (minus)
+					ret *= -1;
+				a.push_back(ret);
 			}
+			if (c == '+')
+				minus = false;
 			else
-				isl = false;
+				minus = true;
+			ret = 0;
+		}
+			
+		else {
+			ret *= 10;
+			ret += c - '0';
 		}
 	}
-	int cnt = 0;
-	while (!q.empty()) {
-		if (letter[q.front().first][q.front().second]) {
-			DFS(q.front().first, q.front().second);
-			cnt++; //count DFS
+	int N = a.size(), ans = 0;
+	for (int i = 0; i < N; i++) {
+		if (a[i] < 0) { 
+			ret = -a[i];
+			for (int j = i + 1; j < N; j++) {
+				if (a[j] > 0) 
+					ret += a[j];			
+				else
+					ret -= a[j];
+				i++;
+			}
+			ans -= ret;
 		}
-		q.pop();
+		else
+			ans += a[i];
 	}
-	printf("%d", cnt);
+	cout << ans;
 	return 0;
 }
