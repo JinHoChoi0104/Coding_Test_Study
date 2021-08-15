1a0573a38d7e1539dbea8c5fa38135845d1b58ea
JinHoChoi <63338253+DovyIsFree@users.noreply.github.com>
2021-08-15 17:37:52 +0900

    Day 80
    BAEKJOON 16236
    Baby shark problem, solved by BFS

--- CodingStudy/BAEKJOON 16236_BFS.cpp
+++ CodingStudy/BAEKJOON 16236_BFS.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 using namespace std;
 
 int sea[20][20];
 int N, shark = 2, eat = 0, m[2][4] = { {-1,0,0,1}, {0,-1, 1,0} }; // up left right down
 struct pos { int x, y, move; };
 queue<pos> q;
-vector <vector<int>> visited(20, vector<int>(20));
-
+vector <vector<bool>> visited(20, vector<bool>(20));
+bool compare(const pair<int, int>& a, const pair<int, int>& b) {
+	if (a.first != b.first)
+		return a.first < b.first;
+	return a.second < b.second;
+}
 void clear() {
 	for (int i = 0; i < N; i++)
 		for (int j = 0; j < N; j++)
@@ -19,24 +24,38 @@ void BFS() {
 	int x, y, tox, toy, total = 0, move = 0;
 	while (!q.empty()) {
 		x = q.front().x, y = q.front().y;
-		move = q.front().move;
+		move = q.front().move + 1;
 		q.pop();
 
 		for (int k = 0; k < 4; k++) {
 			tox = x + m[0][k], toy = y + m[1][k];
 			if (tox > -1 && tox < N && toy > -1 && toy < N) {
-
 				if (!visited[tox][toy] && sea[tox][toy] <= shark) {
-
 					if (sea[tox][toy] == 0 || sea[tox][toy] == shark) { //no eating
 						visited[tox][toy] = true;
-						//cout << tox << toy;
-						q.push({ tox, toy, move +1 });
+						q.push({ tox, toy, move });
 					}
 					else { //can eat
-						while (!q.empty())
+						vector<pair<int, int>> arr;
+						int index = move;
+						arr.push_back(make_pair(tox, toy));
+						visited[tox][toy] = true;
+						while (!q.empty()) { //find all fish that can be eaten in same distance
+							x = q.front().x, y = q.front().y;
+							move = q.front().move;
+							if (move == index) break;
 							q.pop();
-						cout << tox << ", "<<toy << ":  "<<move + 1 << endl;
+							for (int i = 0; i < 4; i++) {
+								tox = x + m[0][i], toy = y + m[1][i];
+								if (tox > -1 && tox < N && toy > -1 && toy < N) 
+									if (!visited[tox][toy] && sea[tox][toy] < shark && sea[tox][toy] > 0) 
+										arr.push_back(make_pair(tox, toy));							
+							}
+						}
+						while (!q.empty()) q.pop();
+						
+						sort(arr.begin(), arr.end(), compare);
+						tox = arr[0].first, toy = arr[0].second;
 						clear(); //restart the journey
 						sea[tox][toy] = 0, eat++; //eat
 						if (shark == eat) {
@@ -45,7 +64,7 @@ void BFS() {
 						}
 						visited[tox][toy] = true;
 						q.push({ tox, toy, 0 });
-						total += (move+1);
+						total += (index);
 						break;
 					}
 				}
@@ -57,17 +76,16 @@ void BFS() {
 int main() {
 	scanf("%d", &N);
 
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++) 
 		for (int j = 0; j < N; j++) {
 			scanf("%d", &sea[i][j]);
 			if (sea[i][j] == 9) {
 				visited[i][j] = true;
-				q.push({ i,j, 0});
+				q.push({ i,j, 0 });
 				sea[i][j] = 0;
 			}
 		}
-	
-	}
+
 	BFS();
 	return 0;
 }
--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,47 +1,91 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <algorithm>
 using namespace std;
 
+int sea[20][20];
+int N, shark = 2, eat = 0, m[2][4] = { {-1,0,0,1}, {0,-1, 1,0} }; // up left right down
+struct pos { int x, y, move; };
+queue<pos> q;
+vector <vector<bool>> visited(20, vector<bool>(20));
+bool compare(const pair<int, int>& a, const pair<int, int>& b) {
+	if (a.first != b.first)
+		return a.first < b.first;
+	return a.second < b.second;
+}
+void clear() {
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			visited[i][j] = false;
+}
+
+void BFS() {
+	int x, y, tox, toy, total = 0, move = 0;
+	while (!q.empty()) {
+		x = q.front().x, y = q.front().y;
+		move = q.front().move + 1;
+		q.pop();
+
+		for (int k = 0; k < 4; k++) {
+			tox = x + m[0][k], toy = y + m[1][k];
+			if (tox > -1 && tox < N && toy > -1 && toy < N) {
+				if (!visited[tox][toy] && sea[tox][toy] <= shark) {
+					if (sea[tox][toy] == 0 || sea[tox][toy] == shark) { //no eating
+						visited[tox][toy] = true;
+						q.push({ tox, toy, move });
+					}
+					else { //can eat
+						vector<pair<int, int>> arr;
+						int index = move;
+						arr.push_back(make_pair(tox, toy));
+						visited[tox][toy] = true;
+						while (!q.empty()) { //find all fish that can be eaten in same distance
+							x = q.front().x, y = q.front().y;
+							move = q.front().move;
+							if (move == index) break;
+							q.pop();
+							for (int i = 0; i < 4; i++) {
+								tox = x + m[0][i], toy = y + m[1][i];
+								if (tox > -1 && tox < N && toy > -1 && toy < N) 
+									if (!visited[tox][toy] && sea[tox][toy] < shark && sea[tox][toy] > 0) 
+										arr.push_back(make_pair(tox, toy));							
+							}
+						}
+						while (!q.empty()) q.pop();
+						
+						sort(arr.begin(), arr.end(), compare);
+						tox = arr[0].first, toy = arr[0].second;
+						clear(); //restart the journey
+						sea[tox][toy] = 0, eat++; //eat
+						if (shark == eat) {
+							shark++; //get bigger
+							eat = 0;
+						}
+						visited[tox][toy] = true;
+						q.push({ tox, toy, 0 });
+						total += (index);
+						break;
+					}
+				}
+			}
+		}
+	}
+	printf("%d", total);
+}
 int main() {
-	int N, rate[4];
 	scanf("%d", &N);
-	for (int i = 0; i < 4; i++)
-		scanf("%d", &rate[i]);
-	for (int i = 0; i < 4; i++)
-		rate[i]--;
 
-	vector<int> arr;
-	char c;
-	for (int i = 0; i < N; i++) {
-		scanf(" %c", &c);
-		if (c == 'B')
-			arr.push_back(0);
-		else if (c == 'S')
-			arr.push_back(1);
-		else if (c == 'G')
-			arr.push_back(2);
-		else if (c == 'P')
-			arr.push_back(3);
-		else
-			arr.push_back(4);
-	}
+	for (int i = 0; i < N; i++) 
+		for (int j = 0; j < N; j++) {
+			scanf("%d", &sea[i][j]);
+			if (sea[i][j] == 9) {
+				visited[i][j] = true;
+				q.push({ i,j, 0 });
+				sea[i][j] = 0;
+			}
+		}
 
-	int sum = 0, a1, a2 = 0, tmp;
-	if (arr[0] < 4)
-		a1 = rate[arr[0]], sum = a1;
-	else
-		a1 = rate[3] + 1, sum = a1;
-	for (int i = 1; i < N; i++) {
-		if (arr[i] < 4)
-			a2 = rate[arr[i]] - a1;
-		else
-			a2 = rate[3] + 1;
-		sum += a2;
-		a1 = a2;
-	}
-
-	printf("%d", sum);
-
+	BFS();
 	return 0;
 }
