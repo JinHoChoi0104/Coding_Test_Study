9f47ce84b7912b683f688d0312b5a17eb4b8e372
JinHoChoi <bearsff@naver.com>
2021-08-25 18:52:09 +0900

    Day 90
    BAEKJOON 5557
    solved by using map

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,40 +1,40 @@
 #include<iostream>
-
+#include<vector>
+#include<map>
 using namespace std;
 
-int max_num = -100000000;
-int min_num = 100000000;
-int A[11]; //input number
-
-void DFS(int num, int plus, int minus, int multiple, int divide, int n) { // 2<= N <= 11, n = N, N-1, ... , 0
-	if (n == -1) { // n는 전체 연산의 횟수가 저장 돼 있었다
-		if (num > max_num)
-			max_num = num;
-		if (num < min_num)
-			min_num = num;
+vector<int>arr(100);
+map<int, long long>m; //index까지 계산해서 얻은 값, 그 값의 갯수
+int N;
+long long cnt = 0;
+void DFS(int index) { //계산해야될 곳의 인덱스
+	if(index == N-1) { //모든 계산을 마쳤을 때
+		for (auto it = m.begin(); it != m.end(); it++)
+			if (it->first == arr[N - 1])
+				cnt+=it->second; //카운트 한다
+		return;
 	}
-	else {
-		if (plus > 0)
-			DFS(num + A[n], plus - 1, minus, multiple, divide, n - 1);
-		if (minus > 0)
-			DFS(num - A[n], plus, minus - 1, multiple, divide, n - 1);
-		if (multiple > 0)
-			DFS(num * A[n], plus, minus, multiple - 1, divide, n - 1);
-		if (divide > 0)
-			DFS(num / A[n], plus, minus, multiple, divide - 1, n - 1);
+	int a, b;
+	map<int, long long>tmp; //임시 저장소
+	for (auto it = m.begin(); it != m.end(); it++) {
+		a = it -> first + arr[index], b = it -> first - arr[index];
+		if (a >= 0 && a <= 20)
+			tmp[a] += it -> second;
+		if (b >= 0 && b <= 20)
+			tmp[b] += it -> second;
 	}
+	m.swap(tmp); //스왑해준다
+	DFS(index + 1);
 }
 
 int main(void) {
-	int N;
-	cin >> N;
-	for (int i = N - 1; i > -1; i--)
-		cin >> A[i];
-	int arr[4]; // plus, minus, multiple, divide
-	for (int i = 0; i < 4; i++)
-		cin >> arr[i];
+	scanf("%d",&N);
+	for (int i = 0; i < N; i++) 
+		scanf("%d", &arr[i]);
 
-	DFS(A[N - 1], arr[0], arr[1], arr[2], arr[3], N - 2);
-	cout << max_num << endl;
-	cout << min_num << endl;
+	m[arr[0]]++;
+	DFS(1);
+
+	printf("%lld", cnt);
+	return 0;
 }
