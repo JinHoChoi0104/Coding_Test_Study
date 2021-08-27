2c3908ce6873dc4ea6b7e199912edec1acccbb5a
JinHoChoi <bearsff@naver.com>
2021-08-27 23:00:59 +0900

    Day 92
    studied string getline()

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,36 +1,16 @@
 #include <iostream>
-#include <vector>
-#include <queue>
+#include <string>
 using namespace std;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL), cout.tie(NULL);
-	int T, N, num;
-	for (cin >> T; T-- > 0;) {
-		cin >> N;
-		vector<int> v(N + 1);
-		vector<bool> visited(N + 1, false);
-		for (int i = 1; i <= N; i++) 
-			cin >> v[i];
-		
-		queue<int>q;
-		int cnt = 0;
-		for (int i = 1; i <= N; i++) {
-			if (!visited[i]) { //방문한적 없으면 탐색 시작
-				cnt++;
-				q.push(v[i]);
-				visited[i] = true;
-				while (!q.empty()) { //이어진 곳은 전부 탐색
-					num = q.front();
-					visited[num] = true;
-					q.pop();
-					if (!visited[v[num]]) 
-						q.push(v[num]);
-				}
-			}
-		}
-		cout << cnt << "\n";
-	}
+	string str;
+	cout << "get string by only cin: " ;
+	cin >> str;
+	cout << str << endl;
+
+	cout << "get string by getline: " ;
+	getline(cin, str);
+	cout << str << endl;
+	
 	return 0;
 }
