762a6d7dfd95dd6ca609038a42a219ac900152d0
JinHoChoi <bearsff@naver.com>
2021-08-28 12:33:04 +0900

    Day 93
    studied string's substr

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
-#include <string>
+
 using namespace std;
 
 int main() {
-	string str;
-	cout << "get string by only cin: " ;
-	cin >> str;
-	cout << str << endl;
+	string str = "0123456789abc";
+	cout << str << "\n*divide string into substrings, 4 charcter per line*\n";
+	for(int i=0; i<str.length();i += 4)
+		cout << str.substr(i,4) << "\n";
 
-	cout << "get string by getline: " ;
-	getline(cin, str);
-	cout << str << endl;
-	
+	string str2 = "01234567";
+	cout << "\n" << str2 << "\n*extract substring starting from index: 5*\n";
+	cout << str2.substr(5) << endl;
 	return 0;
 }
