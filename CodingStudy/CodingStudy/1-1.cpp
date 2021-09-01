ffc42e63f8c827862f0ba177dc0165543fd60120
JinHoChoi <bearsff@naver.com>
2021-09-01 19:20:05 +0900

    5th week
    BAEKJOON 1495

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,38 +1,12 @@
 #include <iostream>
-#include <vector>
-#include <map>
+#include <set>
 
 using namespace std;
-
+int h[100000];
 int main() {
-	int N, C, M, from, to, num;
-	scanf("%d %d %d", &N, &C, &M);
-	map<int, int>m[2001]; //어디로, 얼만큼
-	for (int i = 0; i < M; i++) {
-		scanf("%d %d %d", &from, &to, &num);
-		m[from][to] = num;
-	}
-	int total = 0;
-	int volume = C; //현재 차가 실을 수 있는 박스의 수
-	map <int, int>car; //to, num
-	for (int i = 1; i <= N; i++) {
-		int drop = car[i];
-		car[i] = 0;
-		volume += drop;
-		total += drop;
-		//cout << total << endl;
-		for (auto it = m[i].begin(); it != m[i].end(); it++) {
-			if (it->second <= volume) {
-				car[it->first] += it->second;
-				volume -= it->second;
-			}
-			else {
-				car[it->first] += volume;
-				volume = 0;
-				break;
-			}
-		}
-	}
-	printf("%d", total);
+	int N, M;
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)
+		scanf("%d", &h[i]);
 	return 0;
 }
