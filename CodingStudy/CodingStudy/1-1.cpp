3bc928bd7e0138e76ff45d72e36bae10c26a5a5d
JinHoChoi <bearsff@naver.com>
2021-09-04 14:55:24 +0900

    Day 100
    Binary Tree

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,68 +1,42 @@
 #include <iostream>
-
+#include <stack>
+#include <vector>
 using namespace std;
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	string str;
-	int T;
-	for (cin >> T; T-- > 0;) {
-		cin >> str;
-		str += '2';
-		bool isP = true;
-		int i = 0;
-		while( i < str.size() - 1) {
-			if (str.at(i++) == '0') { // 01인 경우
-				if (str.at(i++) != '1') {
-					isP = false;
-					break;
-				}
-			}
-			else { //1001인 경우
-				if (str.at(i++) != '0') { //0이 2개는 무조건 있어야함
-					isP = false;
-					break;
-				}
-				if (str.at(i++) != '0') {
-					isP = false;
-					break;
-				}
-				while (str.at(i) == '0') 
-					i++;	
+	vector<int> arr;
+	vector<int> tree(100000);
+	stack<int>s;
+	int num, index = 1;
 
-				if (str.at(i++) != '1') { //1이 1개는 무조건 있어야함
-					isP = false;
-					break;
-				}
-				int k = 0;
-				while (str.at(i) == '1') {
-					i++;
-					k++;
-				}
-				if (k == 0) //반복되는 1이 없는 경우 pass
-					continue;
-				
-				if (str.at(i++) == '2') //반복되는 1이 나온 후 끝인 경우
-					break;
-				//반복되는 1이 나온 후 0이 나온 경우 ex) 100111110
-
-				//그 다음 숫자가
-				if (str.at(i) == '0') //00이면 1001일수도 있으니 앞으로 가서 체킹 
-					i -= 2;
-				else if (str.at(i) == '1') //01이면 또 하나의 페턴
-					i++;
-				else { //02이면 나올 수 없는 패턴
-					isP = false;
-					break;
-				}
-			}
-		}
-		if (isP)
-			cout << "YES\n";
-		else
-			cout << "NO\n";
+	while (cin >> num) {
+		arr.push_back(num);
 	}
+	int tmp1, tmp2;
+	tree[0] = 987654321;
+	tree[index] = arr[0];
+	s.push(arr[0]);
+	for (int i = 1; i < arr.size(); i++) {
+		cout << index<<" "<<tree[index] << endl;
+		if (tree[index] > arr[i]) {
+			tree[index * 2] = arr[i];
+			index *= 2;
+			s.push(arr[i]);
+		}
+		else {
+		//	cout << index;
+			while (tree[index/2]<arr[i]) {
+				//cout << "a";
+				index /= 2;
+			}
+			tree[index*2 + 1] = arr[i];
+			index= index*2+1;
+
+		}
+	}
+
 	return 0;
 }
