c422b52a79c47c5e0c2cc3ee59780a07c9bb1480
JinHoChoi <bearsff@naver.com>
2021-09-05 23:51:19 +0900

    Day 101
    BAEKJOON 8980

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,29 +1,66 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <memory.h>
 using namespace std;
 
-vector<int>arr;
+struct box { int from, to, num; };
 
-void findroot(int l, int r) { //루트를 찾는다
-	if (r < l)
-		return;
-	int i;
-	for (i = l + 1; i <= r; i++) //루트보다 큰 수를 찾고 그 수를 기준으로 좌측 우측 자식 노드탐색
-		if (arr[i] > arr[l]) break;
-
-	findroot(l + 1, i - 1); //왼쪽 자식 노드 탐색
-	findroot(i, r); //오른쪽 자식 노드 탐색
-	printf("%d\n", arr[l]); //후위순위 결과 출력
+bool compare(const box& a, const box& b) {
+	if (a.to == b.to)
+		return a.from > b.from;
+	return a.to < b.to;
 }
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
 
-	int num;
-	while (cin >> num) //ctrl + z 나올때까지 입력 받는다
-		arr.push_back(num);
+	vector<box>v;
+	vector<int> truck(2001, 0);
+	int N, C, M, from, to, num, volume, ans = 0, tmp =0, index = 0;
+	
 
-	findroot(0, arr.size()-1);
+	cin >> N >> C >> M;
+	for (int i = 0; i < M; i++) {
+		cin >> from >> to >> num;
+		v.push_back({ from, to, num });
+	}
+	sort(v.begin(), v.end(), compare);
+	for (int i = 2; i <= N; i++) {
+		while (i > v[index].to) {
+			index++;
+			if (index == M)
+				break;
+		}
+		if (index == M)
+			break;
+		while (i == v[index].to) {
+			volume = 0;
+			for (int j = v[index].from; j < i; j++) 
+				volume = max(volume, truck[j]);
+
+			if (C - volume > v[index].num) {
+				for (int j = v[index].from; j < i; j++) 
+					truck[j] += v[index].num;
+				ans += v[index].num;
+			}
+			else {
+				for (int j = v[index].from; j < i; j++) 
+					truck[j] += C - volume;
+				ans += C - volume;
+				break;
+			}
+			index++;
+			if (index == M)
+				break;
+		}
+		if (index == M)
+			break;
+
+	}
+
+	cout << ans;
 
 	return 0;
 }
