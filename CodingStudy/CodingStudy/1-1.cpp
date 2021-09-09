eececba017e211b2e31cae87a743af076f08dcbe
JinHoChoi <bearsff@naver.com>
2021-09-09 12:08:58 +0900

    Day 105
    BAEKJOON 10830

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,44 +1,58 @@
 #include <iostream>
-#include <math.h>
 using namespace std;
+
 #define ll long long
+
+int N, arr[5][5], ans[5][5];
+
+void pow() { // get Squared of A^n
+	int tmp[5][5];
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++) {
+			tmp[i][j] = 0;
+			for (int k = 0; k < N; k++)
+				tmp[i][j] += ans[i][k] * ans[k][j];
+		}
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			ans[i][j] = tmp[i][j] % 1000;
+}
+
+void getSquared(ll n) { // get A^n
+	if (n == 1) return;
+
+	getSquared(n / 2); // get A^(n/2)
+	pow();
+	if (n % 2 != 0) { // if n is odd num
+		int tmp[5][5];
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++) {
+				tmp[i][j] = 0;
+				for (int k = 0; k < N; k++)
+					tmp[i][j] += ans[i][k] * arr[k][j];
+			}
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++)
+				ans[i][j] = tmp[i][j] % 1000;
+	}
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
+	ll n;
+	cin >> N >> n;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++) {
+			cin >> arr[i][j];
+			ans[i][j] = arr[i][j] % 1000;
+		}		
 
-	ll num, tmp = 1;
-	int cnt=0;
-	cin >>  num;
-	if (num == 0) {
-		cout << 0;
-		return 0;
+	getSquared(n);
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) 
+			cout << ans[i][j] <<" ";
+		cout << "\n";
 	}
-	num = abs(num); //계산 편의를 위해 절대값
-	
-	//최소로 이동하기 위해서 목표치보다는 작은 가장 큰 2의 제곱수를 구한다
-	while (num >= tmp) { 
-		tmp *= 2;
-		cnt++;
-	}
-	tmp /= 2;
-	
-	while (1) {
-		if (num > 0) //앞뒤로 왔다갔다
-			num -= tmp;
-		else
-			num += tmp;
-
-		if (num == 0 && tmp == 1) { //이동하는 중간에 0은 나올 수가 없으므로 -1
-			cout << cnt;
-			return 0;
-		}
-		if (num == 0) {
-			cout << -1;
-			return 0;
-		}
-		tmp /= 2;
-	}
-
 	return 0;
 }
