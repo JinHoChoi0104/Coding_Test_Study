6cbf26339534df609e0ee5ad45881832a9cc7f7e
JinHoChoi <bearsff@naver.com>
2021-09-13 23:50:10 +0900

    Day 109
    studing weighted graph

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
-#include <string>
+#include <vector>
 using namespace std;
 
+vector<pair<int, int>> edge[10001];
+
 int main() {
-	string str;
-	getline(cin, str);
-	cout << "Avengers: Endgame";
+	int V, E, a, b, c;
+	scanf("%d %d", &V, &E);
+	for (int i = 1; i <= E; i++) {
+		scanf("%d %d %d", &a, &b, &c);
+		edge[a].push_back({ b,c });
+	}
 	return 0;
 }
