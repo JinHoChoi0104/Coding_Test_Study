2c4f8ebdaa42f00fa6841dfac61fca29c4847de0
JinHoChoi <bearsff@naver.com>
2021-09-19 12:00:44 +0900

    Day 114
    BAEKJOON 1956
    studied Floyd Warshall Algorithm

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,20 +1,56 @@
 #include <iostream>
+#include <map>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
+#define INF 987654321
+map<int, int> adj[101]; //to, cost
+int N, M;
+
+vector<vector<int>> floydWarshall() {
+	//store shortest path on 2nd array
+	vector<vector<int>>dist(N + 1, vector<int>(N + 1, INF));
+	
+	//since the starting city and the destination city are never the same
+	for (int i = 1; i <= N; i++)
+		dist[i][i] = 0;
+
+	//save adjacent path first
+	for (int i = 1; i <= N; i++) 
+		for (auto it = adj[i].begin(); it != adj[i].end(); it++)
+			dist[i][it->first] = it->second;
+
+	for (int k = 1; k <= N; k++) // k: node passing through
+		for (int i = 1; i <= N; i++)
+			for (int j = 1; j <= N; j++)
+				dist[i][j] = min(dist[i][k] + dist[k][j], dist[i][j]);
+
+	return dist;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
 	
-	int N;
-	while (cin >> N) { //입력 들어올때까지 받기
-		string str = "-";
-		while( N-- > 0) { //N번 반복으로 칸토어 구한다
-			string tmp = str;
-			for (int i = 0; i < str.size(); i++)
-				tmp += " ";
-			str = tmp + str;
-		}
-		cout << str <<"\n";
+	cin >> N >> M;
+	int from, to, w;
+	while (M-- > 0) { 
+		cin >> from >> to >> w;
+		if (adj[from][to] == 0 || adj[from][to] > w)
+			adj[from][to] = w;
 	}
+
+	vector<vector<int>>tmp = floydWarshall();
+	for (int i = 1; i <= N; i++){
+		for (int j = 1; j <= N; j++) {
+			if (tmp[i][j] == INF)
+				cout << "0 ";
+			else
+				cout << tmp[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
 	return 0;
 }
