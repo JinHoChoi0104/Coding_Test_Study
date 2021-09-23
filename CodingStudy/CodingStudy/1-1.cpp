650ff1e4d9c89fe1470eb23702f18b9e37d87cf9
JinHoChoi <bearsff@naver.com>
2021-09-23 10:58:01 +0900

    Day 118
    BAEKJOON 2566

--- CodingStudy/CodingStudy/1-1.cpp
+++ CodingStudy/CodingStudy/1-1.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
-#include <set>
-#include <vector>
+
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int N;
-	set<string, greater<string>> s; //내림차순 정렬 저장
-	string a, b;
-	for (cin >> N; N-- > 0;) {
-		cin >> a >> b;
-		if (b == "enter")
-			s.insert(a);
-		else //나가는거면 데이터 삭제
-			s.erase(a);
-	}
-	for (auto it = s.begin(); it != s.end(); it++) //출력
-		cout << *it << "\n";
+	int x, y, num = 0, tmp;
+	for(int i=1; i<=9; i++)
+		for (int j = 1; j <= 9; j++) {
+			cin >> tmp;
+			if (tmp > num) {
+				x = i, y = j;
+				num = tmp ;
+			}
+
+		}
+	cout << num << "\n" << x << " " << y << "\n";
 	return 0;
 }
