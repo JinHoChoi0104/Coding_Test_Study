ee9190df914a47d0e425c05cf0e2ed1e6be66727
JinHoChoi <bearsff@naver.com>
2021-10-08 23:03:01 +0900

    Day 133
    BAEKJOON 2480

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,111 +1,22 @@
-#include <string>
-#include <vector>
-#include <queue>
-#include <set>
-#include <map>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-int N, M;
-bool isInRange(int& x, int& y) {
-	if (x < 0) {
-		x = 0;
-		return false;
-	}
-	else if (x > N - 1) {
-		x = N - 1;
-		return false;
-	}
-	if (y < 0) {
-		y = 0;
-		return false;
-	}
-	else if (y > M - 1) {
-		y = M - 1;
-		return false;
-	}
-	return true;
-}
-long long solution(int n, int m, int x, int y, vector<vector<int>> queries) {
-
-	long long answer = 0;
-	N = n, M = m;
-	int size = 1;
-	bool row = true;
-	int move[2][4] = { {0,0,-1,1}, {-1,1,0,0} };
-
-	set<pair<int, int>>s;
-	s.insert({ x,y });
-	for (int i = queries.size() - 1; i >= 0; i--) {
-		int dir = queries[i][0], len = queries[i][1];
-		if (size == 1) {
-			if ((y == 0 && dir == 0) || (y == m - 1 && dir == 1)) {
-				row = true;
-			}
-			if ((x == 0 && dir == 2) || (x == n - 1 && dir == 3)) {
-				row = false;
-
-				size += len;
-				if (row)
-					if (size > M)
-						size = M;
-				if (!row)
-					if (size > N)
-						size = N;
-
-				if (x == 0 || y == 0)
-					continue;
-
-				x -= move[0][dir] * len, y -= move[1][dir] * len;
-				isInRange(x, y);
-			}
-			else {
-				x -= move[0][dir] * len, y -= move[1][dir] * len;
-				isInRange(x, y);
-			}
-		}
-
-		else {
-			if (row) {
-				if (dir == 2 || dir == 3) {
-					x -= move[0][dir] * len, y -= move[1][dir] * len;
-					isInRange(x, y);
-					continue;
-				}
-				int y1 = y, y2 = y + size - 1;
-				y1 -= move[1][dir] * len;
-				y2 -= move[1][dir] * len;
-				isInRange(x, y1);
-				isInRange(x, y2);
-				y = y1;
-				size = y2 - y1 + 1;
-			}
-			if (!row) {
-				if (dir == 0 || dir == 1) {
-					x -= move[0][dir] * len, y -= move[1][dir] * len;
-					isInRange(x, y);
-					continue;
-				}
-				int x1 = x, x2 = x + size - 1;
-				x1 -= move[1][dir] * len;
-				x2 -= move[1][dir] * len;
-				isInRange(x1, y);
-				isInRange(x2, y);
-				x = x1;
-				size = x2 - x1 + 1;
-			}
-		}
-
-	}
-
-	answer = size;
-	//cout << size;
-	return answer;
-}
 
 int main() {
-
-	vector<vector<int>>arr = { {2,1} ,{0,1},{1,1},{0,1},{2,1} };
-	long long aaa = solution(2, 2, 0, 0, arr);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	int a, b, c, ans;
+	cin >> a >> b >> c;
+	if (a == b && b == c) 
+		ans = 10000 + a * 1000;
+	else if (a == b || a==c) 
+		ans = 1000 + a * 100;
+	else if (b == c) 
+		ans = 1000 + b * 100;
+	else 
+		ans = max({ a,b,c }) * 100;
+	cout << ans;
 
 	return 0;
 }
