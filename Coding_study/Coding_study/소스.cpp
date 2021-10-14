4520839396b07f93e9dfad3baf11b4b80c6dfeeb
JinHoChoi <bearsff@naver.com>
2021-10-14 23:10:05 +0900

    Day 139
    BAEKJOON 2985

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -6,19 +6,25 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
 
-	int N, a, b, c, ans=0;
-	
-	for (cin >> N; N-- > 0;) {
-		cin >> a >> b >> c;
-		if (a == b && b == c)
-			ans = max(ans, 10000 + a * 1000);
-		else if (a == b || a == c)
-			ans = max(ans, 1000 + a * 100);
-		else if (b == c)
-			ans = max(ans, 1000 + b * 100);
-		else
-			ans = max(ans, max({ a,b,c }) * 100);
-	}
-	cout << ans;
+	int a, b, c;
+	cin >> a >> b >> c;
+		
+	if (a + b == c)
+		cout << a << '+' << b << '=' <<c;
+	else if (a - b == c)
+		cout << a << '-' << b << '=' << c;
+	else if (a * b == c)
+		cout << a << '*' << b << '=' << c;
+	else if (a == b + c)
+		cout << a << '=' << b << '+' << c;
+	else if (a == b - c)
+		cout << a << '=' << b << '-' << c;
+	else if (a == b * c)
+		cout << a << '=' << b << '*' << c;
+	else if (a / b == c)
+		cout << a << '/' << b << '=' << c;
+	else
+		cout << a << '=' << b << '/' << c;
+		
 	return 0;
 }
