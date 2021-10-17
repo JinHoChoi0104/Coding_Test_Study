be81bcf368080974e6321a013bcaf4ccbb389025
JinHoChoi <bearsff@naver.com>
2021-10-17 21:09:44 +0900

    Day 142
    BAEKJOON 2744

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,28 +1,24 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
 
-	int a1, b1, c1, a2, b2, c2;
-	int ans1, ans2, ans3;
-	for (int i = 0; i < 3; i++) {
-		cin >> a1 >> b1 >> c1 >> a2 >> b2 >> c2;
-		if (c2 < c1) {
-			b2--;
-			c2 += 60;
+	string str, ans = "";
+	cin >> str;
+	for (int i = 0; i < str.size(); i++) {
+		char c = str[i];
+		if (c >= 'a' && c <= 'z') {
+			c -= 'a';
+			c += 'A';
 		}
-		ans3 = c2 - c1;
-		if (b2 < b1) {
-			a2--;
-			b2 += 60;
+		else {
+			c -= 'A';
+			c += 'a';
 		}
-		ans2 = b2 - b1;
-		ans1 = a2 - a1;
-
-		cout << ans1 << " " << ans2 << " " << ans3 << endl;
+		ans = ans + c;
 	}
+	cout << ans;
 	return 0;
 }
