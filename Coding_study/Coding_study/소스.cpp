1b90b3e43e241e76746bd48431e1a0f59dbf7106
JinHoChoi <bearsff@naver.com>
2021-10-19 21:55:50 +0900

    Day 144
    BAEKJOON 2720

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	string num;
-	cin >> num;
-	int tmp = 0, i = 0, len = num.size();
-	if (len % 3 == 1) {
-		cout << num[0];
-		i = 1;
-	}
-	else if (len % 3 == 2) {
-		cout << ((num[0] - '0') * 2) + (num[1] - '0');
-		i = 2;
-	}
-	for (; i < len; i += 3) {
-		cout << ((num[i] - '0') * 4) + ((num[i + 1] - '0') * 2) + (num[i + 2] - '0');
+	int num, T;
+	for (cin >> T; T-- > 0;) {
+		cin >> num;
+		int a=0, b=0, c=0, d=0;
+		while (num >= 25) {
+			num -= 25;
+			a++;
+		}
+		while (num >= 10) {
+			num -= 10;
+			b++;
+		}
+		while (num >= 5) {
+			num -= 5;
+			c++;
+		}
+		d = num;
+		cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	return 0;
 }
