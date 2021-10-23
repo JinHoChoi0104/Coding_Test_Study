dc0b9c67540c67eb7247859d154d1ef91a2a1b7b
JinHoChoi <bearsff@naver.com>
2021-10-23 11:10:47 +0900

    Day 148
    BAEKJOON 2684

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,20 +1,38 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
 	int T;
-	int a, b, c;
+	string str;
+	
 	for (cin >> T; T-- > 0;) {
-		cin >> a >> b >> c;
-		b -= c;
-		if (a < b)
-			cout << "advertise\n";
-		else if (a == b)
-			cout << "does not matter\n";
-		else
-			cout << "do not advertise\n";
+		vector<int> arr(8, 0);
+		cin >> str;
+		for (int i = 0; i < 38; i++) {
+			string tmp = str.substr(i, 3);
+			if (tmp == "TTT")
+				arr[0]++;
+			else if (tmp == "TTH")
+				arr[1]++;
+			else if (tmp == "THT")
+				arr[2]++;
+			else if (tmp == "THH")
+				arr[3]++;
+			else if (tmp == "HTT")
+				arr[4]++;
+			else if (tmp == "HTH")
+				arr[5]++;
+			else if (tmp == "HHT")
+				arr[6]++;
+			else
+				arr[7]++;
+		}
+		for (int i = 0; i < 8; i++)
+			cout << arr[i] << " ";
+		cout << "\n";
 	}
 	return 0;
 }
