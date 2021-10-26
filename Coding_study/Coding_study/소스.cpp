68a8cdd135c5635b1bce375c014f665087f12515
JinHoChoi <bearsff@naver.com>
2021-10-26 22:39:49 +0900

    Day 151
    BAEKJOON 2576

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int x, y, date=0;
-	cin >> x >> y;
-	
-	for (int i = 1; i < x; i++) {
-		if (i == 2)
-			date += 28;
-		else if (i == 4 || i == 6 || i == 9 || i == 11)
-			date += 30;
-		else
-			date += 31;
+	int num, min_num = 101, sum = 0;
+	for (int i = 0; i < 7; i++) {
+		cin >> num;
+		if (num % 2 == 0)
+			continue;
+		sum += num;
+		min_num = min(min_num, num);
 	}
-	date += y;
-	date %= 7;
-	string day[7] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
-	cout << day[date];
+	if (sum == 0)
+		cout << -1;
+	else
+		cout << sum <<"\n"<< min_num;
 
 	return 0;
 }
