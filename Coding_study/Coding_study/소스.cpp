1a0c699c52fc56fcf8ad1a83940dfd6480274abc
JinHoChoi <bearsff@naver.com>
2021-10-27 22:39:28 +0900

    Day 152
    BAEKJOON 1371

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
-#include <algorithm>
+#include <map>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int num, min_num = 101, sum = 0;
-	for (int i = 0; i < 7; i++) {
-		cin >> num;
-		if (num % 2 == 0)
-			continue;
-		sum += num;
-		min_num = min(min_num, num);
+	char c;
+	int min = 0;
+	map <char, int> m;
+	while (cin >> c) {
+		m[c]++;
+		if (m[c] > min)
+			min=m[c];
 	}
-	if (sum == 0)
-		cout << -1;
-	else
-		cout << sum <<"\n"<< min_num;
+	for (auto it = m.begin(); it != m.end(); it++)
+		if (it->second == min)
+			cout << it->first;
 
 	return 0;
 }
