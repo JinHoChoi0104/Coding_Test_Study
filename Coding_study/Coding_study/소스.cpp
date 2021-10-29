56a68780ba70b95cb0d15a294e2acfbe13304da4
JinHoChoi <bearsff@naver.com>
2021-10-29 23:10:13 +0900

    Day 154
    BAEKJOON 3047

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	string num;
-	long long mul1 = 0, mul2 = 0;
-	cin >> num;
-	for(int i=0; i<num.size(); i++)
-		mul1 += int(num[i] - '0');
-	cin >> num;
-	for (int i = 0; i < num.size(); i++)
-		mul2 += int(num[i] - '0');
-	cout << mul1 * mul2;
+	vector<int> arr;
+	int num;
+	for (int i = 0; i < 3; i++) {
+		cin >> num;
+		arr.push_back(num);
+	}
+	sort(arr.begin(), arr.end());
+	char c;
+	for (int i = 0; i < 3; i++) {
+		cin >> c;
+		if (c == 'A')
+			cout << arr[0] << " ";
+		else if ( c=='B')
+			cout << arr[1] << " ";
+		else
+			cout << arr[2] << " ";
+	}
 	return 0;
 }
