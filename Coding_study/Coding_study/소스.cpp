9b7f3b508b480f7e18b3f3afb148b9dd7699b3ce
JinHoChoi <bearsff@naver.com>
2021-11-01 19:35:38 +0900

    Day 157
    BAEKJOON 5555

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -5,19 +5,21 @@ using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	string str;
-	while (1) {
-		getline(cin, str);
-		if (str == "#")
-			break;
-		int cnt = 0;
-		for (int i = 0; i < str.size(); i++) {
-			if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
-				cnt++;
-			if (str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
-				cnt++;
-		}
-		cout << cnt << "\n";
+	string str, tmp;
+	cin >> str;
+	int T, cnt = 0;
+	for (cin >> T; T-- > 0;) {
+		cin >> tmp;
+		tmp = tmp + tmp; //첫번재 문자와 뒤 문자를 이어준다
+		/*
+		find 함수는 찾고자 하는 문자열의 주소를 반환한다.
+		만약 없을 경우에는 string::npos를 반환한다.
+		파라미터를 두개 줄 경우에는 
+		(찾고자 하는 문자열, 탐색을 시작하는 주소값)
+		*/
+		if (tmp.find(str) != string::npos) //존재한다면
+			cnt++; //카운트
 	}
+	cout << cnt;
 	return 0;
 }
