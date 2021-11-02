59f36f492f6d78b0ea66cd88cce8401dbc4686c2
JinHoChoi <bearsff@naver.com>
2021-11-02 22:14:46 +0900

    Day 158
    BAEKJOON 3986

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,24 +1,35 @@
 #include <iostream>
 #include <string>
+#include <stack>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	string str, tmp;
-	cin >> str;
+	string str;
 	int T, cnt = 0;
+	stack<char> s;
 	for (cin >> T; T-- > 0;) {
-		cin >> tmp;
-		tmp = tmp + tmp; //첫번재 문자와 뒤 문자를 이어준다
-		/*
-		find 함수는 찾고자 하는 문자열의 주소를 반환한다.
-		만약 없을 경우에는 string::npos를 반환한다.
-		파라미터를 두개 줄 경우에는 
-		(찾고자 하는 문자열, 탐색을 시작하는 주소값)
-		*/
-		if (tmp.find(str) != string::npos) //존재한다면
-			cnt++; //카운트
+		cin >> str;
+		bool isGood = true;
+		for (int i = 0; i < str.size(); i++) {
+			char tmp = str[i];
+			if (s.empty())  //비어 있다면 쌓아준다
+				s.push(tmp);
+			else { //스텍에 단어가 있다면
+				if (s.top() != tmp)  //가장 최상단의 단어가 다르다면
+					s.push(tmp);
+				else //단어가 같다면
+					s.pop();
+			}
+		}
+
+		if (s.empty()) //스텍이 비어있다 = 모두 짝을 찾았다
+			cnt++;
+		else {
+			while (!s.empty())
+				s.pop();
+		}
 	}
 	cout << cnt;
 	return 0;
