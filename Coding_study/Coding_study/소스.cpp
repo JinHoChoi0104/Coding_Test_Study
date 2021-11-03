ef14987832ad328b42e2d9e8465fcfa5799e90ef
JinHoChoi <bearsff@naver.com>
2021-11-03 22:03:53 +0900

    Day 159
    BAEKJOON 17390
    Segment Tree

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,36 +1,43 @@
 #include <iostream>
 #include <string>
-#include <stack>
+#include <vector>
+#include <algorithm>
 using namespace std;
+#define MAX_RANGE 300000
+vector<int> arr;
+vector<int> tree((MAX_RANGE+1) * 4);
+
+int init(int l, int r, int index) {
+	int &ret = tree[index];
+	if (l == r)
+		return ret = arr[l];
+	int mid = (l + r) / 2;
+	return ret = init(l, mid, index * 2) + init(mid + 1, r, index * 2 + 1);
+}
+
+int getRangeSum(int l, int r, int index, int start, int end) {
+	if (start > r || end < l) return 0; //범위 벗어나면 0
+	if (start <= l && end >= r) return tree[index]; //범위 안이면 리턴
+	int mid = (l + r) / 2;
+	return getRangeSum(l, mid, index * 2, start, end) + getRangeSum(mid + 1, r, index * 2 + 1, start, end);
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	string str;
-	int T, cnt = 0;
-	stack<char> s;
-	for (cin >> T; T-- > 0;) {
-		cin >> str;
-		bool isGood = true;
-		for (int i = 0; i < str.size(); i++) {
-			char tmp = str[i];
-			if (s.empty())  //비어 있다면 쌓아준다
-				s.push(tmp);
-			else { //스텍에 단어가 있다면
-				if (s.top() != tmp)  //가장 최상단의 단어가 다르다면
-					s.push(tmp);
-				else //단어가 같다면
-					s.pop();
-			}
-		}
-
-		if (s.empty()) //스텍이 비어있다 = 모두 짝을 찾았다
-			cnt++;
-		else {
-			while (!s.empty())
-				s.pop();
-		}
+	int N, Q, num;
+	cin >> N >> Q;
+	for (int i = 0; i < N; i++) {
+		cin >> num;
+		arr.push_back(num);
 	}
-	cout << cnt;
+	sort(arr.begin(), arr.end());
+	init(0, N-1, 1);
+	while (Q-- > 0) {
+		int to, from;
+		cin >> to >> from;
+		cout << getRangeSum(0, N-1, 1, to-1, from-1) << "\n";
+	}
+
 	return 0;
 }
