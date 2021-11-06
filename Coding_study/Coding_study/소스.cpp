0670399ba0961c609558c27c123b0b20f399835c
JinHoChoi <bearsff@naver.com>
2021-11-06 16:36:59 +0900

    Day 162
    BAEKJOON 2754

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
-#include <vector>
+#include <map>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int N, K, num;
-	char c;
-	cin >> N >> K;
-	vector<int> arr;
-	for (int i = 1; i < N; i++) {
-		cin >> num >> c;
-		arr.push_back(num);
+	map<char, double> grade;
+	grade['A'] = 4.0, grade['B'] = 3.0, grade['C'] = 2.0, grade['D'] = 1.0;
+
+	char c1, c2;
+	cin >> c1;
+	if (c1 == 'F') {
+		cout << fixed;
+		cout.precision(1);
+		cout << 0.0;
+		return 0;
 	}
-	cin >> num;
-	arr.push_back(num);
-	for (int i = 1; i <= K; i++) {
-		vector<int>tmp;
-		for (int j = 0; j < N - i; j++) 
-			tmp.push_back(arr[j + 1] - arr[j]);
-		arr.swap(tmp);
-	}
-	auto it = arr.begin();
-	cout << *it;
-	it++;
-	for (; it != arr.end(); it++)
-		cout << "," << *it;
+	cin >> c2;
+	double ans = grade[c1];
+	if (c2 == '+')
+		ans += 0.3;
+	else if(c2=='-')
+		ans -= 0.3;
+
+	cout << fixed;
+	cout.precision(1);
+	cout << ans;
 	return 0;
 }
