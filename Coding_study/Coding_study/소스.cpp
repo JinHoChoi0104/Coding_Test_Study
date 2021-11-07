fbe562edda26db000855325cd1f76d4364c6b082
JinHoChoi <bearsff@naver.com>
2021-11-07 16:31:18 +0900

    Day 163
    BAEKJOON 10815

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,29 +1,22 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <set>
 using namespace std;
 
-vector<int> arr;
-//누적합
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int N, Q, num, to, from;
-	cin >> N >> Q;
-	for (int i = 0; i < N; i++) {
+	int N, M, num;
+	set<int> arr;
+	for (cin >> N; N-- > 0;) {
 		cin >> num;
-		arr.push_back(num);
+		arr.insert(num);
 	}
-	sort(arr.begin(), arr.end());
-	for (int i = 1; i < N; i++) //누적합으로 전환
-		arr[i] += arr[i - 1];
-	
-	while (Q-- > 0) {
-		cin >> to >> from;
-		if (to == 1) //처음부터 구하는 경우
-			cout << arr[from - 1] << "\n";
-		else //중간부터 구하는 경우
-			cout << arr[from - 1] - arr[to - 2] << "\n";
+	for (cin >> M; M-- > 0; ) {
+		cin >> num;
+		if (arr.find(num) != arr.end())
+			cout << "1 ";
+		else
+			cout << "0 ";
 	}
 
 	return 0;
