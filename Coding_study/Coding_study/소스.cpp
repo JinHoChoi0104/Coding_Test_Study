55be1ec194df63b5c6e7799f7e7358677b5b21b7
JinHoChoi <bearsff@naver.com>
2021-11-11 20:41:07 +0900

    Day 167
    BAEKJOON 1268

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+int adj[1001][6];
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
+
+	int N;
+	cin >> N;
+	for (int i = 1; i<= N; i++) //번호
+		for (int j = 1; j <= 5; j++) //학년
+			cin >> adj[i][j];
 	
-	int t, N, M, a, b;
-	for (cin >> t; t-- > 0;) {
-		cin >> N >> M;
-		for (int i = 0; i < M; i++) 
-			cin >> a >> b;
-		cout << N - 1 << "\n";
+	int ans=1, max_friend = 0;
+	for (int i = 1; i <= N; i++) { //번호
+		int tmp = 0;
+		for (int p = 1; p <= N; p++) {
+			for (int q = 1; q <= 5; q++) {
+				if (adj[i][q] == adj[p][q]) {
+					tmp++;
+					break;
+				}
+			}
+		}
+		if (tmp > max_friend) {
+			max_friend = tmp;
+			ans = i;
+		}
 	}
 
+	cout << ans;
 	return 0;
 }
