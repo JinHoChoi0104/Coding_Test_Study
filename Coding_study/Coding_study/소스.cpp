9536185044e6dcf84b07c985cf8aba9e5905c99f
JinHoChoi <bearsff@naver.com>
2021-11-13 00:07:05 +0900

    Day 168
    BAEKJOON 5568

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,36 +1,30 @@
 #include <iostream>
+#include <set>
 #include <vector>
+#include <algorithm>
 using namespace std;
-
-int adj[1001][6];
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-
-	int N;
-	cin >> N;
-	for (int i = 1; i<= N; i++) //번호
-		for (int j = 1; j <= 5; j++) //학년
-			cin >> adj[i][j];
-	
-	int ans=1, max_friend = 0;
-	for (int i = 1; i <= N; i++) { //번호
-		int tmp = 0;
-		for (int p = 1; p <= N; p++) {
-			for (int q = 1; q <= 5; q++) {
-				if (adj[i][q] == adj[p][q]) {
-					tmp++;
-					break;
-				}
-			}
-		}
-		if (tmp > max_friend) {
-			max_friend = tmp;
-			ans = i;
-		}
+	set<string> ans;
+	vector<string> arr;
+	int n, k;
+	cin >> n >> k;
+	string num;
+	for (int i = 0; i < n; i++) { 
+		cin >> num;
+		arr.push_back(num);
 	}
+	sort(arr.begin(), arr.end());
+		
+	do {
+		string tmp="";
+		for (int i = 0; i < k; i++)
+			tmp = tmp + arr[i];
+		ans.insert(tmp);
+	} while (next_permutation(arr.begin(), arr.end()));
 
-	cout << ans;
+	cout << ans.size();
 	return 0;
 }
