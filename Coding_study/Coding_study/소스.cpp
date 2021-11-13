1de1c56d5a7e14b295c2dc43969981c25da1f5cd
JinHoChoi <bearsff@naver.com>
2021-11-13 17:10:11 +0900

    Day 169
    BAEKJOON 2143

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,30 +1,53 @@
 #include <iostream>
-#include <set>
 #include <vector>
-#include <algorithm>
+#include <map>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	set<string> ans;
-	vector<string> arr;
-	int n, k;
-	cin >> n >> k;
-	string num;
-	for (int i = 0; i < n; i++) { 
-		cin >> num;
-		arr.push_back(num);
-	}
-	sort(arr.begin(), arr.end());
-		
-	do {
-		string tmp="";
-		for (int i = 0; i < k; i++)
-			tmp = tmp + arr[i];
-		ans.insert(tmp);
-	} while (next_permutation(arr.begin(), arr.end()));
 
-	cout << ans.size();
+	map<int, int> A, B;
+	int T, n, m, num;
+	cin >> T >> n >> num;
+	vector<int> arr;
+	arr.push_back(num);
+	for (int i = 1; i < n; i++) {
+		cin >> num;
+		arr.push_back(arr.back() + num);
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = i; j < n; j++) {
+			if (i == 0)
+				A[arr[j]]++;
+			else 
+				A[arr[j] - arr[i - 1]]++;
+		}
+	}
+
+	cin >> m >> num;
+	arr.clear();
+	arr.push_back(num);
+	for (int i = 1; i < m; i++) {
+		cin >> num;
+		arr.push_back(arr.back() + num);
+	}
+	for (int i = 0; i < m; i++) {
+		for (int j = i; j < m; j++) {
+			if (i == 0)
+				B[arr[j]]++;
+			else
+				B[arr[j] - arr[i - 1]]++;
+		}
+	}
+
+	long long ans = 0;
+	for (auto it = A.begin(); it != A.end(); it++) {
+		int A_sum = it->first;
+		ans += (long long) it->second * B[T - A_sum];
+	}
+
+	cout << ans;
+
 	return 0;
 }
