3990065971ec8b7c1a1482806e08a11861edafe9
JinHoChoi <bearsff@naver.com>
2021-11-14 10:58:42 +0900

    Day 170
    BAEKJOON 2239

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,53 +1,73 @@
 #include <iostream>
 #include <vector>
-#include <map>
 using namespace std;
 
+void showArr(vector<vector<int>>& arr);
+void makeNum(vector<vector<int>>& arr, int x, int y);
+void makeSudoku(vector<vector<int>> arr, int x, int y, int num);
+bool is_done = false;
+
+void showArr(vector<vector<int>>& arr) {
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++)
+			cout << arr[i][j];
+		cout << "\n";
+	}
+}
+
+void makeNum(vector<vector<int>> &arr, int x, int y) {
+	vector<bool> check(10, false);
+
+	//check row
+	for (int i = 0; i < 9; i++)
+		check[arr[x][i]] = true;
+	//check col
+	for (int i = 0; i < 9; i++)
+		check[arr[i][y]] = true;
+	//check box
+	int p = x/3, q = y/3;
+	p *= 3, q *= 3;
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++)
+			check[arr[p + i][q + j]] = true;
+
+	//type sudoku
+	for(int i=1; i<=9; i++)
+		if (!check[i] && !is_done) {
+			makeSudoku(arr, x, y, i);
+		}
+}
+
+void makeSudoku(vector<vector<int>> arr, int x, int y, int num) {
+	arr[x][y] = num;
+	for(int i=x; i< 9; i++)
+		for (int j = 0; j < 9; j++) {
+			if (arr[i][j] == 0) {
+				makeNum(arr, i, j);
+				return;
+			}
+		}
+
+	is_done = true;
+	showArr(arr);
+}
+
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL), cout.tie(NULL);
-
-	map<int, int> A, B;
-	int T, n, m, num;
-	cin >> T >> n >> num;
-	vector<int> arr;
-	arr.push_back(num);
-	for (int i = 1; i < n; i++) {
-		cin >> num;
-		arr.push_back(arr.back() + num);
-	}
-	for (int i = 0; i < n; i++) {
-		for (int j = i; j < n; j++) {
-			if (i == 0)
-				A[arr[j]]++;
-			else 
-				A[arr[j] - arr[i - 1]]++;
+	vector<vector<int>> arr(9,vector<int>(9));
+	
+	int x=-1, y=-1;
+	for (int i = 0; i < 9; i++)
+		for (int j = 0; j < 9; j++) {
+			scanf("%1d", &arr[i][j]);
+			if (arr[i][j] == 0 && x == -1)
+				x = i, y = j;
 		}
+
+	if (x == -1) {
+		showArr(arr);
+		return 0;
 	}
 
-	cin >> m >> num;
-	arr.clear();
-	arr.push_back(num);
-	for (int i = 1; i < m; i++) {
-		cin >> num;
-		arr.push_back(arr.back() + num);
-	}
-	for (int i = 0; i < m; i++) {
-		for (int j = i; j < m; j++) {
-			if (i == 0)
-				B[arr[j]]++;
-			else
-				B[arr[j] - arr[i - 1]]++;
-		}
-	}
-
-	long long ans = 0;
-	for (auto it = A.begin(); it != A.end(); it++) {
-		int A_sum = it->first;
-		ans += (long long) it->second * B[T - A_sum];
-	}
-
-	cout << ans;
-
+	makeNum(arr, x, y);
 	return 0;
 }
