584214f7c028be8eb14c1428d3db93334e712dd3
JinHoChoi <bearsff@naver.com>
2021-11-15 21:24:52 +0900

    Day 171
    BAEKJOON 11068

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -2,72 +2,40 @@
 #include <vector>
 using namespace std;
 
-void showArr(vector<vector<int>>& arr);
-void makeNum(vector<vector<int>>& arr, int x, int y);
-void makeSudoku(vector<vector<int>> arr, int x, int y, int num);
-bool is_done = false;
-
-void showArr(vector<vector<int>>& arr) {
-	for (int i = 0; i < 9; i++) {
-		for (int j = 0; j < 9; j++)
-			cout << arr[i][j];
-		cout << "\n";
+bool checkPalindrome(vector<int> &num) { //회문 여부 판단
+	int i = 0, j = num.size() - 1;
+	while (i < j) {
+		if(num[i++]!=num[j--])
+			return false;
 	}
+	return true;
 }
 
-void makeNum(vector<vector<int>> &arr, int x, int y) {
-	vector<bool> check(10, false);
-
-	//check row
-	for (int i = 0; i < 9; i++)
-		check[arr[x][i]] = true;
-	//check col
-	for (int i = 0; i < 9; i++)
-		check[arr[i][y]] = true;
-	//check box
-	int p = x/3, q = y/3;
-	p *= 3, q *= 3;
-	for (int i = 0; i < 3; i++)
-		for (int j = 0; j < 3; j++)
-			check[arr[p + i][q + j]] = true;
-
-	//type sudoku
-	for(int i=1; i<=9; i++)
-		if (!check[i] && !is_done) {
-			makeSudoku(arr, x, y, i);
-		}
-}
-
-void makeSudoku(vector<vector<int>> arr, int x, int y, int num) {
-	arr[x][y] = num;
-	for(int i=x; i< 9; i++)
-		for (int j = 0; j < 9; j++) {
-			if (arr[i][j] == 0) {
-				makeNum(arr, i, j);
-				return;
-			}
-		}
-
-	is_done = true;
-	showArr(arr);
-}
 
 int main() {
-	vector<vector<int>> arr(9,vector<int>(9));
-	
-	int x=-1, y=-1;
-	for (int i = 0; i < 9; i++)
-		for (int j = 0; j < 9; j++) {
-			scanf("%1d", &arr[i][j]);
-			if (arr[i][j] == 0 && x == -1)
-				x = i, y = j;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	int T, num;
+	for (cin >> T; T-- > 0;) {
+		cin >> num;
+		bool is_Palin = false;
+		int i = 1;
+		while( i++ < 64 && !is_Palin) {
+			//진법 변환
+			int tmp = num;
+			vector<int> ans;
+			while (tmp > 0) {
+				ans.push_back(tmp%i);
+				tmp /= i;
+			}
+			is_Palin = checkPalindrome(ans);;
+			if (is_Palin) //회문이면 브레이크
+				break;
 		}
-
-	if (x == -1) {
-		showArr(arr);
-		return 0;
+		if (is_Palin)
+			cout << "1\n";
+		else
+			cout << "0\n";
 	}
-
-	makeNum(arr, x, y);
 	return 0;
 }
