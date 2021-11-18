99e7d9eda8a10eb8e45d27ecc3e110578b0cf478
JinHoChoi <bearsff@naver.com>
2021-11-18 23:59:36 +0900

    Day 174
    BAEKJOON 1072

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
-#include <set>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	set<int> day, arr;
-	int N, C, num;
-	cin >> N >> C;
-	for (int i = 0; i < N; i++) {
-		cin >> num;
-		if (day.find(num) != day.end())
-			continue;
-		if (num > C)
-			continue;
-		for (int j = num; j <= C; j += num)
-			day.insert(j);
+	long long X, Y;
+	cin >> X >> Y;
+	int Z = Y * 100 / X;
+	if (X == Y || Z == 99) {
+		cout << "-1";
+		return 0;
 	}
-	cout << day.size();
+	long long left = 0, right = X, mid, num, ans;
+	while (left <= right) {
+		mid = (left + right) / 2;
+		num = (Y+mid) * 100 / (X+mid);
+		if (num != Z) {
+			ans = mid;
+			right = mid - 1;
+		}
+		else
+			left = mid + 1;
+	}
+	cout << ans;
 	return 0;
 }
