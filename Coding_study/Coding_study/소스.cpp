4275b70a0691fe06a70882123a95d8917f5d8a29
JinHoChoi <bearsff@naver.com>
2021-11-22 23:20:34 +0900

    Day 179
    BAEKJOON 13398

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -3,56 +3,28 @@
 #include <vector>
 using namespace std;
 
-struct OptimizedDisjointSet {
-	vector<int> parent, rank;
-	OptimizedDisjointSet(int n) : parent(n), rank(n, 1) {
-		for (int i = 0; i < n; i++)
-			parent[i] = i;
-	}
-	int find(int u) {
-		if (u == parent[u]) return u;
-		return parent[u] = find(parent[u]); //경로 압축
-	}
-
-	void merge(int u, int v) {
-		u = find(u), v = find(v);
-		if (u == v) return; //같은 소속이면 합치지 않는다
-		if (rank[u] > rank[v]) swap(u, v); //v의 랭크(높이)가 더 커지게 한다.
-		parent[u] = v; //u를 v의 자식으로 넣는다.
-		if (rank[u] == rank[v]) rank[v]++;
-	}
-};
-
-int V; //노드 개수
-vector<pair<int, pair<int, int>>> edges;
-int kruskal() {
-	int ans = 0, cnt = 0; //길 유지비, 포함 된 집의 수
-	sort(edges.begin(), edges.end()); // 가중치의 크기에 따라 오름차순 정렬
-
-	OptimizedDisjointSet sets(V + 1);
-	for (int i = 0; i < edges.size(); i++) {
-		int cost = edges[i].first;
-		int u = edges[i].second.first, v = edges[i].second.second;
-		if (sets.find(u) == sets.find(v)) continue; //u와 v가 같은 소속이면 패스
-		sets.merge(u, v);
-		ans += cost;
-		cnt++;
-		if (cnt == V - 2) //필요한 간선은 '전체 노드 - 2개'이다.
-			break;
-	}
-	return ans;
-}
-
+int dp[100000][2];//제외한게 없는 경우, 제외한 경우의 최댓값
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int E, u, v, cost;
-	cin >> V >> E;
-	for (int i = 0; i < E; i++) {
-		cin >> u >> v >> cost;
-		edges.push_back({ cost, { u, v } });
+	int num, N;
+	cin >> N;
+	vector<int> arr;
+	for (int i = 0; i < N; i++) {
+		cin >> num;
+		arr.push_back(num);
 	}
 
-	cout << kruskal();
+	N = arr.size();
+	dp[0][0] = arr[0];
+	dp[0][1] = arr[0];
+	int ans = arr[0];
+	for (int i = 1; i < N; i++) {
+		dp[i][0] = max(dp[i - 1][0] + arr[i], arr[i]);
+		dp[i][1] = max(dp[i - 1][0], dp[i - 1][1] + arr[i]);
+		//i번째 선택 안하기 vs i번째 선택하기
+		ans = max(ans, max(dp[i][0], dp[i][1]));
+	}
+	cout << ans;
 	return 0;
 }
