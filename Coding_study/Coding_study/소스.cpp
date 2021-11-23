527dea0644749c5b856a842ffac479b81e8560a4
JinHoChoi <bearsff@naver.com>
2021-11-23 23:57:59 +0900

    Day 180
    BAEKJOON 1357

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,30 +1,19 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
+#include <string>
 using namespace std;
 
-int dp[100000][2];//제외한게 없는 경우, 제외한 경우의 최댓값
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int num, N;
-	cin >> N;
-	vector<int> arr;
-	for (int i = 0; i < N; i++) {
-		cin >> num;
-		arr.push_back(num);
-	}
-
-	N = arr.size();
-	dp[0][0] = arr[0];
-	dp[0][1] = arr[0];
-	int ans = arr[0];
-	for (int i = 1; i < N; i++) {
-		dp[i][0] = max(dp[i - 1][0] + arr[i], arr[i]);
-		dp[i][1] = max(dp[i - 1][0], dp[i - 1][1] + arr[i]);
-		//i번째 선택 안하기 vs i번째 선택하기
-		ans = max(ans, max(dp[i][0], dp[i][1]));
-	}
-	cout << ans;
+	string str1, str2;
+	cin >> str1 >> str2;
+	reverse(str1.begin(), str1.end());
+	reverse(str2.begin(), str2.end());
+	int num1 = stoi(str1), num2 = stoi(str2);
+	string ans = to_string(num1 + num2);
+	reverse(ans.begin(), ans.end());
+	int ans_num = stoi(ans);
+	cout << ans_num;
 	return 0;
 }
