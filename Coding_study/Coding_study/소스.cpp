b034409a4c68956a0c17061e61d6108224cc73ec
JinHoChoi <bearsff@naver.com>
2021-11-24 23:14:26 +0900

    Day 181
    BAEKJOON 2738

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
-#include <algorithm>
-#include <string>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	string str1, str2;
-	cin >> str1 >> str2;
-	reverse(str1.begin(), str1.end());
-	reverse(str2.begin(), str2.end());
-	int num1 = stoi(str1), num2 = stoi(str2);
-	string ans = to_string(num1 + num2);
-	reverse(ans.begin(), ans.end());
-	int ans_num = stoi(ans);
-	cout << ans_num;
+	int N, M, arr[100][100], num;
+	cin >> N >> M;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
+			cin >> arr[i][j];
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++) {
+			cin >> num;
+			arr[i][j] += num;
+		}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++)
+			cout << arr[i][j] << " ";
+		cout << "\n";
+	}
+
 	return 0;
 }
