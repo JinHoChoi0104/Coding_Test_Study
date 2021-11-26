392fe37cef795f035bed5ed1e4cb8d249894e115
JinHoChoi <bearsff@naver.com>
2021-11-26 23:44:44 +0900

    Day 183
    BAEKJOON 2954

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,30 +1,16 @@
 #include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int N, M, L, cnt = -1, index = 0;
-	cin >> N >> M >> L;
-	vector<int>arr(50, 0);
-	while (!arr.empty()) {
-		int &tmp = arr[index];
-		tmp++, cnt++;
-		if (tmp % 2 == 1) {
-			index += L;
-			if (index >= N)
-				index -= N;
-		}
-		else {
-			index -= L;
-			if (index < 0)
-				index += N;
-		}
-		if (tmp == M) {
-			cout << cnt;
-			return 0;
-		}
+	string str;
+	getline(cin, str);
+	for (int i = 0; i < str.size(); i++) {
+		cout << str[i];
+		if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+			i += 2;
 	}
 	return 0;
 }
