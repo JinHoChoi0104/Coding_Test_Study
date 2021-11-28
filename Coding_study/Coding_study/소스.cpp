baf7938d5cdfa3863ff2f4e361d97ca95f076aff
JinHoChoi <bearsff@naver.com>
2021-11-28 19:40:43 +0900

    Day 185
    BAEKJOON 15829

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 using namespace std;
-
+#define M 1234567891
+#define ll long long
+ll LongPow(ll num, int n) {
+	ll ans = 1;
+	for (int i = 0; i < n; i++) {
+		ans *= num;
+		ans %= M;
+	}
+	return ans;
+}
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
 	int N;
-	cin >> N;
-	cout << N*(N-1)*(N-2)*(N - 3) / 24;
+	string str;
+	cin >> N >> str;
+	ll num = 0;
+	for (int i = 0; i < N; i++) {
+		ll tmp = str[i] - 'a' + 1;
+		num += tmp * LongPow(31, i);
+		num %= M;
+	}
+	cout << num;
 	return 0;
-}
+} 
