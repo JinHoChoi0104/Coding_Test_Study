230035d1daa2e24efee07dab3a5c068b4fb0a3fb
JinHoChoi <bearsff@naver.com>
2021-11-30 23:42:12 +0900

    Day 187
    BAEKJOON 1145

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,26 +1,21 @@
 #include <iostream>
-#include <math.h>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	
-	int t;
-	for (cin >> t; t-- > 0;) {
-		char c;
-		int tmp1 = 0, tmp2 = 0;
-		for (int i = 0; i < 3; i++) {
-			cin >> c;
-			tmp1 += (c - 'A') * pow(26,2-i);
-		}
-		cin >> c;
-		cin >> tmp2;
-
-		if (abs(tmp1 - tmp2) <= 100)
-			cout << "nice\n";
-		else
-			cout << "not nice\n";
+	int arr[5], num = 1;
+	for (int i = 0; i < 5; i++) 
+		cin >> arr[i];
+	while (1) {
+		int tmp = 0;
+		for (int i = 0; i < 5; i++) 
+			if (num % arr[i] == 0)
+				tmp++;
+		if (tmp >= 3)
+			break;
+		num++;
 	}
+	cout << num;
 	return 0;
 }	
