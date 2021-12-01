454f46db1bf48e36a9d8e93193993bc03889b9c7
JinHoChoi <bearsff@naver.com>
2021-12-01 22:28:59 +0900

    Day 188
    BAEKJOON 1718

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int arr[5], num = 1;
-	for (int i = 0; i < 5; i++) 
-		cin >> arr[i];
-	while (1) {
-		int tmp = 0;
-		for (int i = 0; i < 5; i++) 
-			if (num % arr[i] == 0)
-				tmp++;
-		if (tmp >= 3)
-			break;
-		num++;
+	string str1, str2;
+	getline(cin, str1);
+	cin >>  str2;
+	int len = str2.size();
+	for (int i = 0; i < str1.size(); i++) {
+		if(str1[i] == ' '){
+			cout << " ";
+			continue;
+		}
+		int tmp = str1[i] - str2[i % len];
+		if (tmp > 26)
+			tmp -= 26;
+		else if (tmp <= 0)
+			tmp += 26;
+		cout << char(tmp + 'a' - 1);
 	}
-	cout << num;
+	
 	return 0;
 }	
