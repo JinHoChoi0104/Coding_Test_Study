6af2a07afaf3e566ceb127c47169b433f31fa191
JinHoChoi <bearsff@naver.com>
2021-12-02 23:49:57 +0900

    Day 189
    BAEKJOON 2525

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,26 +1,18 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	string str1, str2;
-	getline(cin, str1);
-	cin >>  str2;
-	int len = str2.size();
-	for (int i = 0; i < str1.size(); i++) {
-		if(str1[i] == ' '){
-			cout << " ";
-			continue;
-		}
-		int tmp = str1[i] - str2[i % len];
-		if (tmp > 26)
-			tmp -= 26;
-		else if (tmp <= 0)
-			tmp += 26;
-		cout << char(tmp + 'a' - 1);
+	int a, b, c;
+	cin >> a >> b >> c;
+	b += c;
+	if (b >= 60) {
+		a += b / 60;
+		b %= 60;
+		if (a >= 24)
+			a -= 24;
 	}
-	
+	cout << a << " " << b;
 	return 0;
 }	
