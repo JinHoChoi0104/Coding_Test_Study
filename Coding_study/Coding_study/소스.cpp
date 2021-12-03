a22ad6ab14032886227210f0a0c93de132ff8a6f
JinHoChoi <bearsff@naver.com>
2021-12-03 18:21:47 +0900

    Day 190
    BAEKJOON 5596

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -4,15 +4,20 @@ using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int a, b, c;
-	cin >> a >> b >> c;
-	b += c;
-	if (b >= 60) {
-		a += b / 60;
-		b %= 60;
-		if (a >= 24)
-			a -= 24;
+	int a, b, num;
+	cin >> a;
+	for (int i = 0; i < 3; i++) {
+		cin >> num;
+		a += num;
 	}
-	cout << a << " " << b;
+	cin >> b;
+	for (int i = 0; i < 3; i++) {
+		cin >> num;
+		b += num;
+	}
+	if (a > b)
+		cout << a;
+	else
+		cout << b;
 	return 0;
 }	
