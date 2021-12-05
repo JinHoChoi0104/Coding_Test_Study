2409b18535c298da6461e08026178ff090f7f949
JinHoChoi <bearsff@naver.com>
2021-12-05 17:51:44 +0900

    Day 192
    BAEKJOON 2921

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,31 +1,16 @@
 #include <iostream>
-#include <queue>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int n, num;
-	priority_queue<int>pq;
-	for (cin >> n; n-- > 0;) {
-		cin >> num;
-		pq.push(num);
+	int n, num1 = 3, num2=6, ans=0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		ans += num1;
+		num1 += num2;
+		num2 += 3;
 	}
-	int index, a, b;
-	cin >> index;
-	while (pq.top() >= index) {
-		if (pq.top() == index) {
-			cout << 0;
-			return 0;
-		}
-		b = pq.top();
-		pq.pop();
-		if (pq.empty()) {
-			a = 0;
-			break;
-		}
-		a = pq.top();
-	}
-	cout << (b - index) * (index - a) - 1;
+	cout << ans;
 	return 0;
 }	
