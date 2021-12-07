b4db6463008bfe700c417db86fb52248a013ce17
JinHoChoi <bearsff@naver.com>
2021-12-08 00:32:15 +0900

    Day 194
    BAEKJOON 5532

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,31 +1,21 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
-
-int ans = 0;
-void getEnergy(vector<int>arr, int index, int power) {
-	power += arr[index - 1] * arr[index + 1];
-	arr.erase(arr.begin()+index);
-	if (arr.size() == 2) {
-		ans = max(ans, power);
-		return;
-	}
-	for (int i = 1; i < arr.size() - 1; i++)
-		getEnergy(arr, i, power);
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int n, num;
-	vector<int>arr;
-	for (cin >> n; n-- > 0;) {
-		cin >> num;
-		arr.push_back(num);
-	}
-	for (int i = 1; i < arr.size() - 1; i++)
-		getEnergy(arr, i, 0);
-	cout << ans;
+	int a, b, c, d, num;
+	cin >> num >> a >> b >> c >> d;
+	int tmp1, tmp2;
+	a += (c - 1);
+	tmp1 = a / c;
+	tmp1 = num - tmp1;
+	b += (d - 1);
+	tmp2 = b / d;
+	tmp2 = num - tmp2;
+
+	num = min(tmp1, tmp2);
+	cout << num;
 	return 0;
 }
