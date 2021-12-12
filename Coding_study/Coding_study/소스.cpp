fbf3f2d5afce63a4903bd496030c62c51e21790d
JinHoChoi <bearsff@naver.com>
2021-12-12 16:41:04 +0900

    Day 199
    BAEKJOON 2167

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -4,22 +4,22 @@ using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int n, m, r, c;
-	cin >> n >> m >> r >> c;
-	char ch[50][50];
+	int n, m;
+	cin >> n >> m;
+	int arr[300][300];
 	for (int i = 0; i < n; i++) 
 		for (int j = 0; j < m; j++) 
-			cin >> ch[i][j];
-	
-	for (int i = 0; i < n; i++) {
-		for (int x = 0; x < r; x++) {
-			for (int j = 0; j < m; j++) {
-				for(int y = 0; y < c; y++)
-					cout << ch[i][j];
+			cin >> arr[i][j];
+	int t, x1, y1, x2, y2;
+	for (cin >> t; t-- > 0;) {
+		cin >> x1 >> y1 >> x2 >> y2;
+		int tmp = 0;
+		for (int i = x1-1; i < x2; i++) {
+			for (int j = y1-1; j < y2; j++) {
+				tmp += arr[i][j];
 			}
-			cout << "\n";
 		}
+		cout << tmp << "\n";
 	}
-
 	return 0;
 }
