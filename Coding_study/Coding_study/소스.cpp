c562599a2dc2ca377a700baece3fbf431fbdaf82
JinHoChoi <bearsff@naver.com>
2021-12-15 02:20:10 +0900

    Day 201
    BAEKJOON 10156

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -4,19 +4,11 @@ using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int t;
-	string str;
-	cin >> t;
-	for (int i = 1; i <= t ; i++) {
-		cin >> str;
-		cout << "String #" << i << "\n";
-		for (int j = 0; j < str.size(); j++) {
-			if (str[j] == 'Z')
-				str[j] = 'A';
-			else
-				str[j]++;
-		}
-		cout << str << "\n\n";
-	}
+	int a, b, c, num;
+	cin >> a >> b >> c;
+	if (a * b - c > 0)
+		cout << a * b - c;
+	else
+		cout << 0;
 	return 0;
 }
