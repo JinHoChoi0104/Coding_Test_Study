b3b62b51ef2fdafe91feb1cffd3a0e00002f43d2
JinHoChoi <bearsff@naver.com>
2021-12-15 21:33:36 +0900

    Day 202
    BAEKJOON 2966

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int a, b, c, num;
-	cin >> a >> b >> c;
-	if (a * b - c > 0)
-		cout << a * b - c;
-	else
-		cout << 0;
+	string str, arr[3] = { "ABCABCABCABC", "BABCBABCBABC", "CCAABBCCAABB" }, name[3] = {"Adrian", "Bruno", "Goran"};
+	int n, num[3] = {0,0,0};
+	int max_num = 0;
+	cin >> n >> str;
+	for (int i = 0; i < n; i++) {
+		int index = i % 12;
+		for (int j = 0; j < 3; j++) 
+			if (arr[j][index] == str[i])
+				num[j]++;
+	}
+	for (int i = 0; i < 3; i++) 
+		max_num = max(max_num, num[i]);
+	cout << max_num << "\n";
+	for (int i = 0; i < 3; i++)
+		if (max_num == num[i])
+			cout << name[i] << "\n";
 	return 0;
 }
