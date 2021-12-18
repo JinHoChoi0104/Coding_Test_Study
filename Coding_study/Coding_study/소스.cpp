f1ece028152dc306e8afdc36462c94e6b8f0ba95
JinHoChoi <bearsff@naver.com>
2021-12-18 23:34:54 +0900

    Day 205
    BAEKJOON 1350

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int T, num;
+	long long T, num, cnt = 0;
+	vector<long long> arr;
 	for (cin >> T; T-- > 0;) {
 		cin >> num;
-		int cnt = 0;
-		while (num > 0) {
-			if (num % 2 == 1)
-				cout << cnt <<" ";
-			cnt++;
-			num /= 2;
-		}
-		cout << "\n";
+		arr.push_back(num);
 	}
+	cin >> num;
+	for (int i = 0; i < arr.size(); i++) {
+		int tmp = arr[i];
+		tmp += num - 1;
+		tmp /= num;
+		cnt += tmp;
+	}
+	cout << cnt * num;
 	return 0;
 }
