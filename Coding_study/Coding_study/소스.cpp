55444b429532581cc467b081b7e4b075dd89fce4
JinHoChoi <bearsff@naver.com>
2021-12-21 00:11:22 +0900

    Day 207
    BAEKJOON 4641

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,30 +1,27 @@
 #include <iostream>
-#include <queue>
+#include <vector>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int n, m, num;
-	cin >> n >> m;
-	priority_queue<int> pq;
-	priority_queue<int, vector<int>,greater<int>> concent;
-	for (int i = 0; i < n; i++) {
-		cin >> num;
-		pq.push(num);
+	int num = 1;
+	while (1) {
+		vector<bool>tmp(101,false);
+		int cnt = 0;
+		while (1) {
+			cin >> num;
+			if (num < 1)
+				break;	
+			tmp[num] = true;
+		}
+		if (num == -1)
+			break;
+		for (int i = 1; i <= 50; i++)
+			if (tmp[i] && tmp[i * 2])
+				cnt++;
+		cout << cnt << "\n";
 	}
-	for (int i = 0; i < m; i++)
-		concent.push(0);
-	while (!pq.empty()) {
-		int tmp = concent.top() + pq.top();
-		concent.pop(), pq.pop();
-		concent.push(tmp);
-	}
-	int ans;
-	while (!concent.empty()) {
-		ans = concent.top();
-		concent.pop();
-	}
-	cout << ans;
+	
 	return 0;
 }
