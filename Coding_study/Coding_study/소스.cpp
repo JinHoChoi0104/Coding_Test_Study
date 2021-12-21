f58ef4c58c40933db088a952b9501c5c676f3c49
JinHoChoi <bearsff@naver.com>
2021-12-21 20:48:05 +0900

    Day 208
    BAEKJOON 1312

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,27 +1,17 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int num = 1;
-	while (1) {
-		vector<bool>tmp(101,false);
-		int cnt = 0;
-		while (1) {
-			cin >> num;
-			if (num < 1)
-				break;	
-			tmp[num] = true;
-		}
-		if (num == -1)
-			break;
-		for (int i = 1; i <= 50; i++)
-			if (tmp[i] && tmp[i * 2])
-				cnt++;
-		cout << cnt << "\n";
+	int a, b, n;
+	cin >> a >> b >> n;
+	if (a > b)
+		a %= b;
+	for (int i = 0; i < n; i++) { 
+		a %= b;
+		a *= 10;
 	}
-	
+	cout << a/b;
 	return 0;
 }
