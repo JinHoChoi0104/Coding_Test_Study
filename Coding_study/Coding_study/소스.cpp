5ed172e83806f2bb248231991aa8f7560ca27d06
JinHoChoi <bearsff@naver.com>
2021-12-23 00:05:16 +0900

    Day 209
    BAEKJOON 2965

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int a, b, n;
-	cin >> a >> b >> n;
-	if (a > b)
-		a %= b;
-	for (int i = 0; i < n; i++) { 
-		a %= b;
-		a *= 10;
-	}
-	cout << a/b;
+	int a, b, c;
+	cin >> a >> b >> c;
+	int ans = max(c - b, b - a);
+	if (ans == 0)
+		cout << 0;
+	else
+		cout << ans - 1;
 	return 0;
 }
