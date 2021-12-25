b001d1e186c6742b973f8c235016c1d93db335c4
JinHoChoi <bearsff@naver.com>
2021-12-25 17:33:49 +0900

    Day 212
    BAEKJOON 10162

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -5,24 +5,15 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
 	int num;
-	while (1) {
-		cin >> num;
-		if (num == 0)
-			break;
-		int ans = 0;
-		while (num) {
-			ans += num % 10;
-			num /= 10;
-			if (num == 0) {
-				if (ans < 10)
-					break;
-				else {
-					num = ans;
-					ans = 0;
-				}
-			}
-		}
-		cout << ans << "\n";
+	cin >> num;
+	if (num % 10 != 0) {
+		cout << -1;
+		return 0;
 	}
+	cout << num / 300 << " ";
+	num %= 300;
+	cout << num / 60 << " ";
+	num %= 60;
+	cout << num / 10;
 	return 0;
 }
