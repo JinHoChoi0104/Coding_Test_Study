44b3e2cc24d3b11f93ab94345a0b69410d2d20ff
JinHoChoi <bearsff@naver.com>
2021-12-26 18:10:14 +0900

    Day 213
    BAEKJOON 3023

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -4,16 +4,32 @@ using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int num;
-	cin >> num;
-	if (num % 10 != 0) {
-		cout << -1;
-		return 0;
+	int n, m;
+	cin >> n >> m;
+	char c[101][101];
+	for (int i = 0; i < n; i++) 
+		for (int j = 0; j < m; j++) 
+			cin >> c[i][j];
+	int p, q;
+	cin >> p >> q;
+	
+	for (int i = 0; i < n; i++) 
+		for (int j = m; j < m * 2; j++) 
+			c[i][j] = c[i][2 * m - j - 1];
+	for (int i = n; i < n * 2; i++) 
+		for (int j = 0; j < m * 2; j++) 
+			c[i][j] = c[2 * n - i - 1][j];
+		
+	if (c[p - 1][q - 1] == '#')
+		c[p - 1][q - 1] = '.';
+	else
+		c[p - 1][q - 1] = '#';
+
+	for (int i = 0; i < n * 2; i++) {
+		for (int j = 0; j < m * 2; j++)
+			cout << c[i][j];
+		cout << "\n";
 	}
-	cout << num / 300 << " ";
-	num %= 300;
-	cout << num / 60 << " ";
-	num %= 60;
-	cout << num / 10;
+	
 	return 0;
 }
