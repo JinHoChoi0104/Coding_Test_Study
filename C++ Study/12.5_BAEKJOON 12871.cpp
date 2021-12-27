f54ce265539e8940740d03e4d4965a0feccc98ae
JinHoChoi <bearsff@naver.com>
2021-12-27 23:36:05 +0900

    c++ 12.5
    BAEKJOON 12871

---
+++ C++ Study/12.5_BAEKJOON 12871.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+bool isSame(string a, string b) {
+	int len1 = a.size(), len2 = b.size();
+	for (int i = 0; i < len1 * len2; i++) {
+		if (a[i % len1] != b[i % len2]) // 다르면 거짓
+			return false;
+		if ((i+1) % len1 == 0 && (i+1) % len2 == 0) //최소공배수면 스탑
+			return true;
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	string a, b;
+	cin >> a >> b;
+
+	if (isSame(a, b))
+		cout << 1;
+	else
+		cout << 0;
+	return 0;
+}
