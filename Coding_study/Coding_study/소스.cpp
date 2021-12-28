15ea2e5fa9be6956e5a40d36451a5a49eb6288bc
JinHoChoi <bearsff@naver.com>
2021-12-29 00:02:21 +0900

    Day 215
    BAEKJOON 2386

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,25 +1,24 @@
 #include <iostream>
+#include <string>
 using namespace std;
-
-bool isSame(string a, string b) {
-	int len1 = a.size(), len2 = b.size();
-	for (int i = 0; i < len1 * len2; i++) {
-		if (a[i % len1] != b[i % len2])
-			return false;
-		if ((i+1) % len1 == 0 && (i+1) % len2 == 0)
-			return true;
-	}
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	string a, b;
-	cin >> a >> b;
-
-	if (isSame(a, b))
-		cout << 1;
-	else
-		cout << 0;
+	string str;
+	char c, tmp;
+	while (1) {
+		cin >> c;
+		if (c == '#')
+			break;
+		tmp = c - 32;
+		getline(cin, str, '\n');
+		int cnt = 0;
+		for (int i = 0; i < str.size(); i++) 
+			if (str[i] == c || str[i] == tmp)
+				cnt++;
+		cout << c << " " << cnt << "\n";
+	}
+	
 	return 0;
 }
