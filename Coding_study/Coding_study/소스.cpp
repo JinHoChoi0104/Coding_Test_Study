b6282192401c07c738a3339c82272491edc0a156
JinHoChoi <bearsff@naver.com>
2021-12-31 00:29:59 +0900

    Day 217
    BAEKJOON 5217

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,24 +1,19 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	string str;
-	char c, tmp;
-	while (1) {
-		cin >> c;
-		if (c == '#')
-			break;
-		tmp = c - 32;
-		getline(cin, str, '\n');
-		int cnt = 0;
-		for (int i = 0; i < str.size(); i++) 
-			if (str[i] == c || str[i] == tmp)
-				cnt++;
-		cout << c << " " << cnt << "\n";
+	int t, num;
+	for (cin >> t; t-- > 0;) {
+		cin >> num;
+		cout << "Pairs for " << num << ": ";
+		for (int i = 1; i < (num+1) / 2; i++) {
+			if (i != 1)
+				cout << ", ";
+			cout << i << " " << num - i;
+		}
+		cout << "\n";
 	}
-	
 	return 0;
 }
