2c9fd6ac0e6ddb93b353bb890f4df7d58c2a8d94
JinHoChoi <bearsff@naver.com>
2022-01-02 00:09:31 +0900

    Day 219
    BAEKJOON 2999

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -5,16 +5,27 @@ using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int n, w, h, num;
-	cin >> n >> w >> h;
-	int tmp = pow(w, 2) + pow(h, 2);
-	tmp = sqrt(tmp);
-	for (int i = 0; i < n; i++) {
-		cin >> num;
-		if (num <= tmp)
-			cout << "DA\n";
-		else
-			cout << "NE\n";
+	string str;
+	cin >> str;
+	int n = str.size();
+	int r = sqrt(n), c;
+	while (1) {
+		if (n % r != 0) {
+			r--;
+			continue;
+		}
+		c = n / r;
+		break;
 	}
+	int index = 0;
+	char ch[101][101];
+	for (int i = 0; i < c; i++) 
+		for (int j = 0; j < r; j++) 
+			ch[j][i] = str[index++];
+
+	for (int i = 0; i < r; i++) 
+		for (int j = 0; j < c; j++) 
+			cout << ch[i][j];
+
 	return 0;
 }
