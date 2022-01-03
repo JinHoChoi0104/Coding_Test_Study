19a34f464665f8a0567804033428be36f262e143
JinHoChoi <bearsff@naver.com>
2022-01-04 01:25:48 +0900

    Day 221
    BAEKJOON 2959

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -6,11 +6,10 @@ using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	vector<int>arr(3);
-	for (int i = 0; i < 3; i++) 
+	vector<int>arr(4);
+	for (int i = 0; i < 4; i++) 
 		cin >> arr[i];
 	sort(arr.begin(), arr.end());
-	for (int i = 0; i < 3; i++)
-		cout << arr[i] << " ";
+	cout << arr[0] * arr[2];
 	return 0;
 }
