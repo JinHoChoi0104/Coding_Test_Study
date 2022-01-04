f149f2673cec48719022c7304c2db97fba4e3979
JinHoChoi <bearsff@naver.com>
2022-01-04 23:56:00 +0900

    Day 222
    BAEKJOON 4880

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	vector<int>arr(4);
-	for (int i = 0; i < 4; i++) 
-		cin >> arr[i];
-	sort(arr.begin(), arr.end());
-	cout << arr[0] * arr[2];
+	int a, b, c;
+	while (1) {
+		cin >> a >> b >> c;
+		if (a == 0 && b == 0 && c == 0)
+			break;
+		if (a * c == b * b) 
+			cout << "GP " << b * c / a << "\n";
+		else
+			cout << "AP " << b + c - a << "\n";
+	}
 	return 0;
 }
