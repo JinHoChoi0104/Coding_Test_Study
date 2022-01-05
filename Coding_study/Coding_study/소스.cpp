de4603f9fdafd526b2cd41bda803d3084ab6ac0f
JinHoChoi <bearsff@naver.com>
2022-01-05 19:09:19 +0900

    Day 223
    BAEKJOON 4493

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -4,15 +4,37 @@ using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int a, b, c;
-	while (1) {
-		cin >> a >> b >> c;
-		if (a == 0 && b == 0 && c == 0)
-			break;
-		if (a * c == b * b) 
-			cout << "GP " << b * c / a << "\n";
+	int T, num;
+	for (cin >> T; T-- > 0;) {
+		int tmp = 0;;
+		for (cin >> num; num-- > 0;) {
+			char a, b;
+			cin >> a >> b;
+			if (a == 'R') {
+				if (b == 'S')
+					tmp++;
+				else if (b == 'P')
+					tmp--;
+			}
+			else if (a == 'S') {
+				if (b == 'P')
+					tmp++;
+				else if (b == 'R')
+					tmp--;
+			}
+			else {
+				if (b == 'R')
+					tmp++;
+				else if (b == 'S')
+					tmp--;
+			}
+		}
+		if (tmp > 0)
+			cout << "Player 1\n";
+		else if (tmp < 0)
+			cout << "Player 2\n";
 		else
-			cout << "AP " << b + c - a << "\n";
+			cout << "TIE\n";
 	}
 	return 0;
 }
