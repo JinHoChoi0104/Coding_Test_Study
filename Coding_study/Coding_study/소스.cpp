8b1143c00b7e28f594a8c45da28bd0c52af1bff4
JinHoChoi <bearsff@naver.com>
2022-01-07 23:57:13 +0900

    Day 225
    BAEKJOON 4766

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,41 +1,18 @@
 #include <iostream>
 using namespace std;
-bool arr[3];
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	string str;
-	cin >> str;
-	arr[0] = true;
-
-	for (int i = 0; i < str.size(); i++) {
-		if (str[i] == 'C') {
-			if (!arr[0] && !arr[2])
-				continue;
-			if (arr[0])
-				arr[0] = false, arr[2] = true;
-			else
-				arr[2] = false, arr[0] = true;
-		}
-		else if (str[i] == 'B') {
-			if (!arr[1] && !arr[2])
-				continue;
-			if (arr[1])
-				arr[1] = false, arr[2] = true;
-			else
-				arr[2] = false, arr[1] = true;
-		}
-		else {
-			if (!arr[0] && !arr[1])
-				continue;
-			if (arr[1])
-				arr[1] = false, arr[0] = true;
-			else
-				arr[0] = false, arr[1] = true;
-		}
+	double num, tmp;
+	cin >> num;
+	while (1) {
+		cin >> tmp;
+		if (tmp == 999)
+			break;
+		cout << fixed;
+		cout.precision(2);
+		cout << tmp - num << "\n";
+		num = tmp;
 	}
-	for (int i = 0; i < 3; i++)
-		if (arr[i])
-			cout << i + 1;
 	return 0;
 }
