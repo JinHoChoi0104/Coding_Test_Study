4462660f71702097c14f4270af8e6fcd75e9f585
JinHoChoi <bearsff@naver.com>
2022-01-08 12:07:41 +0900

    Day 226
    BAEKJOON 4619

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	double num, tmp;
-	cin >> num;
+
+	int B, N;
 	while (1) {
-		cin >> tmp;
-		if (tmp == 999)
+		cin >> B >> N;
+		if (B == 0 && N == 0)
 			break;
-		cout << fixed;
-		cout.precision(2);
-		cout << tmp - num << "\n";
-		num = tmp;
+		for (int i = 1; i <= B; i++) {
+			int tmp = pow(i, N);
+			if (tmp == B) {
+				cout << i << "\n";
+				break;
+			}
+			if (tmp > B) {
+				int dif1 = B - pow(i - 1, N);
+				int dif2 = tmp - B;
+				if (dif1 > dif2)
+					cout << i << "\n";
+				else
+					cout << i - 1 << "\n";
+				break;
+			}
+		}
 	}
 	return 0;
 }
