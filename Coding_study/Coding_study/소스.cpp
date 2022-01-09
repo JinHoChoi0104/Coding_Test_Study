ee748ea347b57da9253c973526decf1f88f930f2
JinHoChoi <bearsff@naver.com>
2022-01-09 11:31:35 +0900

    Day 227
    BAEKJOON 10707

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,31 +1,20 @@
 #include <iostream>
-#include <math.h>
 using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
 
-	int B, N;
-	while (1) {
-		cin >> B >> N;
-		if (B == 0 && N == 0)
-			break;
-		for (int i = 1; i <= B; i++) {
-			int tmp = pow(i, N);
-			if (tmp == B) {
-				cout << i << "\n";
-				break;
-			}
-			if (tmp > B) {
-				int dif1 = B - pow(i - 1, N);
-				int dif2 = tmp - B;
-				if (dif1 > dif2)
-					cout << i << "\n";
-				else
-					cout << i - 1 << "\n";
-				break;
-			}
-		}
+	int a, b, c, d, p;
+	cin >> a >> b >> c >> d >> p;
+	int ans1 = a * p;
+	int ans2 = b;
+	if (c < p) {
+		int tmp = p - c;
+		ans2 += tmp * d;
 	}
+	if (ans1 > ans2)
+		cout << ans2;
+	else
+		cout << ans1;
 	return 0;
 }
