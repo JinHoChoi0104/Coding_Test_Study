f452262041b721a6760eab3aa4c53d131151bf2d
JinHoChoi <bearsff@naver.com>
2022-01-12 01:22:55 +0900

    Day 229
    BAEKJOON 2953

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -4,25 +4,18 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
 
-	double a, b, c, d, max_num, tmp;
-	cin >> a >> b >> c >> d;
-	int ans = 0;
-	max_num = a / c + b / d;
-	tmp = c / d + a / b;
-	if (tmp > max_num) {
-		max_num = tmp;
-		ans = 1;
+	int index=0, ans=0, num;
+	for (int i = 0; i < 5; i++) {
+		int tmp = 0;
+		for (int j = 0; j < 4; j++) {
+			cin >> num;
+			tmp += num;
+		}
+		if (tmp > ans) {
+			ans = tmp;
+			index = i + 1;
+		}
 	}
-	tmp = d / b + c / a;
-	if (tmp > max_num) {
-		max_num = tmp;
-		ans = 2;
-	}
-	tmp = b / a + d / c;
-	if (tmp > max_num) {
-		max_num = tmp;
-		ans = 3;
-	}
-	cout << ans;
+	cout << index << " " << ans;
 	return 0;
 }
