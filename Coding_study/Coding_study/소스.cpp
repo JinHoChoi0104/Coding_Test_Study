9cb92e5b098e78a2236d00a59a3c0f8c0f17c038
JinHoChoi <bearsff@naver.com>
2022-01-15 01:01:45 +0900

    Day 232
    BAEKJOON 2420

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,39 +1,11 @@
 #include <iostream>
-#include <algorithm>
+#include <math.h>
 using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-
-	int n, m, num, arr[500][500];
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> num;
-			int cnt = 0;
-			while (num > 0) {
-				if (num % 10 == 9)
-					cnt++;
-				num /= 10;
-			}
-			arr[i][j] = cnt;
-		}
-	}
-	int total = 0, max_num = 0;
-	for (int i = 0; i < n; i++) {
-		int tmp = 0;
-		for (int j = 0; j < m; j++) 
-			tmp += arr[i][j];
-		total += tmp;
-		max_num = max(max_num, tmp);
-	}
-	for (int i = 0; i < m; i++) {
-		int tmp = 0;
-		for (int j = 0; j < n; j++)
-			tmp += arr[j][i];
-		max_num = max(max_num, tmp);
-	}
-	
-	cout << total - max_num;
+	long long a, b;
+	cin >> a >> b;
+	cout << abs(a - b);
 	return 0;
 }
