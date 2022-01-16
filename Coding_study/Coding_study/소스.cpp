e18cef512567dbcb3069d0727337ca6b25e1030f
JinHoChoi <bearsff@naver.com>
2022-01-17 01:12:21 +0900

    Day 234
    BAEKJOON 5073

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
-
 using namespace std;
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int n, num = 2, tmp = 1;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		num += tmp;
-		tmp *= 2;
+	int a, b, c;
+	while (1) {
+		cin >> a >> b >> c;
+		if (a == 0)
+			break;
+		
+		if (a + b <= c || a + c <= b || b + c <= a) {
+			cout << "Invalid\n";
+			continue;
+		}
+
+		if (a == b && b == c)
+			cout << "Equilateral\n";
+		else if (a == b || b == c || a == c)
+			cout << "Isosceles\n";
+		else
+			cout << "Scalene\n";
 	}
-	cout << num * num;
 	return 0;
 }
