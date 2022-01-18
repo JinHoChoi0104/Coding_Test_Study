82dc41e48ebea0472ced30115260d3e194b35a66
JinHoChoi <bearsff@naver.com>
2022-01-18 23:41:44 +0900

    Day 236
    BAEKJOON 17496

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -4,19 +4,10 @@ using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int a, b, c;
-	cin >> a >> b >> c;
-
-	if (a + b + c != 180) 
-		cout << "Error\n";
-	else {
-		if(a==60 && b==60)
-			cout << "Equilateral\n";
-		else if (a == b || b == c || a == c)
-			cout << "Isosceles\n";
-		else
-			cout << "Scalene\n";
-	}
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	int tmp = (a - 1) / b;
+	cout << tmp * c * d;
 	
 	return 0;
 }
