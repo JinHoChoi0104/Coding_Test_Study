390cb4944d6e870ac0513de7ae7e40c2df180fc2
JinHoChoi <bearsff@naver.com>
2022-01-20 00:29:17 +0900

    Day 237
    BAEKJOON 2355

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -4,10 +4,19 @@ using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
-	int a, b, c, d;
-	cin >> a >> b >> c >> d;
-	int tmp = (a - 1) / b;
-	cout << tmp * c * d;
+	long long a, b;
+	cin >> a >> b;
+	if (a > b) {
+		int k = a;
+		a = b;
+		b = k;
+	}
+	long long ans = a + b, tmp = b - a + 1;
+	if (ans % 2 == 0)
+		ans /= 2;
+	else
+		tmp /= 2;
+	cout << ans * tmp;
 	
 	return 0;
 }
