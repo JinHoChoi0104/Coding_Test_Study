ead1d014a18dea61292c7db6319a43886442e72c
JinHoChoi <bearsff@naver.com>
2022-01-26 23:14:37 +0900

    Day 244
    BAEKJOON 7770

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 int main(){
-	int t, num;
-	for (cin >> t; t-- > 0;) {
-		int sum = 0, ans = 100;
-		for (int i = 0; i < 7; i++) {
-			cin >> num;
-			if (num % 2 != 0)
-				continue;
-			ans = min(ans, num);
-			sum += num;
+	int num, cnt = 0, tmp = 4, sum = 1;
+	cin >> num;
+	while (num > 0) {
+		num -= sum;
+		sum += tmp;
+		tmp += 4;
+		cnt++;
+		if (num < sum) {
+			break;
 		}
-		cout << sum << " " << ans << "\n";
 	}
+	cout << cnt;
 	return 0;
 } 
