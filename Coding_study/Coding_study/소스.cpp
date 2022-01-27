bdcb68073ebd7a09cd10c1a714f3dcbdf6ff7ade
JinHoChoi <bearsff@naver.com>
2022-01-27 22:56:38 +0900

    Day 245
    BAEKJOON 1640

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
 int main(){
-	int num, cnt = 0, tmp = 4, sum = 1;
-	cin >> num;
-	while (num > 0) {
-		num -= sum;
-		sum += tmp;
-		tmp += 4;
-		cnt++;
-		if (num < sum) {
-			break;
-		}
+	int num, ans = 0;
+	vector<int> arr;
+	for (int i = 0; i < 4; i++) {
+		cin >> num;
+		arr.push_back(num);
 	}
-	cout << cnt;
+	sort(arr.begin(), arr.end());
+	for (int i = 1; i < 4; i++) {
+		ans += arr[i];
+	}
+	int tmp1, tmp2;
+	cin >> tmp1 >> tmp2;
+	if (tmp1 > tmp2)
+		ans += tmp1;
+	else
+		ans += tmp2;
+	cout << ans;
 	return 0;
 } 
