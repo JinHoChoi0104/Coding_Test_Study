f1f82f9dabd8625f94564f38c33b50783654d132
JinHoChoi <bearsff@naver.com>
2022-01-29 03:46:32 +0900

    Day 246
    BAEKJOON 10797

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,24 +1,13 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 using namespace std;
 int main(){
-	int num, ans = 0;
-	vector<int> arr;
-	for (int i = 0; i < 4; i++) {
-		cin >> num;
-		arr.push_back(num);
+	int num, tmp, ans = 0;
+	cin >> num;
+	for (int i = 0; i < 5; i++) {
+		cin >> tmp;
+		if (num == tmp)
+			ans++;
 	}
-	sort(arr.begin(), arr.end());
-	for (int i = 1; i < 4; i++) {
-		ans += arr[i];
-	}
-	int tmp1, tmp2;
-	cin >> tmp1 >> tmp2;
-	if (tmp1 > tmp2)
-		ans += tmp1;
-	else
-		ans += tmp2;
 	cout << ans;
 	return 0;
 } 
