a5f9da666044e1d9b66d3cc9b585f4dcf04c66d9
JinHoChoi <bearsff@naver.com>
2022-02-02 16:33:59 +0900

    Day 250
    BAEKJOON 24418

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 using namespace std;
 int main(){
-	int num;
-	cin >> num;
-	for (int i = 0; i < num * 4; i++) {
-		for (int j = 0; j < num; j++) {
-			cout << "@";
-		}
-		cout << "\n";
+	int n, num, arr[16][16];
+	for (int i = 1; i <= 15; i++) {
+		arr[1][i] = i + 1;
+		arr[i][1] = i + 1;
 	}
-	for (int i = 0; i < num; i++) {
-		for (int j = 0; j < num*5; j++) {
-			cout << "@";
-		}
-		cout << "\n";
-	}
+	for (int i = 2; i <= 15; i++) 
+		for (int j = 2; j <= 15; j++) 
+			arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			cin >> num;
 
+	cout << arr[n][n] << " " << n * n;
 	return 0;
 } 
