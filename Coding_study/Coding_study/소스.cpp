f0727f3a72b60c779feaab2c5e85e3f7a4958ea2
JinHoChoi <bearsff@naver.com>
2022-03-05 21:11:24 +0900

    BAEKJOON 10926

--- Coding_study/Coding_study/소스.cpp
+++ Coding_study/Coding_study/소스.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
 using namespace std;
-int main(){
-	int num, arr[6] = {1,1,2,2,2,8};
-	for (int i = 0; i < 6; i++) {
-		cin >> num;
-		arr[i] -= num;
-	}
-	for (int i = 0; i < 6; i++) {
-		cout << arr[i] << " ";
-	}
+int main() {
+	string str;
+	cin >> str;
+	cout << str << "??!";
 	return 0;
-} 
+}
